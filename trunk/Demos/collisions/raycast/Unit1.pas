cb5dfcf7ef32cfc9a5eea0470b4f77b862c5e15d
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2001-08-19 14:13:11 +0000

    Added RayCast demo, minor fixes,
    git-svn-id: http://svn.code.sf.net/p/glscene/code@38 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Demos/collisions/raycast/Unit1.pas
@@ -0,0 +1,113 @@
+{: Demo/sample/testbed for RayCastIntersect.<p>
+
+   The RayCastIntersect aims at determining an as precise as possible collision
+   detection between a ray and and object. With the intersection point is also
+   returned the normal (which can be used for things like bouncing).<p>
+
+   In this sample, this mechanism is used to implement a two-cents worth
+   raytracer, simply by throwing rays for each point in a raster image (that
+   is what raytracers do, but they go beyond throwing simple rays ;)),
+   the intersection's normal and intersected  object's material is then used
+   to calculate a basic lighting.<p>
+
+   To calculate the raytraced/raycasted image, just it the "cast" button.
+}
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  GLScene, GLObjects, GLMisc, StdCtrls, ExtCtrls, GLTexture, GLCadencer;
+
+type
+  TForm1 = class(TForm)
+    GLSceneViewer1: TGLSceneViewer;
+    GLScene1: TGLScene;
+    Sphere1: TSphere;
+    GLLightSource1: TGLLightSource;
+    GLCamera1: TGLCamera;
+    Image1: TImage;
+    BUCast: TButton;
+    Bevel1: TBevel;
+    Bevel2: TBevel;
+    GLCadencer1: TGLCadencer;
+    Sphere2: TSphere;
+    DummyCube1: TDummyCube;
+    Panel1: TPanel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure BUCastClick(Sender: TObject);
+    procedure GLCadencer1Progress(Sender: TObject; const deltaTime,
+      newTime: Double);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.DFM}
+
+uses Geometry;
+
+procedure TForm1.BUCastClick(Sender: TObject);
+var
+   o, v, vLight, light, iPoint, iNormal : TAffineVector;
+   up, right, dir : TAffineVector;
+   x, y, dx, dy : Integer;
+   f, d : Single;
+   color : TColor;
+   iObj : TGLBaseSceneObject;
+begin
+   Screen.Cursor:=crHourGlass;
+   // First we extract/prepare the vector we will use during our raycasting
+   // the origin is the camera position, and factor was grossly adjusted so
+   // that both view look grossly similar
+   SetVector(o,   GLCamera1.AbsolutePosition);
+   SetVector(dir, GLCamera1.AbsoluteDirection);
+   SetVector(up,  GLCamera1.AbsoluteUp);
+   SetVector(light, GLLightSource1.AbsolutePosition);
+   right:=VectorCrossProduct(dir, up);
+   f:=1/300;
+   dx:=(Image1.Width div 2);
+   dy:=(Image1.Height div 2);
+   // Cover a square area
+   for y:=0 to Image1.Height-1 do begin
+      for x:=0 to Image1.Width-1 do begin
+         // Calculate our ray vector
+         v:=VectorCombine3(dir, right, up, 1, (x-dx)*f, (dy-y)*f);
+         // ray vectors must be of unit length!
+         NormalizeVector(v);
+         // ray cast
+         iObj:=GLScene1.RayCastIntersect(o, v, @iPoint, @iNormal);
+         if Assigned(iObj) then begin
+            // if something found, calculate vector to light source
+            vLight:=VectorSubtract(light, iPoint);
+            NormalizeVector(vLight);
+            // color is given by the normal/lightsource vectors dot-product
+            // and this intensity is composited with the object's diffuse color
+            d:=VectorDotProduct(iNormal, vLight);
+            if d<0 then d:=0;
+            with (iObj as TGLCustomSceneObject).Material.FrontProperties do
+               color:=ConvertColorVector(Diffuse.Color, d);
+         end else color:=clGray;
+         // plot our point
+         Image1.Canvas.Pixels[x, y]:=color;
+      end;
+   end;
+   Screen.Cursor:=crDefault;
+end;
+
+procedure TForm1.GLCadencer1Progress(Sender: TObject; const deltaTime,
+  newTime: Double);
+begin
+   DummyCube1.TurnAngle:=newTime*60;
+end;
+
+end.
