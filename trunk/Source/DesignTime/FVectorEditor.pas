3cc8826eb1d518df38818945efc6051376b29c2f
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2001-12-09 20:30:51 +0000

    Design time enhancements (Neil Graham)
    git-svn-id: http://svn.code.sf.net/p/glscene/code@113 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/DesignTime/FVectorEditor.pas
+++ trunk/Source/DesignTime/FVectorEditor.pas
@@ -19,12 +19,17 @@ type
     IMx: TImage;
     IMy: TImage;
     IMz: TImage;
-    ToolBar: TToolBar;
-    TBx: TToolButton;
-    TBy: TToolButton;
-    TBz: TToolButton;
-    TBnull: TToolButton;
-    ToolButton5: TToolButton;
+    SpeedButton1: TSpeedButton;
+    SBmX: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SBmY: TSpeedButton;
+    SpeedButton5: TSpeedButton;
+    SBmZ: TSpeedButton;
+    SpeedButton7: TSpeedButton;
+    SBUnit: TSpeedButton;
+    SpeedButton9: TSpeedButton;
+    Bevel1: TBevel;
+    SBInvert: TSpeedButton;
     procedure TBxClick(Sender: TObject);
     procedure TByClick(Sender: TObject);
     procedure TBzClick(Sender: TObject);
@@ -32,6 +37,12 @@ type
     procedure EDxChange(Sender: TObject);
     procedure EDyChange(Sender: TObject);
     procedure EDzChange(Sender: TObject);
+    procedure SBmXClick(Sender: TObject);
+    procedure SBmYClick(Sender: TObject);
+    procedure SBmZClick(Sender: TObject);
+    procedure SBUnitClick(Sender: TObject);
+    procedure SpeedButton9Click(Sender: TObject);
+    procedure SBInvertClick(Sender: TObject);
   private
     { Déclarations privées }
     vx, vy, vz : Single;
@@ -54,7 +65,7 @@ implementation
 
 {$R *.DFM}
 
-uses SysUtils;
+uses SysUtils, Geometry;
 
 var
 	vVectorEditorForm : TVectorEditorForm;
@@ -139,6 +150,50 @@ end;
 procedure TVectorEditorForm.EDzChange(Sender: TObject);
 begin
    TestInput(EDz, IMz, vz);
+end;
+
+procedure TVectorEditorForm.SBmXClick(Sender: TObject);
+begin
+   EDx.Text:='-1'; EDy.Text:='0'; EDz.Text:='0';
+end;
+
+procedure TVectorEditorForm.SBmYClick(Sender: TObject);
+begin
+   EDx.Text:='0'; EDy.Text:='-1'; EDz.Text:='0';
+end;
+
+procedure TVectorEditorForm.SBmZClick(Sender: TObject);
+begin
+   EDx.Text:='0'; EDy.Text:='0'; EDz.Text:='-1';
+end;
+
+procedure TVectorEditorForm.SBUnitClick(Sender: TObject);
+begin
+   EDx.Text:='1'; EDy.Text:='1'; EDz.Text:='1';
+end;
+
+procedure TVectorEditorForm.SpeedButton9Click(Sender: TObject);
+var
+   v : TAffineVector;
+begin
+   SetVector(v, StrToFloatDef(EDx.Text, 0), StrToFloatDef(EDy.Text, 0), StrToFloatDef(EDy.Text, 0));
+   if VectorLength(v)=0 then
+      v:=NullVector
+   else NormalizeVector(v);
+   EDx.Text:=FloatToStr(v[0]);
+   EDy.Text:=FloatToStr(v[1]);
+   EDz.Text:=FloatToStr(v[2]);
+end;
+
+procedure TVectorEditorForm.SBInvertClick(Sender: TObject);
+var
+   v : TAffineVector;
+begin
+   SetVector(v, StrToFloatDef(EDx.Text, 0), StrToFloatDef(EDy.Text, 0), StrToFloatDef(EDy.Text, 0));
+   NegateVector(v);
+   EDx.Text:=FloatToStr(v[0]);
+   EDy.Text:=FloatToStr(v[1]);
+   EDz.Text:=FloatToStr(v[2]);
 end;
 
 // ------------------------------------------------------------------
