4c097ba1cfa740f53faba5104d5a8e54510c7432
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2002-01-23 12:41:47 +0000

    GF2 stats
    git-svn-id: http://svn.code.sf.net/p/glscene/code@236 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/bench/volcano/Unit1.pas
+++ trunk/Demos/bench/volcano/Unit1.pas
@@ -9,8 +9,9 @@
 
    CPU               Graphics          Colors      FPS         Sort Time
 
+   TBird 1.1GHz      GeForce2 Pro      32 bits     96.9        2.69 msec
    Duron 800MHz      TNT2 M64          32 bits     16.6        4.45 msec
-   --- 23/01/02 - ZWrite=False in the PFX Renderer
+   --- 23/01/02 - ZWrite=False in the PFX Renderer, minor optims
    TBird 1.1GHz      GeForce2 Pro      32 bits     91.7        2.86 msec
    Duron 800MHz      TNT2 M64          32 bits     12.2        4.45 msec
    --- 20/01/02 - Optimized PFX (sort) and TGLPolygonPFXManager (rendering)
