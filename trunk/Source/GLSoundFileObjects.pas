601d39837a4a40e675f5518434dcc7ba1208c7ea
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2002-02-26 21:40:34 +0000

    Added MP3 'support'
    git-svn-id: http://svn.code.sf.net/p/glscene/code@363 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLSoundFileObjects.pas
+++ trunk/Source/GLSoundFileObjects.pas
@@ -130,6 +130,35 @@ type
 	      function LengthInBytes : Integer; override;
    end;
 
+   // TGLMP3File
+   //
+   {: Support for MP3 format.<p>
+      *Partial* support only, access to PCMData is NOT supported. }
+   TGLMP3File = class (TGLSoundFile)
+      private
+         { Public Declarations }
+         data : String; // used to store MP3 bitstream
+
+      protected
+         { Protected Declarations }
+
+      public
+         { Private Declarations }
+         function CreateCopy(AOwner: TPersistent) : TDataFile; override;
+
+         class function Capabilities : TDataFileCapabilities; override;
+
+         procedure LoadFromStream(Stream: TStream); override;
+         procedure SaveToStream(Stream: TStream); override;
+
+         procedure PlayOnWaveOut; override;
+
+	      function WAVData : Pointer; override;
+         function WAVDataSize : Integer; override;
+	      function PCMData : Pointer; override;
+	      function LengthInBytes : Integer; override;
+   end;
+
    // TGLSoundFileFormat
    //
    TGLSoundFileFormat = record
@@ -475,13 +504,6 @@ begin
    stream.Write(data[1], Length(data));
 end;
 
-// LengthInBytes
-//
-function TGLWAVFile.LengthInBytes : Integer;
-begin
-   Result:=Length(data)-pcmOffset;
-end;
-
 // PlayOnWaveOut
 //
 procedure TGLWAVFile.PlayOnWaveOut;
@@ -513,6 +535,88 @@ begin
    if Length(data)>0 then
       Result:=@data[1+pcmOffset]
    else Result:=nil;
+end;
+
+// LengthInBytes
+//
+function TGLWAVFile.LengthInBytes : Integer;
+begin
+   Result:=Length(data)-pcmOffset;
+end;
+
+// ------------------
+// ------------------ TGLMP3File ------------------
+// ------------------
+
+// CreateCopy
+//
+function TGLMP3File.CreateCopy(AOwner: TPersistent) : TDataFile;
+begin
+   Result:=inherited CreateCopy(AOwner);
+   if Assigned(Result) then begin
+      TGLWAVFile(Result).data:=data;
+   end;
+end;
+
+// Capabilities
+//
+class function TGLMP3File.Capabilities : TDataFileCapabilities;
+begin
+   Result:=[dfcRead, dfcWrite];
+end;
+
+// LoadFromStream
+//
+procedure TGLMP3File.LoadFromStream(stream : TStream);
+begin
+   // MP3 isn't actually, just loaded directly...
+   Assert(Assigned(stream));
+   SetLength(data, stream.Size);
+   stream.Read(data[1], Length(data));
+end;
+
+// SaveToStream
+//
+procedure TGLMP3File.SaveToStream(stream: TStream);
+begin
+   stream.Write(data[1], Length(data));
+end;
+
+// PlayOnWaveOut
+//
+procedure TGLMP3File.PlayOnWaveOut;
+begin
+   Assert(False, 'MP3 playback on WaveOut not supported.');
+end;
+
+// WAVData
+//
+function TGLMP3File.WAVData : Pointer;
+begin
+   if Length(data)>0 then
+      Result:=@data[1]
+   else Result:=nil;
+end;
+
+// WAVDataSize
+//
+function TGLMP3File.WAVDataSize : Integer;
+begin
+   Result:=Length(data);
+end;
+
+// PCMData
+//
+function TGLMP3File.PCMData : Pointer;
+begin
+   Result:=nil;
+end;
+
+// LengthInBytes
+//
+function TGLMP3File.LengthInBytes : Integer;
+begin
+   Result:=0;
 end;
 
 // ------------------
@@ -618,6 +722,7 @@ initialization
 
 	// class registrations
   RegisterSoundFileFormat('wav', 'Windows WAV files', TGLWAVFile);
+  RegisterSoundFileFormat('mp3', 'MPEG Layer3 files', TGLMP3File);
 
 end.
 
