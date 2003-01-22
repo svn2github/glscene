78daf0d1a28f0d5b9de760df5f544bdc287bd4e6
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-01-22 14:20:49 +0000

    IntersectAABBs moved in (Bernd Klaiber)
    git-svn-id: http://svn.code.sf.net/p/glscene/code@799 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Base/GeometryBB.pas
+++ trunk/Source/Base/GeometryBB.pas
@@ -3,6 +3,7 @@
 	Calculations and manipulations on Bounding Boxes.<p>
 
 	<b>History : </b><font size=-1><ul>
+      <li>22/01/03 - EG - IntersectAABBs moved in (Bernd Klaiber)
       <li>04/09/03 - EG - New AABB functions
       <li>17/08/01 - EG - Removed "math" dependency
       <li>09/07/01 - EG - Added AABB types and functions
@@ -54,6 +55,10 @@ function BBToAABB(const aBB : THmgBoundingBox) : TAABB;
 function AABBToBB(const anAABB : TAABB) : THmgBoundingBox; overload;
 {: Transforms an AABB to a BB. }
 function AABBToBB(const anAABB : TAABB; const m : TMatrix) : THmgBoundingBox; overload;
+
+{: Determines if two AxisAlignedBoundingBoxes intersect.<p>
+   The matrices are the ones that convert one point to the other's AABB system }
+function IntersectAABBs(const aabb1, aabb2 : TAABB; const m1To2, m2To1 : TMatrix) : Boolean; overload;
 
 type
    TPlanIndices = array [0..3] of Integer;
@@ -311,6 +316,110 @@ function AABBToBB(const anAABB : TAABB; const m : TMatrix) : THmgBoundingBox;
 begin
    Result:=AABBToBB(anAABB);
    BBTransform(Result, m);
+end;
+
+// IntersectCubes (AABBs)
+//
+function IntersectAABBs(const aabb1, aabb2 : TAABB;
+                        const m1To2, m2To1 : TMatrix) : boolean;
+const
+  cWires : array[0..11,0..1] of Integer //Points of the wire
+         = ((0,1),(1,2),(2,3),(3,0),
+            (4,5),(5,6),(6,7),(7,4),
+            (0,4),(1,5),(2,6),(3,7));
+  cPlanes : array[0..5,0..3] of Integer //points of the planes
+         = ((1,2,6,5), (2,3,7,6), (0,1,2,3), (0,3,7,4), (0,1,5,4), (5,6,7,4));
+
+   procedure MakeAABBPoints(const AABB : TAABB; var pt : array of TVertex);
+   begin
+      with AABB do begin
+         SetVector(pt[0], min[0], min[1], min[2]);
+         SetVector(pt[1], max[0], min[1], min[2]);
+         SetVector(pt[2], max[0], max[1], min[2]);
+         SetVector(pt[3], min[0], max[1], min[2]);
+         SetVector(pt[4], min[0], min[1], max[2]);
+         SetVector(pt[5], max[0], min[1], max[2]);
+         SetVector(pt[6], max[0], max[1], max[2]);
+         SetVector(pt[7], min[0], max[1], max[2]);
+      end;
+   end;
+
+   procedure MakePlanes(const pt : array of TVertex; var planes : array of THmgPlane);
+   var
+      i : Integer;
+   begin
+      for i:=0 to 5 do
+         planes[i]:=PlaneMake(pt[cPlanes[i, 0]], pt[cPlanes[i, 1]], pt[cPlanes[i, 2]]);
+   end;
+
+var
+  pt1, pt2: array[0..7] of TVertex;
+  pt:TVertex;
+  Planes2: array[0..5] of THmgPlane;
+  i, t: integer;
+  V: TVertex;
+  P: TVector;
+begin
+  result:= false;
+
+  //Build Points
+  MakeAABBPoints(AABB1, pt1);
+  MakeAABBPoints(AABB2, pt2);
+  for i:=0 to 7 do
+  begin
+    pt:= VectorTransform(pt2[i], m2To1);
+    //check for inclusion (points of Obj2 in Obj1)
+    if IsInRange(pt[0], AABB1.Min[0], AABB1.Max[0]) and
+      IsInRange(pt[1], AABB1.Min[1], AABB1.Max[1]) and
+      IsInRange(pt[2], AABB1.Min[2], AABB1.Max[2]) then
+    begin
+      result:= true;
+      exit;
+    end;
+  end;
+
+  for i:=0 to 7 do
+  begin
+    pt1[i]:= VectorTransform(pt1[i], m1To2);
+    //check for inclusion (points of Obj1 in Obj2)
+    if IsInRange(pt1[i][0], AABB2.Min[0], AABB2.Max[0]) and
+      IsInRange(pt1[i][1], AABB2.Min[1], AABB2.Max[1]) and
+      IsInRange(pt1[i][2], AABB2.Min[2], AABB2.Max[2]) then
+    begin
+      result:= true;
+      exit;
+    end;
+  end;
+
+  //Build Planes
+  MakePlanes(pt2, Planes2);
+
+  //Wire test
+  for i:=0 to 11 do
+  begin
+    for t:=0 to 5 do
+    begin
+      //Build Vector of Ray
+      V:= VectorSubtract(pt1[cWires[i,0]], pt1[cWires[i,1]]);
+      if IntersectLinePlane(VectorMake(pt1[cWires[i,0]]), VectorMake(V), Planes2[t], @P) = 1 then
+      begin
+        //check point in Wire
+        if IsInRange(P[0], pt1[cWires[i,0]][0], pt1[cWires[i,1]][0]) and
+          IsInRange(P[1], pt1[cWires[i,0]][1], pt1[cWires[i,1]][1]) and
+          IsInRange(P[2], pt1[cWires[i,0]][2], pt1[cWires[i,1]][2]) then
+        begin
+          //check point in Plane
+          if IsInRange(P[0], pt2[cPlanes[t, 0]][0], pt2[cPlanes[t, 2]][0]) and
+            IsInRange(P[1], pt2[cPlanes[t, 0]][1], pt2[cPlanes[t, 2]][1]) and
+            IsInRange(P[2], pt2[cPlanes[t, 0]][2], pt2[cPlanes[t, 2]][2]) then
+          begin
+            result:= true;
+            exit;
+          end;
+        end;
+      end;
+    end;
+  end;
 end;
 
 end.
