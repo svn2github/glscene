dcaae0798295619a0bd4911e38f4a926c2ed9250
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-01-22 14:23:23 +0000

    Misc. changes to face-to-face collision
    git-svn-id: http://svn.code.sf.net/p/glscene/code@802 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLCollision.pas
+++ trunk/Source/GLCollision.pas
@@ -14,8 +14,8 @@ unit GLCollision;
 
 interface
 
-uses Classes, GLScene, XCollection, Geometry, VectorLists, GLVectorFileObjects, GeometryBB,
-  GLCrossPlatform;
+uses Classes, GLScene, XCollection, Geometry, VectorLists, GLVectorFileObjects,
+   GeometryBB, GLCrossPlatform;
 
 type
 
@@ -126,15 +126,10 @@ function FastCheckCubeVsSphere(obj1, obj2 : TGLBaseSceneObject) : Boolean;
 function FastCheckCubeVsEllipsoid(obj1, obj2 : TGLBaseSceneObject) : Boolean;
 function FastCheckCubeVsCube(obj1, obj2 : TGLBaseSceneObject) : Boolean;
 function FastCheckFaceVsFace(obj1, obj2 : TGLBaseSceneObject) : Boolean;
-  //returns true when the bounding box cubes does intersect the other
-  //also true when the one cube does contain the other completely
-function IntersectCubes(Obj1, Obj2:TGLBaseSceneObject):boolean; overload;
-  //this one only uses the AxisAlignedBoundingBoxes and the conversion Matrixes
-  //to convert one point to the others AABB system
-function IntersectCubes(const AABB1, AABB2: TAABB; const M1To2, M2To1: TMatrix):boolean; overload;
 
-var
-  UseTriCubes:boolean = true; //debugging
+{: Returns true when the bounding box cubes does intersect the other.<p>
+   Also true when the one cube does contain the other completely. }
+function IntersectCubes(obj1, obj2 : TGLBaseSceneObject) : Boolean; overload;
 
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
@@ -460,50 +455,35 @@ type
    TTriangle = array [0..2] of TAffineVector;
    PTriangle = ^TTriangle;
 
-   procedure KillDuplicates(src, newList : TAffineVectorList);
-//   var
-//      i : integer;
-   begin
-      if src.Count<1 then Exit;
-//      src.Sort; // as triangles
-{      for i:=src.Count-1 downto 1 do begin
-         //adding the good ones to  anew list is better (faster) than deleting the bad ones
-         if CompareTri(i, i-1) <> 0 then
-            newList.Add(self[i]);
-      end;
-      newList.Add(self[0]);
-      }
-      newList.Assign(src);
-   end;
-
 var
-  i:integer;
-  tl: TAffineVectorList;
-  TriListAll, TriList: TAffineVectorList;
-  tri : PTriangle;
+   i : Integer;
+   triList : TAffineVectorList;
+   tri : PTriangle;
+   m1to2, m2to1 : TMatrix;
 begin
-  result:= false;
-  if (obj1 is TGLFreeForm) and (obj2 is TGLFreeForm) then
-  begin
-    //check if we are initialized correct
-    if not assigned(TGLFreeForm(obj1).Octree) then TGLFreeForm(obj1).BuildOctree;
-    if not assigned(TGLFreeForm(obj2).Octree) then TGLFreeForm(obj2).BuildOctree;
+   result:= false;
+   if (obj1 is TGLFreeForm) and (obj2 is TGLFreeForm) then begin
+      //check if we are initialized correctly
+      if not Assigned(TGLFreeForm(obj1).Octree) then
+         TGLFreeForm(obj1).BuildOctree;
+      if not Assigned(TGLFreeForm(obj2).Octree) then
+         TGLFreeForm(obj2).BuildOctree;
 
-    //Check triangles against the other object
-    if UseTriCubes then
-    begin //check only the one that are near the destination object (using octree of obj1)
-      TriListAll:=TGLFreeForm(obj1).GetTrianglesInCube(obj2); //get the 'hot' ones using the tree
-      TriList:=TAffineVectorList.Create;
+      // Check triangles against the other object
+      // check only the one that are near the destination object (using octree of obj1)
+      // get the 'hot' ones using the tree
 
-      KillDuplicates(TriListAll, TriList); //kill the duplicates
+      MatrixMultiply(obj2.AbsoluteMatrix, obj1.InvAbsoluteMatrix, m1to2);
+      MatrixMultiply(obj1.AbsoluteMatrix, obj2.InvAbsoluteMatrix, m2to1);
 
-      TriListAll.Free;
+      triList:=TGLFreeForm(obj1).Octree.GetTrianglesInCube(obj2.AxisAlignedBoundingBox, m1to2, m2to1);
+
       //in the list originally are the local coords, TransformAsPoints-> now we have obj1 absolute coords
-      TriList.TransformAsPoints(obj1.AbsoluteMatrix); //Transform to Absolute Coords
+      triList.TransformAsPoints(obj1.AbsoluteMatrix); //Transform to Absolute Coords
       try
          i:=0;
          while i<TriList.Count-2 do begin
-            //here we pass absolute coords, then these are transformed with Obj2's InvAbsoluteMatrix to match the local Obj2 System
+            // here we pass absolute coords, then these are transformed with Obj2's InvAbsoluteMatrix to match the local Obj2 System
             Tri:=@TriList.List[i];
             // the next function will check the given Triangle against only these ones that are close (using the octree of obj2)
             if TGLFreeForm(obj2).OctreeTriangleIntersect(Tri[0], Tri[1], Tri[2]) then begin
@@ -514,150 +494,30 @@ begin
             Inc(i, 3);
          end;
       finally
-        TriList.Free;
+         TriList.Free;
       end;
-    end else
-    begin //the stupid way just check all triangles
-      tl:=TGLFreeForm(obj1).MeshObjects.ExtractTriangles;
-      tl.TransformAsPoints(obj1.AbsoluteMatrix);
-      try
-        for i:=0 to (tl.count div 3)-1 do
-        begin
-          //in the list originally are the local coords, TransformAsPoints-> we have obj1 absolute coords
-          //here we pass absolute coords, then these are transformed with Obj2's AbsoluteMatrix to match the local Obj2 System
-          if TGLFreeForm(obj2).OctreeTriangleIntersect(
-            tl.Items[i*3],
-            tl.Items[i*3+1],
-            tl.Items[i*3+2]) then
-          begin
-            result:= true;
-            { TODO : Optimize, exit was disabled for performance checks }
-            exit;
-          end;
-        end;
-      finally
-        tl.Free;
-      end;
-    end;
-  end
-  else
-    result:= IntersectCubes(obj1, obj2); //FaceVsFace does work only for two FreeForm Objects
+   end else begin
+      // FaceVsFace does work only for two FreeForm Objects
+      Result:=IntersectCubes(obj1, obj2);
+   end;
 end;
 
-function IntersectCubes(Obj1, Obj2:TGLBaseSceneObject):boolean;
+// IntersectCubes (objects)
+//
+function IntersectCubes(obj1, obj2 : TGLBaseSceneObject) : Boolean;
 var
-  AABB1, AABB2: TAABB;
-  M1To2, M2To1: TMatrix;
+  aabb1, aabb2 : TAABB;
+  m1To2, m2To1 : TMatrix;
 begin
-  //Calc AABBs
-  AABB1:= Obj1.AxisAlignedBoundingBox;
-  AABB2:= Obj2.AxisAlignedBoundingBox;
-  //Calc Conversion Matrixes
-  MatrixMultiply(obj1.AbsoluteMatrix,obj2.InvAbsoluteMatrix,M1To2);
-  MatrixMultiply(obj2.AbsoluteMatrix,obj1.InvAbsoluteMatrix,M2To1);
+   // Calc AABBs
+   aabb1:=Obj1.AxisAlignedBoundingBox;
+   aabb2:=Obj2.AxisAlignedBoundingBox;
+   // Calc Conversion Matrixes
+   MatrixMultiply(obj1.AbsoluteMatrix, obj2.InvAbsoluteMatrix, m1To2);
+   MatrixMultiply(obj2.AbsoluteMatrix, obj1.InvAbsoluteMatrix, m2To1);
 
-  result:= IntersectCubes(AABB1, AABB2, M1To2, M2To1);
+   Result:=IntersectAABBs(aabb1, aabb2, m1To2, m2To1);
 end;
-
-function IntersectCubes(const AABB1, AABB2: TAABB;
-  const M1To2, M2To1: TMatrix):boolean;
-const
-  cWires : array[0..11,0..1] of Integer //Points of the wire
-         = ((0,1),(1,2),(2,3),(3,0),
-            (4,5),(5,6),(6,7),(7,4),
-            (0,4),(1,5),(2,6),(3,7));
-  cPlanes : array[0..5,0..3] of Integer //points of the planes
-         = ((1,2,6,5), (2,3,7,6), (0,1,2,3), (0,3,7,4), (0,1,5,4), (5,6,7,4));
-  procedure MakeAABBPoints(const AABB: TAABB; var pt:array of TVertex);
-  begin
-    pt[0] := AffineVectorMake(AABB.min[0], AABB.min[1], AABB.min[2]);
-    pt[1] := AffineVectorMake(AABB.max[0], AABB.min[1], AABB.min[2]);
-    pt[2] := AffineVectorMake(AABB.max[0], AABB.max[1], AABB.min[2]);
-    pt[3] := AffineVectorMake(AABB.min[0], AABB.max[1], AABB.min[2]);
-    pt[4] := AffineVectorMake(AABB.min[0], AABB.min[1], AABB.max[2]);
-    pt[5] := AffineVectorMake(AABB.max[0], AABB.min[1], AABB.max[2]);
-    pt[6] := AffineVectorMake(AABB.max[0], AABB.max[1], AABB.max[2]);
-    pt[7] := AffineVectorMake(AABB.min[0], AABB.max[1], AABB.max[2]);
-  end;
-  procedure MakePlanes(const pt:array of TVertex; var Planes:array of THmgPlane);
-  var
-    i:integer;
-  begin
-    for i:=0 to 5 do
-    begin
-      Planes[i]:= PlaneMake(pt[cPlanes[i,0]], pt[cPlanes[i,1]], pt[cPlanes[i,2]]);
-    end;
-  end;
-var
-  pt1, pt2: array[0..7] of TVertex;
-  pt:TVertex;
-  Planes2: array[0..5] of THmgPlane;
-  i, t: integer;
-  V: TVertex;
-  P: TVector;
-begin
-  result:= false;
-
-  //Build Points
-  MakeAABBPoints(AABB1, pt1);
-  MakeAABBPoints(AABB2, pt2);
-  for i:=0 to 7 do
-  begin
-    pt:= VectorTransform(pt2[i], M2To1);
-    //check for inclusion (points of Obj2 in Obj1)
-    if IsInRange(pt[0], AABB1.Min[0], AABB1.Max[0]) and
-      IsInRange(pt[1], AABB1.Min[1], AABB1.Max[1]) and
-      IsInRange(pt[2], AABB1.Min[2], AABB1.Max[2]) then
-    begin
-      result:= true;
-      exit;
-    end;
-  end;
-
-  for i:=0 to 7 do
-  begin
-    pt1[i]:= VectorTransform(pt1[i], M1To2);
-    //check for inclusion (points of Obj1 in Obj2)
-    if IsInRange(pt1[i][0], AABB2.Min[0], AABB2.Max[0]) and
-      IsInRange(pt1[i][1], AABB2.Min[1], AABB2.Max[1]) and
-      IsInRange(pt1[i][2], AABB2.Min[2], AABB2.Max[2]) then
-    begin
-      result:= true;
-      exit;
-    end;
-  end;
-
-  //Build Planes
-  MakePlanes(pt2, Planes2);
-
-  //Wire test
-  for i:=0 to 11 do
-  begin
-    for t:=0 to 5 do
-    begin
-      //Build Vector of Ray
-      V:= VectorSubtract(pt1[cWires[i,0]], pt1[cWires[i,1]]);
-      if IntersectLinePlane(VectorMake(pt1[cWires[i,0]]), VectorMake(V), Planes2[t], @P) = 1 then
-      begin
-        //check point in Wire
-        if IsInRange(P[0], pt1[cWires[i,0]][0], pt1[cWires[i,1]][0]) and
-          IsInRange(P[1], pt1[cWires[i,0]][1], pt1[cWires[i,1]][1]) and
-          IsInRange(P[2], pt1[cWires[i,0]][2], pt1[cWires[i,1]][2]) then
-        begin
-          //check point in Plane
-          if IsInRange(P[0], pt2[cPlanes[t, 0]][0], pt2[cPlanes[t, 2]][0]) and
-            IsInRange(P[1], pt2[cPlanes[t, 0]][1], pt2[cPlanes[t, 2]][1]) and
-            IsInRange(P[2], pt2[cPlanes[t, 0]][2], pt2[cPlanes[t, 2]][2]) then
-          begin
-            result:= true;
-            exit;
-          end;
-        end;
-      end;
-    end;
-  end;
-end;
-
 
 // ------------------
 // ------------------ TCollisionManager ------------------
