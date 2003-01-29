3ac51bdaba1f022f2ed5c96007e9fc63a21f949d
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-01-29 15:56:31 +0000

    Fixed NavMap initialization
    git-svn-id: http://svn.code.sf.net/p/glscene/code@825 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/utilities/HTFUtils/TerrainPack.dpr
+++ trunk/Demos/utilities/HTFUtils/TerrainPack.dpr
@@ -2,12 +2,14 @@ program TerrainPack;
 
 uses
   Forms,
-  FMainForm in 'FMainForm.pas' {MainForm};
+  FMainForm in 'FMainForm.pas' {MainForm},
+  FNavForm in 'FNavForm.pas' {NavForm};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(TNavForm, NavForm);
   Application.Run;
 end.
