b74c48ff3f4aa128eee120a96d60a8233fd32809
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-02-02 12:07:09 +0000

    Initial
    git-svn-id: http://svn.code.sf.net/p/glscene/code@858 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Source/FileFormats/FileOCT.pas
@@ -0,0 +1,111 @@
+{: FileOCT<p>
+
+	Loader for FSRad OCT files.<p>
+
+	<b>History : </b><font size=-1><ul>
+	   <li>30/01/03 - Egg - Creation
+	</ul></font>
+}
+unit FileOCT;
+
+interface
+
+uses Classes, Geometry;
+
+type
+
+   TOCTHeader = record
+		numVerts : Integer;
+		numFaces : Integer;
+		numTextures : Integer;
+		numLightmaps : Integer;
+		numLights : Integer;
+	end;
+
+	TOCTVertex = record
+		tv : TTexPoint;      // texture coordinates
+		lv : TTexpoint;      // lightmap coordinates
+		pos : TAffineVector; // vertex position
+   end;
+
+	TOCTFace = record
+		start : Integer;     // first face vert in vertex array
+		num : Integer;			// number of verts in the face
+		id : Integer;			// texture index into the texture array
+		lid : Integer;			// lightmap index into the lightmap array
+		p : THmgPlane;
+	end;
+   POCTFace = ^TOCTFace;
+
+	TOCTTexture = record
+		id : Integer;				      // texture id
+		Name : array [0..63] of Char;	// texture name
+   end;
+
+	TOCTLightmap = record
+		id : Integer;				         // lightmaps id
+		map : array [0..49151] of Byte;	// 128 x 128 raw RGB data
+   end;
+   POCTLightmap = ^TOCTLightmap;
+
+	TOCTLight = record
+		pos : TAffineVector;		      // Position
+		color : TAffineVector;			// Color (RGB)
+		intensity : Integer;			   // Intensity
+	end;
+
+   // TOCTFile
+   //
+   TOCTFile = class (TObject)
+      public
+         { Public Declarations }
+         Header         : TOCTHeader;
+         Vertices       : array of TOCTVertex;
+         Faces          : array of TOCTFace;
+         Textures       : array of TOCTTexture;
+         Lightmaps      : array of TOCTLightmap;
+         Lights         : array of TOCTLight;
+
+         constructor Create(octStream : TStream);
+   end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+implementation
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+uses SysUtils;
+
+// ------------------
+// ------------------ TOCTFile ------------------
+// ------------------
+
+// Create
+//
+constructor TOCTFile.Create(octStream : TStream);
+begin
+   // Read in the header
+   octStream.Read(Header, SizeOf(header));
+
+   // then the rest of the stuff
+
+   SetLength(Vertices, Header.numVerts);
+   octStream.Read(Vertices[0], Header.numVerts*SizeOf(TOCTVertex));
+
+   SetLength(Faces, Header.numFaces);
+   octStream.Read(Faces[0], Header.numFaces*SizeOf(TOCTFace));
+
+   SetLength(Textures, Header.numTextures);
+   octStream.Read(Textures[0], Header.numTextures*SizeOf(TOCTTexture));
+
+   SetLength(Lightmaps, Header.numLightmaps);
+   octStream.Read(Lightmaps[0], Header.numLightmaps*SizeOf(TOCTLightmap));
+
+   SetLength(Lights, Header.numLights);
+   octStream.Read(Lights[0], Header.numLights*SizeOf(TOCTLight));
+end;
+
+end.
---
+++ trunk/Source/FileFormats/GLFileOCT.pas
@@ -0,0 +1,132 @@
+{: GLFileOCT<p>
+
+    Support-code to load OCT Files into TGLFreeForm-Components in GLScene.<br>
+    (OCT being the format output from FSRad, http://www.fluidstudios.com/fsrad.html).<p>
+
+	<b>History : </b><font size=-1><ul>
+      <li>02/02/03 - EG - Creation
+   </ul><p>
+}
+unit GLFileOCT;
+
+interface
+
+uses Classes, GLVectorFileObjects, GLMisc, Geometry;
+
+type
+
+   // TGLOCTVectorFile
+   //
+   {: The OCT vector file (FSRad output).<p> }
+   TGLOCTVectorFile = class(TVectorFile)
+      public
+         { Public Declarations }
+         class function Capabilities : TDataFileCapabilities; override;
+
+         procedure LoadFromStream(aStream: TStream); override;
+   end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+implementation
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+uses SysUtils, FileOCT, GLTexture, Graphics;
+
+// ------------------
+// ------------------ TGLOCTVectorFile ------------------
+// ------------------
+
+// Capabilities
+//
+class function TGLOCTVectorFile.Capabilities : TDataFileCapabilities;
+begin
+   Result:=[dfcRead];
+end;
+
+// LoadFromStream
+//
+procedure TGLOCTVectorFile.LoadFromStream(aStream : TStream);
+var
+   i, y, n : Integer;
+   oct : TOCTFile;
+   octFace : POCTFace;
+   octLightmap : POCTLightmap;
+   mo : TMeshObject;
+   fg : TFGVertexIndexList;
+   lightmapLib : TGLMaterialLibrary;
+   lightmapBmp : TBitmap;
+   libMat : TGLLibMaterial;
+begin
+   oct:=TOCTFile.Create(aStream);
+   try
+      mo:=TMeshObject.CreateOwned(Owner.MeshObjects);
+      mo.Mode:=momFaceGroups;
+
+      lightmapLib:=Owner.LightmapLibrary;
+      if Assigned(lightmapLib) then begin
+         // import lightmaps
+         n:=oct.Header.numLightmaps;
+         lightmapBmp:=TBitmap.Create;
+         try
+            lightmapBmp.PixelFormat:=pf24bit;
+            lightmapBmp.Width:=128;
+            lightmapBmp.Height:=128;
+            for i:=0 to n-1 do begin
+               octLightmap:=@oct.Lightmaps[i];
+               // convert RAW RGB to BMP
+               for y:=0 to 127 do
+                  Move(octLightmap.map[y*128*3], lightmapBmp.ScanLine[127-y]^, 128*3);
+               // spawn lightmap
+               libMat:=lightmapLib.AddTextureMaterial(IntToStr(i), lightmapBmp);
+               with libMat.Material.Texture do begin
+                  TextureWrap:=twNone;
+               end;
+            end;
+         finally
+            lightmapBmp.Free;
+         end;
+      end;
+
+      // import geometry
+      n:=oct.Header.numVerts;
+
+      mo.Vertices.AdjustCapacityToAtLeast(n);
+      mo.TexCoords.AdjustCapacityToAtLeast(n);
+      mo.LighmapTexCoords.AdjustCapacityToAtLeast(n);
+      for i:=0 to n-1 do with oct.Vertices[i] do begin
+         mo.Vertices.Add(-pos[0], pos[1], pos[2]);
+         mo.TexCoords.Add(tv.s, tv.t);
+         mo.LighmapTexCoords.Add(lv.s, lv.t);
+      end;
+      // import faces
+      n:=oct.Header.numFaces;
+      for i:=0 to n-1 do begin
+         octFace:=@oct.Faces[i];
+         fg:=TFGVertexIndexList.CreateOwned(mo.FaceGroups);
+         fg.Mode:=fgmmTriangleFan;
+         fg.VertexIndices.Add(octFace.start);
+         fg.VertexIndices.AddSerie(octFace.start+octFace.num-1, -1, octFace.num-1);
+         if Assigned(lightmapLib) then
+            fg.LightMapIndex:=octFace.lid;
+      end;
+
+   finally
+      oct.Free;
+   end;
+end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+initialization
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+   RegisterVectorFileFormat('oct', 'FSRad OCT files', TGLOCTVectorFile);
+
+end.
