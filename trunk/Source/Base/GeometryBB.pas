df657e407f8ec2b55db1152ff83d5a900cf4223e
danbartlett <danbartlett@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-02-13 16:19:41 +0000

    Faster IntersectAABBsAbsoluteXY function
    git-svn-id: http://svn.code.sf.net/p/glscene/code@911 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Base/GeometryBB.pas
+++ trunk/Source/Base/GeometryBB.pas
@@ -428,50 +428,12 @@ end;
 // IntersectAABBsAbsoluteXY (AABBs)
 //
 function IntersectAABBsAbsoluteXY(const aabb1, aabb2 : TAABB) : Boolean;
-
-   procedure MakeAABBPoints(const AABB : TAABB; var pt : array of TVertex);
-   begin
-      with AABB do begin
-         SetVector(pt[0], min[0], min[1], min[2]);
-         SetVector(pt[1], max[0], min[1], min[2]);
-         SetVector(pt[2], max[0], max[1], min[2]);
-         SetVector(pt[3], min[0], max[1], min[2]);
-      end;
-   end;
-
-var
-  pt1, pt2: array[0..3] of TVertex;
-  pt:TVertex;
-  i: integer;
 begin
   result:= false;
 
-  //Build Points
-  MakeAABBPoints(AABB1, pt1);
-  MakeAABBPoints(AABB2, pt2);
-  for i:=0 to 3 do
-  begin
-    pt:= pt2[i];
-    //check for inclusion (points of Obj2 in Obj1)
-    if IsInRange(pt[0], AABB1.Min[0], AABB1.Max[0]) and
-      IsInRange(pt[1], AABB1.Min[1], AABB1.Max[1]) then
-    begin
-      result:= true;
-      exit;
-    end;
-  end;
-
-  for i:=0 to 3 do
-  begin
-    pt:=pt1[i];
-    //check for inclusion (points of Obj1 in Obj2)
-    if IsInRange(pt[0], AABB2.Min[0], AABB2.Max[0]) and
-      IsInRange(pt[1], AABB2.Min[1], AABB2.Max[1]) then
-    begin
-      result:= true;
-      exit;
-    end;
-  end;
+  if (AABB2.min[0]>AABB1.max[0])or (AABB2.min[1]>AABB1.max[1]) then Exit
+  else if (AABB2.max[0]<AABB1.min[0])or (AABB2.max[1]<AABB1.min[1]) then Exit
+  else Result:=true;
 
 end;
 
