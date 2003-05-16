5b344bd4dc98610897b826f7060ace9b00c037f5
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-05-16 15:46:35 +0000

    Initial
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1018 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Source/Base/GLTextureCombiners.pas
@@ -0,0 +1,231 @@
+{: GLTextureCombiners<p>
+
+   Texture combiners setup utility functions.<p>
+
+   <b>History : </b><font size=-1><ul>
+      <li>16/05/03 - EG - Creation
+   </ul></font>
+}
+unit GLTextureCombiners;
+
+interface
+
+{: Parses a TC text description and setups combiners accordingly.<p>
+   *experimental*<br>
+   Knowledge of texture combiners is a requirement<br>
+   Syntax: pascal-like, one instruction per line, use '//' for comment.<p>
+
+   Examples:<ul>
+   <li>Tex1:=Tex0;   // replace texture 1 with texture 0
+   <li>Tex1:=Tex0+Tex1; // additive blending between textures 0 and 1
+   <li>Tex1:=Tex0-Tex1; // subtractive blending between textures 0 and 1
+   <li>Tex1:=Tex0*Tex1; // modulation between textures 0 and 1
+   <li>Tex1:=Tex0+Tex1-0.5; // signed additive blending between textures 0 and 1
+   <li>Tex1:=Interpolate(Tex0, Tex1, PrimaryColor); // interpolation between textures 0 and 1 using primary color as factor
+   <li>Tex1:=Dot3(Tex0, Tex1); // dot3 product between textures 0 and 1
+   </ul><p>
+
+   Accepted tokens:<ul>
+   <li>Tex0, Tex1, etc. : texture unit
+   <li>PrimaryColor, Col : the primary color
+   <li>ConstantColor, EnvCol : texture environement constant color
+   </ul>
+}
+procedure SetupTextureCombiners(const tcCode : String);
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+implementation
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+uses OpenGL12;
+
+// RemoveSpaces
+//
+function RemoveSpaces(const str : String) : String;
+var
+   c : Char;
+   i, p, n : Integer;
+begin
+   n:=Length(str);
+   SetLength(Result, n);
+   p:=1;
+   for i:=1 to n do begin
+      c:=str[i];
+      if c<>' ' then begin
+         Result[p]:=c;
+         Inc(p);
+      end;
+   end;
+   SetLength(Result, p-1);
+end;
+
+// ProcessTextureCombinerArgument
+//
+procedure ProcessTextureCombinerArgument(const arg : String; sourceEnum, operandEnum : Integer;
+                                         const dest : String);
+var
+   sourceValue, operandValue : Integer;
+begin
+   sourceValue:=0;
+   operandValue:=GL_SRC_COLOR;
+   if (arg='tex') or (arg=dest) then
+      sourceValue:=GL_TEXTURE
+   else if ((arg='tex0') and (dest='tex1')) or ((arg='tex1') and (dest='tex2'))
+        or ((arg='tex2') and (dest='tex3')) then
+      sourceValue:=GL_PREVIOUS_ARB
+   else if (arg='col') or (arg='col0') or (arg='primarycolor') then
+      sourceValue:=GL_PRIMARY_COLOR_ARB
+   else if (arg='envcol') or (arg='constcol') or (arg='constantcolor') then
+      sourceValue:=GL_CONSTANT_COLOR_ARB
+   else if Copy(arg, 1, 3)='tex' then begin
+      Assert(GL_ARB_texture_env_crossbar or GL_NV_texture_env_combine4,
+             'Requires GL_ARB_texture_env_crossbar or NV_texture_env_combine4');
+      sourceValue:=GL_TEXTURE0_ARB+StrToInt(Copy(arg, 4, MaxInt));
+   end;
+   Assert(sourceValue>0, 'invalid argument : "'+arg+'"');
+   glTexEnvf(GL_TEXTURE_ENV, sourceEnum , sourceValue);
+   glTexEnvf(GL_TEXTURE_ENV, operandEnum, operandValue);
+   CheckOpenGLError;
+end;
+
+// ProcessTextureCombinerLine
+//
+procedure ProcessTextureCombinerLine(const tcLine : String);
+var
+   line, dest, arg1, arg2, arg3, funcname : String;
+   p : Integer;
+   destEnum, operEnum : Integer;
+   sourceBaseEnum, operandBaseEnum : Integer;
+   sl : TStrings;
+begin
+   // initial filtering
+   line:=LowerCase(RemoveSpaces(Trim(tcLine)));
+   if Copy(line, 1, 2)='//' then Exit;
+   if line='' then Exit;
+   if line[Length(line)]=';' then begin
+      line:=Trim(Copy(line, 1, Length(line)-1));
+      if line='' then Exit;
+   end;
+   // Parse destination
+   p:=Pos(':=', line);
+   dest:=Copy(line, 1, p-1);
+   line:=Copy(line, p+2, MaxInt);
+   p:=Pos('.', dest);
+   destEnum:=GL_COMBINE_RGB_ARB;
+   sourceBaseEnum:=GL_SOURCE0_RGB_ARB;
+   operandBaseEnum:=GL_OPERAND0_RGB_ARB;
+   if p>0 then begin
+      if Copy(dest, p+1, 1)='a' then begin
+         destEnum:=GL_COMBINE_ALPHA_ARB;
+         sourceBaseEnum:=GL_SOURCE0_ALPHA_ARB;
+         operandBaseEnum:=GL_OPERAND0_ALPHA_ARB;
+      end;
+      dest:=Copy(dest, 1, p-1);
+   end;
+   if dest='tex0' then
+      glActiveTextureARB(GL_TEXTURE0_ARB)
+   else if dest='tex1' then
+      glActiveTextureARB(GL_TEXTURE1_ARB)
+   else Assert(False, 'invalid destination texture unit');
+   // parse combiner operator
+   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE_ARB);
+   CheckOpenGLError;
+   operEnum:=0;
+   arg1:=''; arg2:=''; arg3:='';
+   p:=Pos('+', line);
+   if p>0 then begin
+      // ADD & ADD_SIGNED operators
+      if Copy(line, Length(line)-4, 4)='-0.5' then
+         operEnum:=GL_ADD
+      else operEnum:=GL_ADD_SIGNED_ARB;
+      arg1:=Copy(line, 1, p-1);
+      arg2:=Copy(line, p+1, MaxInt);
+   end;
+   p:=Pos('*', line);
+   if p>0 then begin
+      // MODULATE operator
+      operEnum:=GL_MODULATE;
+      arg1:=Copy(line, 1, p-1);
+      arg2:=Copy(line, p+1, MaxInt);
+      line:='';
+   end;
+   p:=Pos('(', line);
+   if p>0 then begin
+      // function
+      sl:=TStringList.Create;
+      try
+         funcName:=Copy(line, 1, p-1);
+         p:=Pos('(', line);
+         line:=Copy(line, p+1, MaxInt);
+         p:=Pos(')', line);
+         sl.CommaText:=Copy(line, 1, p-1);
+         if funcName='interpolate' then begin
+            // INTERPOLATE operator
+            Assert(sl.Count=3, 'Invalid parameter count');
+            operEnum:=GL_INTERPOLATE_ARB;
+            arg1:=sl[0];
+            arg2:=sl[1];
+            arg3:=sl[2];
+         end else if funcName='dot3' then begin
+            // DOT3 operator
+            Assert(sl.Count=2, 'Invalid parameter count');
+            Assert(GL_ARB_texture_env_dot3, 'Requires GL_ARB_texture_env_dot3');
+            operEnum:=GL_DOT3_RGB_ARB;
+            arg1:=sl[0];
+            arg2:=sl[1];
+         end;
+      finally
+         sl.Free;
+      end;
+      line:='';
+   end;
+   p:=Pos('-', line);
+   if p>0 then begin
+      // SUBTRACT operator
+      operEnum:=GL_SUBTRACT_ARB;
+      arg1:=Copy(line, 1, p-1);
+      arg2:=Copy(line, p+1, MaxInt);
+      line:='';
+   end;
+   if operEnum=0 then begin
+      // REPLACE by default
+      operEnum:=GL_REPLACE;
+      arg1:=line;
+   end;
+
+   glTexEnvi(GL_TEXTURE_ENV, destEnum, operEnum);
+   CheckOpenGLError;
+   // parse arguments
+   if arg1<>'' then
+      ProcessTextureCombinerArgument(arg1, sourceBaseEnum, operandBaseEnum, dest);
+   if arg2<>'' then
+      ProcessTextureCombinerArgument(arg2, sourceBaseEnum+1, operandBaseEnum+1, dest);
+   if arg3<>'' then
+      ProcessTextureCombinerArgument(arg3, sourceBaseEnum+2, operandBaseEnum+2, dest);
+
+   glActiveTextureARB(GL_TEXTURE0_ARB);
+end;
+
+// SetupTextureCombiners
+//
+procedure SetupTextureCombiners(const tcCode : String);
+var
+   i : Integer;
+   sl : TStringList;
+begin
+   Assert(GL_ARB_texture_env_combine, 'Requires GL_ARB_texture_env_combine support');
+   sl:=TStringList.Create;
+   try
+      sl.Text:=tcCode;
+      for i:=0 to sl.Count-1 do
+         ProcessTextureCombinerLine(sl[i]);
+   finally
+      sl.Free;
+   end;
+end;
+
+end.
