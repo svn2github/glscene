defd3b1c3e5c81d1f75a9eb324507bb8148bf600
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-06-05 14:33:49 +0000

    TDataFile moved in from GLMisc
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1120 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Base/ApplicationFileIO.pas
+++ trunk/Source/Base/ApplicationFileIO.pas
@@ -3,8 +3,9 @@
    Allows re-routing file reads to reads from a single archive file f.i.<p>
 
 	<b>History : </b><font size=-1><ul>
-      <li>31/01/03 - Egg - Added FileExists mechanism
-	   <li>21/11/02 - Egg - Creation
+      <li>05/06/03 - EG - TDataFile moved in from GLMisc
+      <li>31/01/03 - EG - Added FileExists mechanism
+	   <li>21/11/02 - EG - Creation
 	</ul></font>
 }
 unit ApplicationFileIO;
@@ -62,6 +63,58 @@ type
          {: Event that allows you to specify if a stream for the file exists.<p> }
          property OnFileStreamExists : TAFIOFileStreamExistsEvent read FOnFileStreamExists write FOnFileStreamExists;
 	end;
+
+   // TDataFileCapabilities
+   //
+   TDataFileCapability = (dfcRead, dfcWrite);
+   TDataFileCapabilities = set of TDataFileCapability;
+
+   // TDataFile
+   //
+   {: Abstract base class for data file formats interfaces.<p>
+      This class declares base file-related behaviours, ie. ability to load/save
+      from a file or a stream.<p>
+      It is highly recommended to overload ONLY the stream based methods, as the
+      file-based one just call these, and stream-based behaviours allow for more
+      enhancement (such as other I/O abilities, compression, cacheing, etc.)
+      to this class, without the need to rewrite subclasses. }
+   TDataFile = class (TPersistent)
+      private
+         { Private Declarations }
+         FOwner : TPersistent;
+         FResourceName : String;
+
+      protected
+         { Protected Declarations }
+         function GetOwner : TPersistent; override;
+
+      public
+         { Public Declarations }
+	      constructor Create(AOwner: TPersistent);
+         destructor Destroy; override;
+
+         {: Describes what the TDataFile is capable of.<p>
+            Default value is [dfcRead]. }
+         class function Capabilities : TDataFileCapabilities; virtual;
+
+         {: Duplicates Self and returns a copy.<p>
+            Subclasses should override this method to duplicate their data. }
+         function CreateCopy(AOwner: TPersistent) : TDataFile; dynamic;
+
+         procedure LoadFromFile(const fileName : String); dynamic;
+         procedure SaveToFile(const fileName : String); dynamic;
+         procedure LoadFromStream(stream : TStream); dynamic; abstract;
+         procedure SaveToStream(stream : TStream); dynamic;
+
+         {: Optionnal resource name.<p>
+            When using LoadFromFile/SaveToFile, the filename is placed in it,
+            when using the Stream variants, the caller may place the resource
+            name in it for parser use. }
+         property ResourceName : String read FResourceName write FResourceName;
+   end;
+
+   TDataFileClass = class of TDataFile;
+
 
 {: Creates a file stream corresponding to the fileName.<p>
    If the file does not exists, an exception will be triggered.<br>
@@ -141,6 +194,85 @@ destructor TApplicationFileIO.Destroy;
 begin
    vAFIO:=nil;
 	inherited Destroy;
+end;
+
+// ------------------
+// ------------------ TDataFile ------------------
+// ------------------
+
+// Create
+//
+constructor TDataFile.Create(AOwner: TPersistent);
+begin
+   inherited Create;
+   FOwner:=AOwner;
+end;
+
+// Destroy
+//
+destructor TDataFile.Destroy;
+begin
+   inherited;
+end;
+
+// Capabilities
+//
+class function TDataFile.Capabilities : TDataFileCapabilities;
+begin
+   Result:=[dfcRead];
+end;
+
+// GetOwner
+//
+function TDataFile.GetOwner : TPersistent;
+begin
+   Result:=FOwner;
+end;
+
+// CreateCopy
+//
+function TDataFile.CreateCopy(AOwner: TPersistent) : TDataFile;
+begin
+   if Self<>nil then
+      Result:=TDataFileClass(Self.ClassType).Create(AOwner)
+   else Result:=nil;
+end;
+
+// LoadFromFile
+//
+procedure TDataFile.LoadFromFile(const fileName : String);
+var
+   fs : TStream;
+begin
+   ResourceName:=ExtractFileName(fileName);
+   fs:=CreateFileStream(fileName, fmOpenRead+fmShareDenyNone);
+   try
+      LoadFromStream(fs);
+   finally
+      fs.Free;
+   end;
+end;
+
+// SaveToFile
+//
+procedure TDataFile.SaveToFile(const fileName : String);
+var
+   fs : TStream;
+begin
+   ResourceName:=ExtractFileName(fileName);
+   fs:=CreateFileStream(fileName, fmCreate);
+   try
+      SaveToStream(fs);
+   finally
+      fs.Free;
+   end;
+end;
+
+// SaveToStream
+//
+procedure TDataFile.SaveToStream(stream : TStream);
+begin
+   Assert(False, 'Export for '+ClassName+' not available.');
 end;
 
 end.
