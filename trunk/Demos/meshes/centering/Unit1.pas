016b62d553831ae62b55449959225fa1116fd9b3
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-06-05 02:25:05 +0000

    Added GLFile3DS to uses clause.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1108 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/meshes/centering/Unit1.pas
+++ trunk/Demos/meshes/centering/Unit1.pas
@@ -11,7 +11,7 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   GLScene, GLVectorFileObjects, GLObjects, GLMisc, ComCtrls, StdCtrls,
-  GLWin32Viewer;
+  GLWin32Viewer, GLFile3DS;
 
 type
   TForm1 = class(TForm)
