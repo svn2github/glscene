7050ab0a59fb77c976848fd0cc32abe6f874ffcf
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-06-06 16:23:47 +0000

    Basic shadow casting operationnal (barely tested)
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1131 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLShadowVolume.pas
+++ trunk/Source/GLShadowVolume.pas
@@ -123,15 +123,18 @@ implementation
 //-------------------------------------------------------------
 //-------------------------------------------------------------
 
-uses SysUtils;
+uses SysUtils, VectorLists;
 
 // ------------------
 // ------------------ TGLShadowVolumeCaster ------------------
 // ------------------
 
+// Create
+//
 constructor TGLShadowVolumeCaster.Create(Collection: TCollection);
 begin
-   ..
+   inherited Create(Collection);
+   FCapping:=svcAlways;
 end;
 
 // Assign
@@ -254,46 +257,6 @@ begin
    inherited;
 end;
 
-// DoRender
-//
-procedure TGLShadowVolume.DoRender(var rci : TRenderContextInfo;
-                                   renderSelf, renderChildren : Boolean);
-var
-   i : Integer;
-   obj : TGLBaseSceneObject;
-   caster : TGLShadowVolumeCaster;
-   lights, opaques : TList;
-begin
-   if FRendering then Exit;
-   lights:=TList.Create;
-   opaques:=TList.Create;
-   FRendering:=True;
-   try
-      // collect visible/shining casters
-      for i:=0 to Casters.Count-1 do begin
-         caster:=Casters[i];
-         obj:=caster.Caster;
-         if     Assigned(obj)
-            and ((caster.EffectiveRadius<=0)
-                 or (obj.DistanceTo(rci.cameraPosition)<caster.EffectiveRadius)) then begin
-            if obj is TGLLightSource then begin
-               if TGLLightSource(obj).Shining then
-                  lights.Add(obj)
-            end else begin
-               if obj.Visible then
-                  opaques.Add(obj);
-            end;
-         end;
-      end;
-      // render
-      // ...
-   finally
-      FRendering:=False;
-      opaques.Free;
-      lights.Free;
-   end;
-end;
-
 // Notification
 //
 procedure TGLShadowVolume.Notification(AComponent: TComponent; Operation: TOperation);
@@ -313,6 +276,233 @@ begin
       StructureChanged;
    end;
    inherited Assign(Source);
+end;
+
+// DoRender
+//
+procedure TGLShadowVolume.DoRender(var rci : TRenderContextInfo;
+                                   renderSelf, renderChildren : Boolean);
+var
+   i, j, k, n : Integer;
+   lightSource : TGLLightSource;
+   sil : TGLBaseSilhouette;
+   lightID : Cardinal;
+   obj : TGLBaseSceneObject;
+   caster : TGLShadowVolumeCaster;
+   lights, opaques, opaqueCapping : TList;
+   silParams : TGLSilhouetteParameters;
+   extrudedVertices : TVectorList;
+   mat : TMatrix;
+begin
+   if FRendering then Exit;
+   if not (renderSelf or renderChildren) then Exit;
+   lights:=TList.Create;
+   opaques:=TList.Create;
+   opaqueCapping:=TList.Create;
+   extrudedVertices:=TVectorList.Create;
+   FRendering:=True;
+   try
+      // collect visible/shining casters
+      for i:=0 to Casters.Count-1 do begin
+         caster:=Casters[i];
+         obj:=caster.Caster;
+         if     Assigned(obj)
+            and ((caster.EffectiveRadius<=0)
+                 or (obj.DistanceTo(rci.cameraPosition)<caster.EffectiveRadius)) then begin
+            if obj is TGLLightSource then begin
+               if TGLLightSource(obj).Shining then
+                  lights.Add(obj)
+            end else begin
+               if obj.Visible then begin
+                  opaques.Add(obj);
+                  opaqueCapping.Add(Pointer(caster.Capping=svcAlways));
+               end;
+            end;
+         end;
+      end;
+      // render the shadow volumes
+      glPushAttrib(GL_ENABLE_BIT);
+
+      // first turn of all the shadow casting lights diffuse and specular
+      for i:=0 to lights.Count-1 do begin
+         lightID:=TGLLightSource(lights[i]).LightID;
+         glLightfv(lightID, GL_DIFFUSE, @NullHmgVector);
+         glLightfv(lightID, GL_SPECULAR, @NullHmgVector);
+      end;
+      // render shadow receivers with ambient lighting
+      Self.RenderChildren(0, Count-1, rci);
+
+      glDepthMask(False);
+      rci.ignoreBlendingRequests:=True;
+
+      glEnable(GL_STENCIL_TEST);
+      glEnable(GL_DEPTH_TEST);
+
+      // turn off *all* lights (they'll be reenabled by the PopAttrib)
+      glGetIntegerv(GL_MAX_LIGHTS, @n);
+      for i:=0 to n-1 do
+         glDisable(GL_LIGHT0+i);
+
+      // render contribution of all shadow casting lights
+      for i:=0 to lights.Count-1 do begin
+         lightSource:=TGLLightSource(lights[i]);
+         lightID:=lightSource.LightID;
+
+         SetVector(silParams.LightDirection, lightSource.SpotDirection.DirectVector);
+         case lightSource.LightStyle of
+            lsParallel : silParams.Style:=ssParallel
+         else
+            silParams.Style:=ssOmni;
+         end;
+         silParams.CappingRequired:=True;
+
+         // clear the stencil and prepare for shadow volume pass
+         glClear(GL_STENCIL_BUFFER_BIT);
+         glStencilFunc(GL_ALWAYS, 0, 0);
+         glDepthFunc(GL_LESS);
+
+{         glColor3f(0, 0, 1);
+         glEnable(GL_BLEND);
+         glBlendFunc(GL_SRC_ALPHA, GL_ONE); }
+
+         SetGLState(rci.currentStates, stCullFace);
+
+         glColorMask(False, False, False, False);
+         glDisable(GL_BLEND);
+         glDisable(GL_LIGHTING);
+
+         // for all opaque shadow casters
+         for k:=0 to opaques.Count-1 do begin
+            obj:=TGLBaseSceneObject(opaques[k]);
+
+            SetVector(silParams.SeenFrom, obj.AbsoluteToLocal(lightSource.AbsolutePosition));
+
+            sil:=obj.GenerateSilhouette(silParams);
+            try
+               glPushMatrix;
+
+               mat:=obj.AbsoluteMatrix;
+               glMultMatrixf(@mat);
+
+               // extrude vertices to infinity
+               extrudedVertices.Count:=sil.Vertices.Count;
+               for n:=0 to extrudedVertices.Count-1 do begin
+                  VectorSubtract(sil.Vertices.List[n], silParams.SeenFrom,
+                                 extrudedVertices.List[n]);
+                  extrudedVertices.List[n][3]:=0;
+               end;
+
+               if Boolean(opaqueCapping[k]) then begin
+                  // z-fail
+                  glCullFace(GL_FRONT);
+                  glStencilOp(GL_KEEP, GL_INCR, GL_KEEP);
+
+                  glBegin(GL_QUADS);
+                  n:=0; while n<sil.Indices.Count do begin
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n]]);
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n]]);
+                     Inc(n, 2);
+                  end;
+                  glEnd;
+                  glBegin(GL_TRIANGLES);
+                  for n:=sil.CapIndices.Count-1 downto 0 do
+                     glVertex3fv(@sil.Vertices.List[sil.CapIndices[n]]);
+                  glEnd;
+                  glBegin(GL_TRIANGLES);
+                  for n:=0 to sil.CapIndices.Count-1 do
+                     glVertex4fv(@extrudedVertices.List[sil.CapIndices[n]]);
+                  glEnd;
+
+                  glCullFace(GL_BACK);
+                  glStencilOp(GL_KEEP, GL_DECR, GL_KEEP);
+
+                  glBegin(GL_QUADS);
+                  n:=0; while n<sil.Indices.Count do begin
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n]]);
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n]]);
+                     Inc(n, 2);
+                  end;
+                  glEnd;
+                  glBegin(GL_TRIANGLES);
+                  for n:=sil.CapIndices.Count-1 downto 0 do
+                     glVertex3fv(@sil.Vertices.List[sil.CapIndices[n]]);
+                  glEnd;
+                  glBegin(GL_TRIANGLES);
+                  for n:=0 to sil.CapIndices.Count-1 do
+                     glVertex4fv(@extrudedVertices.List[sil.CapIndices[n]]);
+                  glEnd;
+               end else begin
+                  // z-pass
+                  glCullFace(GL_FRONT);
+                  glStencilOp(GL_KEEP, GL_KEEP, GL_INCR);
+
+                  glBegin(GL_QUADS);
+                  n:=0; while n<sil.Indices.Count do begin
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n]]);
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n]]);
+                     Inc(n, 2);
+                  end;
+                  glEnd;
+
+                  glCullFace(GL_BACK);
+                  glStencilOp(GL_KEEP, GL_KEEP, GL_DECR);
+
+                  glBegin(GL_QUADS);
+                  n:=0; while n<sil.Indices.Count do begin
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n]]);
+                     glVertex3fv(@sil.Vertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n+1]]);
+                     glVertex4fv(@extrudedVertices.List[sil.Indices[n]]);
+                     Inc(n, 2);
+                  end;
+                  glEnd;
+               end;
+               glPopMatrix;
+            finally
+               sil.Free;
+            end;
+         end;
+
+         glColorMask(True, True, True, True);
+         glDepthFunc(GL_EQUAL);
+         glStencilFunc(GL_EQUAL, 0, 255);
+         glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+
+         glEnable(GL_BLEND);
+         glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+         // re-enable light's diffuse and specular, but no ambient
+         glEnable(lightID);
+         glLightfv(lightID, GL_AMBIENT, @NullHmgVector);
+         glLightfv(lightID, GL_DIFFUSE, lightSource.Diffuse.AsAddress);
+         glLightfv(lightID, GL_SPECULAR, lightSource.Specular.AsAddress);
+         glEnable(GL_LIGHTING);
+
+         glCullFace(GL_BACK);
+
+         Self.RenderChildren(0, Count-1, rci);
+
+         glDisable(lightID);
+      end;
+
+      // restore OpenGL state
+      glPopAttrib;
+      glDepthMask(True);
+      glDepthFunc(GL_LESS);
+      rci.ignoreBlendingRequests:=False;
+   finally
+      FRendering:=False;
+      extrudedVertices.Free;
+      opaques.Free;
+      opaqueCapping.Free;
+      lights.Free;
+   end;
 end;
 
 end.
