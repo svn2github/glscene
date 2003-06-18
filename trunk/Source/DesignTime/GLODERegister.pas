4dc1d70fb7007647bd4092ac979379cc05891be9
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-06-18 05:38:13 +0000

    Added
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1265 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Source/DesignTime/GLODERegister.pas
@@ -0,0 +1,65 @@
+{
+  GLODERegister - Design time registration code for the ODE Manager
+
+  History:
+
+    18/06/03 - SG - Creation.
+}
+unit GLODERegister;
+
+interface
+
+uses
+  Classes, GLODEManager, GLSceneRegister, XCollection;
+
+procedure Register;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+implementation
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+// Register
+//
+procedure Register;
+begin
+  RegisterClasses([TGLODEManager, TGLODEJointList,
+                   TGLODEDummy, TGLODEPlane,
+                   TODEJoints, TODEElements]);
+  RegisterComponents('GLScene',[TGLODEManager,TGLODEJointList]);
+end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+initialization
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+  RegisterXCollectionItemClass(TGLODEDynamicBehaviour);
+
+  with ObjectManager do begin
+    RegisterSceneObject(TGLODEDummy,'GLODEDummy','ODE Objects');
+    RegisterSceneObject(TGLODEPlane,'GLODEPlane','ODE Objects');
+  end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+finalization
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+  UnregisterXCollectionItemClass(TGLODEDynamicBehaviour);
+
+  with ObjectManager do begin
+    UnregisterSceneObject(TGLODEDummy);
+    UnregisterSceneObject(TGLODEPlane);
+  end;
+
+end.
