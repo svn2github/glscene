b723d8d54ee5d1c35d4a4d64d922e028dfed2d67
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-07-21 14:01:00 +0000

    Initial, from GLObjects split
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1689 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Source/GLPolyhedron.pas
@@ -0,0 +1,59 @@
+// GLPolyhedron
+{: Standard polyhedrons.<p>
+
+	<b>History : </b><font size=-1><ul>
+      <li>21/07/03 - EG - Creation from GLObjects split
+   </ul></font>
+}
+unit GLPolyhedron;
+
+interface
+
+uses Classes, GLScene, GLTexture, Geometry, OpenGL12, GLMisc;
+
+type
+
+   // TGLDodecahedron
+   //
+   {: A Dodecahedron.<p>
+      The dodecahedron has no texture coordinates defined, ie. without using
+      a texture generation mode, no texture will be mapped. }
+   TGLDodecahedron = class(TGLSceneObject)
+      public
+			{ Public Declarations }
+         procedure BuildList(var rci : TRenderContextInfo); override;
+   end;
+
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+implementation
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+
+uses GLObjects;
+
+// ------------------
+// ------------------ TGLDodecahedron ------------------
+// ------------------
+
+// BuildList
+//
+procedure TGLDodecahedron.BuildList(var rci : TRenderContextInfo);
+begin
+   DodecahedronBuildList;
+end;
+
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+initialization
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+
+   RegisterClasses([TGLDodecahedron]);
+
+end.
+
