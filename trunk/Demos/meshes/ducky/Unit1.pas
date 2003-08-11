ec1e31b52a6145010f0dea99ae6bffc971d2105b
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-08-11 07:33:14 +0000

    Nurbs duck
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1779 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Demos/meshes/ducky/Unit1.pas
@@ -0,0 +1,89 @@
+{: Loading NURBS into a GLScene FreeForm object<p>
+
+   The bottom of his beak is wound wrong, I'll sort that out
+   soon.<p>
+}
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, GLScene, GLVectorFileObjects, GLObjects, GLWin32Viewer, GLMisc,
+  ExtCtrls, ComCtrls, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    GLScene1: TGLScene;
+    GLCamera1: TGLCamera;
+    GLDummyCube1: TGLDummyCube;
+    GLFreeForm1: TGLFreeForm;
+    GLLightSource1: TGLLightSource;
+    Panel1: TPanel;
+    GLSceneViewer1: TGLSceneViewer;
+    TrackBar1: TTrackBar;
+    Label1: TLabel;
+    procedure GLSceneViewer1MouseDown(Sender: TObject;
+      Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure GLSceneViewer1MouseMove(Sender: TObject; Shift: TShiftState;
+      X, Y: Integer);
+    procedure FormCreate(Sender: TObject);
+    procedure TrackBar1Change(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    mx,my : Integer;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses GLFileNurbs, GLParametricSurfaces, Geometry, VectorLists;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  cp : TAffineVectorList;
+begin
+  // Load the nurbs data
+  GLFreeForm1.LoadFromFile('..\..\media\duck1.nurbs');
+  GLFreeForm1.AddDataFromFile('..\..\media\duck2.nurbs');
+  GLFreeForm1.AddDataFromFile('..\..\media\duck3.nurbs');
+  GLFreeForm1.SaveToFile('');
+
+  { Translate FreeForm based on the first mesh object's average
+    control point. Quick and dirty ... or maybe just dirty :P }
+  cp:=TMOParametricSurface(GLFreeForm1.MeshObjects[0]).ControlPoints;
+  GLFreeForm1.Position.Translate(VectorNegate(VectorScale(cp.Sum,1/cp.Count)));
+end;
+
+procedure TForm1.GLSceneViewer1MouseDown(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  mx:=x;
+  my:=y;
+end;
+
+procedure TForm1.GLSceneViewer1MouseMove(Sender: TObject;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if ssLeft in Shift then
+    GLCamera1.MoveAroundTarget(my-y,mx-x);
+  mx:=x;
+  my:=y;
+end;
+
+procedure TForm1.TrackBar1Change(Sender: TObject);
+var
+  i : integer;
+begin
+  for i:=0 to 2 do
+    TMOParametricSurface(GLFreeForm1.MeshObjects[i]).Resolution:=TrackBar1.Position;
+  GLFreeForm1.StructureChanged;
+end;
+
+end.
