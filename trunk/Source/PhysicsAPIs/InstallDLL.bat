49f31c7259001e934155d2d6d5cc41716beb20a4
mattias_f <mattias_f@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-08-27 08:17:55 +0000

    *** empty log message ***
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1843 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/PhysicsAPIs/InstallDLL.bat
+++ trunk/Source/PhysicsAPIs/InstallDLL.bat
@@ -4,8 +4,8 @@ echo Copying ode.dll to the windows system32 directory
 @echo Deploying for NT
 copy ode.dll %SystemRoot%\System32\
 
-@echo Deploying for Windows95 / Windows98 / WindowsME
-copy ode.dll %Windir%\System\
+rem @echo Deploying for Windows95 / Windows98 / WindowsME
+rem copy ode.dll %Windir%\System\
 
 
 pause
