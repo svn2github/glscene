8acf463fcbec639b3a27d11cc78cb86a1a1d1ce7
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-09-26 00:07:54 +0000

    Removed Variants unit from uses clause.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@1941 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/meshes/ducky/Unit1.pas
+++ trunk/Demos/meshes/ducky/Unit1.pas
@@ -15,7 +15,7 @@ unit Unit1;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
   Dialogs, GLScene, GLVectorFileObjects, GLObjects, GLWin32Viewer, GLMisc,
   ExtCtrls, ComCtrls, StdCtrls;
 
