2c5608457a2900effced27c10cc6ce752ab8c11d
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2003-12-12 02:53:49 +0000

    Now uses AddToVerletWorld to build the constraints.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2148 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLVerletSkeletonColliders.pas
+++ trunk/Source/GLVerletSkeletonColliders.pas
@@ -4,6 +4,7 @@
    constraints.<p>
 
    <b>History :</b><font size=-1><ul>
+     <li>11/12/03 - SG - Now uses AddToVerletWorld to build the constraints.
      <li>08/10/03 - SG - Creation.
    </ul></font>
 }
@@ -27,11 +28,10 @@ type
     public
       procedure WriteToFiler(writer : TVirtualWriter); override;
       procedure ReadFromFiler(reader : TVirtualReader); override;
+      procedure AddToVerletWorld(VerletWorld : TVerletWorld); virtual;
 
-      {: Warning: The constriants aren't destroyed by the collider.
-         Use AddSCVerletConstriantsToVerletWorld to add the constraints
-         to a VerletWorld. The VerletWorld will handle the constraint
-         cleanup. }
+      {: The verlet constraint is created through the AddToVerletWorld
+         procedure. }
       property VerletConstraint : TVerletConstraint read FVerletConstraint;
   end;
 
@@ -49,6 +49,7 @@ type
       constructor Create; override;
       procedure WriteToFiler(writer : TVirtualWriter); override;
       procedure ReadFromFiler(reader : TVirtualReader); override;
+      procedure AddToVerletWorld(VerletWorld : TVerletWorld); override;
       procedure AlignCollider; override;
 
       property Radius : Single read FRadius write SetRadius;
@@ -70,6 +71,7 @@ type
       constructor Create; override;
       procedure WriteToFiler(writer : TVirtualWriter); override;
       procedure ReadFromFiler(reader : TVirtualReader); override;
+      procedure AddToVerletWorld(VerletWorld : TVerletWorld); override;
       procedure AlignCollider; override;
 
       property Radius : Single read FRadius write SetRadius;
@@ -102,7 +104,7 @@ var
 begin
   for i:=0 to colliders.Count-1 do
     if colliders[i] is TSCVerletBase then
-      world.AddConstraint(TSCVerletBase(colliders[i]).VerletConstraint);
+      TSCVerletBase(colliders[i]).AddToVerletWorld(world);
 end;
 
 // ------------------
@@ -132,6 +134,13 @@ begin
   else RaiseFilerException(archiveVersion);
 end;
 
+// AddToVerletWorld
+//
+procedure TSCVerletBase.AddToVerletWorld(VerletWorld : TVerletWorld);
+begin
+  AlignCollider;
+end;
+
 
 // ------------------
 // ------------------ TSCVerletSphere ------------------
@@ -142,7 +151,6 @@ end;
 constructor TSCVerletSphere.Create;
 begin
   inherited;
-  FVerletConstraint:=TVCSphere.Create(nil);
   Radius:=0.5;
   AlignCollider;
 end;
@@ -171,12 +179,22 @@ begin
   else RaiseFilerException(archiveVersion);
 end;
 
+// AddToVerletWorld
+//
+procedure TSCVerletSphere.AddToVerletWorld(VerletWorld : TVerletWorld);
+begin
+  FVerletConstraint:=TVCSphere.Create(VerletWorld);
+  TVCSphere(FVerletConstraint).Radius:=FRadius;
+  inherited;
+end;
+
 // AlignCollider
 //
 procedure TSCVerletSphere.AlignCollider;
 begin
   inherited;
-  TVCSphere(FVerletConstraint).Location:=AffineVectorMake(GlobalMatrix[3]);
+  if Assigned(FVerletConstraint) then
+    TVCSphere(FVerletConstraint).Location:=AffineVectorMake(GlobalMatrix[3]);
 end;
 
 // SetRadius
@@ -185,7 +203,8 @@ procedure TSCVerletSphere.SetRadius(const val : Single);
 begin
   if val<>FRadius then begin
     FRadius:=val;
-    TVCSphere(FVerletConstraint).Radius:=FRadius;
+    if Assigned(FVerletConstraint) then
+      TVCSphere(FVerletConstraint).Radius:=FRadius;
   end;
 end;
 
@@ -199,7 +218,6 @@ end;
 constructor TSCVerletCapsule.Create;
 begin
   inherited;
-  FVerletConstraint:=TVCCapsule.Create(nil);
   Radius:=0.5;
   Length:=1;
   AlignCollider;
@@ -231,13 +249,25 @@ begin
   end else RaiseFilerException(archiveVersion);
 end;
 
+// AddToVerletWorld
+//
+procedure TSCVerletCapsule.AddToVerletWorld(VerletWorld : TVerletWorld);
+begin
+  FVerletConstraint:=TVCCapsule.Create(VerletWorld);
+  TVCCapsule(FVerletConstraint).Radius:=FRadius;
+  TVCCapsule(FVerletConstraint).Length:=FLength;
+  inherited;
+end;
+
 // AlignCollider
 //
 procedure TSCVerletCapsule.AlignCollider;
 begin
   inherited;
-  TVCCapsule(FVerletConstraint).Location:=AffineVectorMake(GlobalMatrix[3]);
-  TVCCapsule(FVerletConstraint).Axis:=AffineVectorMake(GlobalMatrix[1]);
+  if Assigned(FVerletConstraint) then begin
+    TVCCapsule(FVerletConstraint).Location:=AffineVectorMake(GlobalMatrix[3]);
+    TVCCapsule(FVerletConstraint).Axis:=AffineVectorMake(GlobalMatrix[1]);
+  end;
 end;
 
 // SetRadius
@@ -246,7 +276,8 @@ procedure TSCVerletCapsule.SetRadius(const val : Single);
 begin
   if val<>FRadius then begin
     FRadius:=val;
-    TVCCapsule(FVerletConstraint).Radius:=FRadius;
+    if Assigned(FVerletConstraint) then
+      TVCCapsule(FVerletConstraint).Radius:=FRadius;
   end;
 end;
 
@@ -256,7 +287,8 @@ procedure TSCVerletCapsule.SetLength(const val : Single);
 begin
   if val<>FLength then begin
     FLength:=val;
-    TVCCapsule(FVerletConstraint).Length:=FLength;
+    if Assigned(FVerletConstraint) then
+      TVCCapsule(FVerletConstraint).Length:=FLength;
   end;
 end;
 
