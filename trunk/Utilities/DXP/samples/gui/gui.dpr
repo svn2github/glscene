002387af9df99a4068d17952a9dc4b8d5f709538
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-01-05 11:48:39 +0000

    Dynamic from creation
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2234 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Utilities/DXP/samples/gui/gui.dpr
+++ trunk/Utilities/DXP/samples/gui/gui.dpr
@@ -2,7 +2,16 @@ program gui;
 
 uses Forms, Dialogs {$IFDEF FPC}, Interfaces{$ENDIF};
 
+var
+   frm : TForm;
 begin
    Application.Initialize;
-   ShowMessage('Hello World!');
+
+   frm:=TForm.Create(nil);
+   frm.Caption:='Hello World!';
+   frm.Position:=poScreenCenter;
+
+   frm.ShowModal;
+
+   frm.Free;
 end.
