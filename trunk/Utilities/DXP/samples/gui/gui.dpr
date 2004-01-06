2a0ca1ac919cd46d77c8652b1d15c1ace015d082
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-01-06 15:06:24 +0000

    Added a button to the sample form
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2240 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Utilities/DXP/samples/gui/gui.dpr
+++ trunk/Utilities/DXP/samples/gui/gui.dpr
@@ -1,15 +1,27 @@
 program gui;
 
-uses Forms, Dialogs {$IFDEF FPC}, Interfaces{$ENDIF};
+{$APPTYPE gui}
+
+uses Forms, Controls, StdCtrls {$IFDEF FPC}, Buttons, Interfaces{$ENDIF};
 
 var
    frm : TForm;
+   button : TButton;
 begin
    Application.Initialize;
 
    frm:=TForm.Create(nil);
    frm.Caption:='Hello World!';
    frm.Position:=poScreenCenter;
+   frm.Width:=200;
+   frm.Height:=100;
+
+   button:=TButton.Create(frm);
+   button.Left:=60;
+   button.Top:=20;
+   button.Caption:='Ok!';
+   button.ModalResult:=mrOk;
+   button.Parent:=frm;
 
    frm.ShowModal;
 
