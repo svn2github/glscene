f7452e8eb785f0875e475b9779b24a6c8149181f
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-01-22 00:30:50 +0000

    Removed design-time rendering option because the shader is unstable at design-time
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2275 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Shaders/GLMultiMaterialShader.pas
+++ trunk/Source/Shaders/GLMultiMaterialShader.pas
@@ -4,6 +4,8 @@
    its assigned MaterialLibrary.<p>
 
    <b>History : </b><font size=-1><ul>
+      <li>29/07/03 - SG - Removed design-time rendering option
+                          (shader unstable at design-time)
       <li>29/07/03 - SG - Creation
    </ul></font>
 }
@@ -12,25 +14,22 @@ unit GLMultiMaterialShader;
 interface
 
 uses
-  Classes, GLTexture, OpenGL1x;
+   Classes, GLTexture, OpenGL1x;
 
 type
-  TGLMultiMaterialShader = class(TGLShader)
-    private
-      FPass : Integer;
-      FMaterialLibrary : TGLMaterialLibrary;
-      FDesignTimeEnabled : Boolean;
-    protected
-      procedure SetDesignTimeEnabled(const val : Boolean);
-      procedure SetMaterialLibrary(const val : TGLMaterialLibrary);
-      procedure DoApply(var rci : TRenderContextInfo; Sender : TObject); override;
-      function DoUnApply(var rci : TRenderContextInfo) : Boolean; override;
-    public
-      constructor Create(aOwner : TComponent); override;
-    published
-      property MaterialLibrary : TGLMaterialLibrary read FMaterialLibrary write SetMaterialLibrary;
-      property DesignTimeEnabled : Boolean read FDesignTimeEnabled write SetDesignTimeEnabled;
-  end;
+   TGLMultiMaterialShader = class(TGLShader)
+      private
+         FPass : Integer;
+         FMaterialLibrary : TGLMaterialLibrary;
+      protected
+         procedure SetMaterialLibrary(const val : TGLMaterialLibrary);
+         procedure DoApply(var rci : TRenderContextInfo; Sender : TObject); override;
+         function DoUnApply(var rci : TRenderContextInfo) : Boolean; override;
+      public
+         constructor Create(aOwner : TComponent); override;
+      published
+         property MaterialLibrary : TGLMaterialLibrary read FMaterialLibrary write SetMaterialLibrary;
+   end;
 
 procedure Register;
 
@@ -44,10 +43,7 @@ implementation
 
 procedure Register;
 begin
-   // EG: no longer registers by default, unstable at design-time
-   //
-//  RegisterComponents('GLScene Shaders', [TGLMultiMaterialShader]);
-   RegisterClass(TGLMultiMaterialShader);
+   RegisterComponents('GLScene Shaders', [TGLMultiMaterialShader]);
 end;
 
 // ------------------
@@ -58,66 +54,56 @@ end;
 //
 constructor TGLMultiMaterialShader.Create(aOwner : TComponent);
 begin
-  inherited;
-  ShaderStyle:=ssReplace;
+   inherited;
+   ShaderStyle:=ssReplace;
 end;
 
 // DoApply
 //
 procedure TGLMultiMaterialShader.DoApply(var rci: TRenderContextInfo; Sender : TObject);
 begin
-  if not Assigned(FMaterialLibrary) then exit;
+   if not Assigned(FMaterialLibrary) then exit;
 
-  FPass:=1;
-  if (FDesignTimeEnabled or (not (csDesigning in ComponentState))) then begin
-    glPushAttrib(GL_ALL_ATTRIB_BITS);
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-    if FMaterialLibrary.Materials.Count>0 then
-      FMaterialLibrary.Materials[0].Apply(rci);
+   FPass:=1;
+   if not (csDesigning in ComponentState) then begin
+      glPushAttrib(GL_ALL_ATTRIB_BITS);
+      glEnable(GL_DEPTH_TEST);
+      glDepthFunc(GL_LEQUAL);
+      if FMaterialLibrary.Materials.Count>0 then
+         FMaterialLibrary.Materials[0].Apply(rci);
   end;
 end;
 
 // DoUnApply
 //
 function TGLMultiMaterialShader.DoUnApply(
-  var rci: TRenderContextInfo): Boolean;
+   var rci: TRenderContextInfo): Boolean;
 begin
-  Result:=False;
-  if not Assigned(FMaterialLibrary) then exit;
-  if (FDesignTimeEnabled or (not (csDesigning in ComponentState))) then begin
-    if FMaterialLibrary.Materials.Count>0 then
-      FMaterialLibrary.Materials[FPass-1].UnApply(rci);
-    if (FPass >= FMaterialLibrary.Materials.Count) then begin
-      glDepthFunc(GL_LESS);
-      glPopAttrib;
-      exit;
-    end;
-    FMaterialLibrary.Materials[FPass].Apply(rci);
-    Result:=True;
-    Inc(FPass);
-  end;
-end;
-
-// SetDesignTimeEnabled
-//
-procedure TGLMultiMaterialShader.SetDesignTimeEnabled(const val : Boolean);
-begin
-  if val<>FDesignTimeEnabled then begin
-    FDesignTimeEnabled:=val;
-    NotifyChange(Self);
-  end;
+   Result:=False;
+   if not Assigned(FMaterialLibrary) then exit;
+   if not (csDesigning in ComponentState) then begin
+      if FMaterialLibrary.Materials.Count>0 then
+         FMaterialLibrary.Materials[FPass-1].UnApply(rci);
+      if (FPass >= FMaterialLibrary.Materials.Count) then begin
+         glDepthFunc(GL_LESS);
+         glPopAttrib;
+         exit;
+      end;
+      FMaterialLibrary.Materials[FPass].Apply(rci);
+      Result:=True;
+      Inc(FPass);
+   end;
 end;
 
 // SetMaterialLibrary
 //
 procedure TGLMultiMaterialShader.SetMaterialLibrary(
-  const val: TGLMaterialLibrary);
+   const val: TGLMaterialLibrary);
 begin
-  if val<>FMaterialLibrary then begin
-    FMaterialLibrary:=val;
-    NotifyChange(Self);
-  end;
+   if val<>FMaterialLibrary then begin
+      FMaterialLibrary:=val;
+      NotifyChange(Self);
+   end;
 end;
 
 end.
