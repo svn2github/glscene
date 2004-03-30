01a5079f9e3e106b323c9445a749e5704dba02f4
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-03-30 03:59:05 +0000

    ODE enabled scene is now built totally at design-time.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2416 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/physics/machine/Unit1.pas
+++ trunk/Demos/physics/machine/Unit1.pas
@@ -1,18 +1,14 @@
 {: Creating ODE joints with GLODEManager.<p>
    
    This demo shows how to create connections between ODE objects using
-   joints. At the moment, the only way to initialize a joint between two
-   objects is the ODEJoint.Attach method. A way around this is being
-   looked into and will be implemented into this demo when it's been
-   worked out.<p>
-   
-   In this demo, the GLODEDynamicBehaviour object was used to pass the
-   control of GLScene objects to ODE. The same methods can be used to 
-   join GLODEDummy objects to other GLODEDummy objects, or to
-   GLODEDynamicBehaviours through their Body properties.<p>
-   
-   A joint can be fixed to space by attaching to nil, as done with the
-   WheelBehaviour.<p>
+   joints.<p>
+
+   In this demo, the Dynamic Behaviour object was used to pass the
+   control of GLScene objects to ODE. Joints are used to connect
+   the objects together physically.<p>
+
+   A joint can be fixed to space by attaching to a nothing, as done
+   with the 'Wheel Fixed' and 'Pin2 Fixed'.<p>
 }
 unit Unit1;
 
@@ -64,76 +60,8 @@ uses VectorGeometry;
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  WheelBehaviour,
-  ArmBehaviour,
-  Pin2Behaviour  : TGLODEDynamicBehaviour;
 begin
-  WheelBehaviour:=TGLODEDynamicBehaviour.Create(Wheel.Behaviours);
-  ArmBehaviour:=TGLODEDynamicBehaviour.Create(Arm.Behaviours);
-  Pin2Behaviour:=TGLODEDynamicBehaviour.Create(Pin2.Behaviours);
-
-  with WheelBehaviour do begin
-    Manager:=GLODEManager1;
-    with TODEElementCapsule(AddNewElement(TODEElementCapsule)) do begin
-      Position.SetPoint(0,0,0);
-      Radius:=Wheel.TopRadius;
-      Length:=Wheel.Height;
-    end;
-    with TODEElementCapsule(AddNewElement(TODEElementCapsule)) do begin
-      Position.AsVector:=Axle.Position.AsVector;
-      Radius:=Axle.TopRadius;
-      Length:=Axle.Height;
-    end;
-    CalibrateCenterOfMass;
-  end;
-  with ArmBehaviour do begin
-    Manager:=GLODEManager1;
-    with TODEElementBox(AddNewElement(TODEElementBox)) do begin
-      Position.SetPoint(0,0,0);
-      BoxWidth:=Arm.CubeWidth;
-      BoxHeight:=Arm.CubeHeight;
-      BoxDepth:=Arm.CubeDepth;
-    end;
-  end;
-  with Pin2Behaviour do begin
-    Manager:=GLODEManager1;
-    with TODEElementCapsule(AddNewElement(TODEElementCapsule)) do begin
-      Position.SetPoint(0,0,0);
-      Radius:=Pin2.TopRadius;
-      Length:=Pin2.Height;
-    end;
-  end;
-
-  with TODEJointHinge.Create(GLODEJointList1.Joints) do begin
-    Manager:=GLODEManager1;
-    Attach(Wheel,nil);
-    Axis.SetVector(0,1,0);
-    Anchor.AsVector:=Wheel.AbsolutePosition;
-  end;
-
-  with TODEJointHinge.Create(GLODEJointList1.Joints) do begin
-    Manager:=GLODEManager1;
-    Attach(Wheel,Arm);
-    Axis.SetVector(0,1,0);
-    Anchor.AsVector:=Pin1.AbsolutePosition;
-  end;
-
-  with TODEJointHinge.Create(GLODEJointList1.Joints) do begin
-    Manager:=GLODEManager1;
-    Attach(Arm,Pin2);
-    Axis.SetVector(0,1,0);
-    Anchor.AsVector:=Pin2.AbsolutePosition;
-  end;
-
-  with TODEJointSlider.Create(GLODEJointList1.Joints) do begin
-    Manager:=GLODEManager1;
-    Attach(Pin2,nil);
-    Axis.SetVector(1,0,0);
-  end;
-
-  //WheelBehaviour.AddTorque(AffineVectorMake(0,1000,0));
-  Pin2Behaviour.AddForce(AffineVectorMake(-1000,0,0));
+  TGLODEDynamicBehaviour(Pin2.Behaviours[0]).AddForce(AffineVectorMake(-50,0,0));
 end;
 
 procedure TForm1.GLSceneViewer1MouseDown(Sender: TObject;
