978524bdce414092980982175b7a62a6d4fcbc30
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-04-08 12:58:27 +0000

    Updated web links
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2451 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/AdvDemos/PanoViewer/Unit1.pas
+++ trunk/AdvDemos/PanoViewer/Unit1.pas
@@ -1,11 +1,12 @@
 {
   Simple spherical panorama viewer using GLScene
 
-  The sample input images are by Philippe Hurbain. http://philohome.free.fr/
+  The sample input images are by Philippe Hurbain. http://www.philohome.com
 
   Other resources on how to make your own spherical or cylindrical panorama:
     http://www.fh-furtwangen.de/~dersch/
     http://www.panoguide.com/
+    http://home.no.net/dmaurer/~dersch/Index.htm
 
   Why IPIX patents regarding use of fisheye photos are questionable:
     http://www.worldserver.com/turk/quicktimevr/fisheye.html
