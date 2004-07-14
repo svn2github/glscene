8bb85ea9e0b29c7af33ddc5d83e07bdec7c2231b
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-07-14 01:24:56 +0000

    Removed FrameIndex, Dimension Auto now uses CurrentFrame for generating the offset coords. Frames only used now if Dimension is Manual.
    Added Rotation and Mirror properties.
    
    Other minor changes.
    
    
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2788 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLAnimatedSprite.pas
+++ trunk/Source/GLAnimatedSprite.pas
@@ -23,7 +23,6 @@ type
 
   TSpriteAnimFrame = class(TXCollectionItem)
     private
-      FFrameIndex,
       FOffsetX,
       FOffsetY,
       FWidth,
@@ -32,7 +31,6 @@ type
       procedure DoChanged;
 
     protected
-      procedure SetFrameIndex(const Value : Integer);
       procedure SetOffsetX(const Value : Integer);
       procedure SetOffsetY(const Value : Integer);
       procedure SetWidth(const Value : Integer);
@@ -41,12 +39,10 @@ type
       procedure ReadFromFiler(reader : TReader); override;
 
     public
-      constructor Create(aOwner : TXCollection); override;
       class function FriendlyName : String; override;
       class function FriendlyDescription : String; override;
 
     published
-      property FrameIndex : Integer read FFrameIndex write SetFrameIndex;
       property OffsetX : Integer read FOffsetX write SetOffsetX;
       property OffsetY : Integer read FOffsetY write SetOffsetY;
       property Width : Integer read FWidth write SetWidth;
@@ -66,6 +62,8 @@ type
   TSpriteAnimation = class(TXCollectionItem)
     private
       FCurrentFrame,
+      FStartFrame,
+      FEndFrame,
       FFrameWidth,
       FFrameHeight : Integer;
       FFrames : TSpriteAnimFrameList;
@@ -95,6 +93,8 @@ type
 
     published
       property CurrentFrame : Integer read FCurrentFrame write SetCurrentFrame;
+      property StartFrame : Integer read FStartFrame write FStartFrame;
+      property EndFrame : Integer read FEndFrame write FEndFrame;
       property FrameWidth : Integer read FFrameWidth write SetFrameWidth;
       property FrameHeight : Integer read FFrameHeight write SetFrameHeight;
       property LibMaterialName : TGLLibMaterialName read FLibMaterialName write SetLibMaterialName;
@@ -118,10 +118,13 @@ type
       FAnimations : TSpriteAnimationList;
       FMaterialLibrary : TGLMaterialLibrary;
       FAnimationIndex,
-      FInterval : Integer;
+      FInterval,
+      FRotation,
+      FPixelRatio : Integer;
+      FMirrorU,
+      FMirrorV : Boolean;
       FAnimationMode : TSpriteAnimationMode;
       FCurrentFrameDelta : Double;
-      FPixelRatio : Integer;
       FOnFrameChanged : TNotifyEvent;
       FOnEndFrameReached : TNotifyEvent;
       FOnStartFrameReached : TNotifyEvent;
@@ -136,12 +139,15 @@ type
       procedure SetAnimationMode(const val : TSpriteAnimationMode);
       procedure SetMaterialLibrary(const val : TGLMaterialLibrary);
       procedure SetPixelRatio(const val : Integer);
+      procedure SetRotation(const val : Integer);
+      procedure SetMirrorU(const val : Boolean);
+      procedure SetMirrorV(const val : Boolean);
 
     public
       constructor Create(AOwner : TComponent); override;
       destructor Destroy; override;
 
-      procedure DoRender(var rci : TRenderContextInfo; renderSelf, renderChildren : Boolean); override;
+      procedure BuildList(var rci : TRenderContextInfo); override;
       procedure DoProgress(const progressTime : TProgressTimes); override;
 
       procedure NextFrame;
@@ -153,6 +159,9 @@ type
       property AnimationIndex : Integer read FAnimationIndex write SetAnimationIndex;
       property AnimationMode : TSpriteAnimationMode read FAnimationMode write SetAnimationMode;
       property PixelRatio : Integer read FPixelRatio write SetPixelRatio;
+      property Rotation : Integer read FRotation write SetRotation;
+      property MirrorU : Boolean read FMirrorU write SetMirrorU;
+      property MirrorV : Boolean read FMirrorV write SetMirrorV;
 
       property Position;
       property Scale;
@@ -183,14 +192,6 @@ end;
 // ----------
 // ---------- TSpriteAnimFrame ----------
 // ----------
-
-// Create
-//
-constructor TSpriteAnimFrame.Create(aOwner : TXCollection);
-begin
-  inherited;
-  FFrameIndex:=0;
-end;
 
 // DoChanged
 //
@@ -224,7 +225,6 @@ begin
   inherited;
   writer.WriteInteger(0); // Archive version number
   with writer do begin
-    WriteInteger(FrameIndex);
     WriteInteger(OffsetX);
     WriteInteger(OffsetY);
     WriteInteger(Width);
@@ -242,21 +242,10 @@ begin
   archiveVersion:=reader.ReadInteger;
   Assert(archiveVersion = 0);
   with reader do begin
-    FrameIndex:=ReadInteger;
     OffsetX:=ReadInteger;
     OffsetY:=ReadInteger;
     Width:=ReadInteger;
     Height:=ReadInteger;
-  end;
-end;
-
-// SetFrameIndex
-//
-procedure TSpriteAnimFrame.SetFrameIndex(const Value: Integer);
-begin
-  if Value<>FFrameIndex then begin
-    FFrameIndex := Value;
-    DoChanged;
   end;
 end;
 
@@ -364,6 +353,8 @@ begin
   with writer do begin
     WriteString(LibMaterialName);
     WriteInteger(CurrentFrame);
+    WriteInteger(StartFrame);
+    WriteInteger(EndFrame);
     WriteInteger(FrameWidth);
     WriteInteger(FrameHeight);
     WriteInteger(Integer(Dimensions));
@@ -383,6 +374,8 @@ begin
   with reader do begin
     FLibMaterialName:=ReadString;
     CurrentFrame:=ReadInteger;
+    StartFrame:=ReadInteger;
+    EndFrame:=ReadInteger;
     FrameWidth:=ReadInteger;
     FrameHeight:=ReadInteger;
     Dimensions:=TSpriteFrameDimensions(ReadInteger);
@@ -396,7 +389,7 @@ begin
   if Assigned(Owner) then begin
     if Assigned(Owner.Owner) then
       if Owner.Owner is TGLBaseSceneObject then
-        TGLBaseSceneObject(Owner.Owner).NotifyChange(nil);
+        TGLBaseSceneObject(Owner.Owner).NotifyChange(Self);
   end;
 end;
 
@@ -499,12 +492,15 @@ constructor TGLAnimatedSprite.Create(AOwner: TComponent);
 begin
   inherited;
 
-  ObjectStyle:=ObjectStyle+[osDirectDraw];
-
   FAnimations:=TSpriteAnimationList.Create(Self);
   FAnimationIndex:=-1;
   FInterval:=100;
   FPixelRatio:=100;
+  FRotation:=0;
+  FMirrorU:=False;
+  FMirrorV:=False;
+
+  ObjectStyle:=[osDirectDraw];
 end;
 
 // Destroy
@@ -517,93 +513,122 @@ end;
 
 // DoRender
 //
-procedure TGLAnimatedSprite.DoRender(var rci : TRenderContextInfo;
-                                     renderSelf, renderChildren : Boolean);
+procedure TGLAnimatedSprite.BuildList(var rci : TRenderContextInfo);
 var
-  vx, vy : TAffineVector;
-  w, h : Single;
+  vx,vy : TAffineVector;
+  w,h,temp : Single;
   mat : TMatrix;
   u0,v0,u1,v1 : Single;
   x0,y0,x1,y1,TexWidth,TexHeight : Integer;
   Anim : TSpriteAnimation;
   Frame : TSpriteAnimFrame;
   libMat : TGLLibMaterial;
+  IsAuto : Boolean;
 begin
-  if renderSelf and (FAnimationIndex<>-1) and (FAnimationIndex<Animations.Count) then begin
+  if (FAnimationIndex<>-1) and (FAnimationIndex<Animations.Count) then begin
     Anim:=TSpriteAnimation(Animations[FAnimationIndex]);
-    if (Anim.CurrentFrame<>-1) and (Anim.CurrentFrame<Anim.Frames.Count) then begin
-      Frame:=TSpriteAnimFrame(Anim.Frames[Anim.CurrentFrame]);
-      libMat:=Anim.LibMaterialCached;
 
-      h:=0.5;
-      w:=0.5;
-      u0:=0;
-      v0:=0;
-      u1:=0;
-      v1:=0;
+    if (Anim.CurrentFrame>=0) then begin
+      if (Anim.Dimensions = sfdManual) and (Anim.CurrentFrame<Anim.Frames.Count) then
+        Frame:=TSpriteAnimFrame(Anim.Frames[Anim.CurrentFrame])
+      else
+        Frame:=nil;
+      IsAuto:=(Anim.CurrentFrame<=Anim.EndFrame) and
+              (Anim.CurrentFrame>=Anim.StartFrame) and
+              (Anim.Dimensions = sfdAuto);
+      if Assigned(Frame) or IsAuto then begin
+        libMat:=Anim.LibMaterialCached;
 
-      if Assigned(libMat) then begin
-        TexWidth:=libMat.Material.Texture.Image.Width;
-        TexHeight:=libMat.Material.Texture.Image.Height;
-        if Anim.Dimensions = sfdManual then begin
-          x0:=Frame.OffsetX;
-          y0:=Frame.OffsetY;
-          x1:=x0+Frame.Width;
-          y1:=y0+Frame.Height;
-        end else begin
-          if TexWidth>0 then begin
-            x0:=Anim.FrameWidth*(Frame.FrameIndex mod (TexWidth div Anim.FrameWidth));
-            y0:=Anim.FrameHeight*(Frame.FrameIndex div (TexWidth div Anim.FrameWidth));
+        h:=0.5;
+        w:=0.5;
+        u0:=0;
+        v0:=0;
+        u1:=0;
+        v1:=0;
+
+        if Assigned(libMat) then begin
+          TexWidth:=libMat.Material.Texture.Image.Width;
+          TexHeight:=libMat.Material.Texture.Image.Height;
+          if Anim.Dimensions = sfdManual then begin
+            x0:=Frame.OffsetX;
+            y0:=Frame.OffsetY;
+            x1:=x0+Frame.Width;
+            y1:=y0+Frame.Height;
           end else begin
-            x0:=0;
-            y0:=0;
+            if TexWidth>0 then begin
+              x0:=Anim.FrameWidth*(Anim.CurrentFrame mod (TexWidth div Anim.FrameWidth));
+              y0:=Anim.FrameHeight*(Anim.CurrentFrame div (TexWidth div Anim.FrameWidth));
+            end else begin
+              x0:=0;
+              y0:=0;
+            end;
+            x1:=x0+Anim.FrameWidth;
+            y1:=y0+Anim.FrameHeight;
           end;
-          x1:=x0+Anim.FrameWidth;
-          y1:=y0+Anim.FrameHeight;
+          if (TexWidth>0) and (TexHeight>0) and (x0<>x1) and (y0<>y1) then begin
+            u0:=x0/TexWidth; v0:=1-y1/TexHeight;
+            u1:=x1/TexWidth; v1:=1-y0/TexHeight;
+            w:=0.5*(x1-x0)/FPixelRatio;
+            h:=0.5*(y1-y0)/FPixelRatio;
+          end;
         end;
-        if (TexWidth>0) and (TexHeight>0) and (x0<>x1) and (y0<>y1) then begin
-          u0:=x0/TexWidth; v0:=1-y1/TexHeight;
-          u1:=x1/TexWidth; v1:=1-y0/TexHeight;
-          w:=0.5*(x1-x0)/FPixelRatio;
-          h:=0.5*(y1-y0)/FPixelRatio;
+
+        glGetFloatv(GL_MODELVIEW_MATRIX, @mat);
+        vx[0]:=mat[0][0]; vy[0]:=mat[0][1];
+        vx[1]:=mat[1][0]; vy[1]:=mat[1][1];
+        vx[2]:=mat[2][0]; vy[2]:=mat[2][1];
+        ScaleVector(vx, w*VectorLength(vx));
+        ScaleVector(vy, h*VectorLength(vy));
+
+        if FMirrorU then begin
+          temp:=u0;
+          u0:=u1;
+          u1:=temp;
         end;
+        if FMirrorV then begin
+          temp:=v0;
+          v0:=v1;
+          v1:=temp;
+        end;
+
+        if Assigned(libMat) then libMat.Apply(rci);
+        glPushAttrib(GL_ENABLE_BIT);
+        glDisable(GL_LIGHTING);
+        if FRotation<>0 then begin
+          glMatrixMode(GL_MODELVIEW);
+          glPushMatrix;
+          glRotatef(FRotation,mat[0][2],mat[1][2],mat[2][2]);
+        end;
+        glBegin(GL_QUADS);
+          glTexCoord2f(u1, v1); glVertex3f( vx[0]+vy[0], vx[1]+vy[1], vx[2]+vy[2]);
+          glTexCoord2f(u0, v1); glVertex3f(-vx[0]+vy[0],-vx[1]+vy[1],-vx[2]+vy[2]);
+          glTexCoord2f(u0, v0); glVertex3f(-vx[0]-vy[0],-vx[1]-vy[1],-vx[2]-vy[2]);
+          glTexCoord2f(u1, v0); glVertex3f( vx[0]-vy[0], vx[1]-vy[1], vx[2]-vy[2]);
+        glEnd;
+        if FRotation<>0 then begin
+          glPopMatrix;
+        end;
+        glPopAttrib;
+        if Assigned(libMat) then libMat.UnApply(rci);
       end;
-
-      glGetFloatv(GL_MODELVIEW_MATRIX, @mat);
-      vx[0]:=mat[0][0]; vy[0]:=mat[0][1];
-      vx[1]:=mat[1][0]; vy[1]:=mat[1][1];
-      vx[2]:=mat[2][0]; vy[2]:=mat[2][1];
-      ScaleVector(vx, w*VectorLength(vx));
-      ScaleVector(vy, h*VectorLength(vy));
-
-      if Assigned(libMat) then libMat.Apply(rci);
-      glPushAttrib(GL_ENABLE_BIT);
-      glDisable(GL_LIGHTING);
-      glBegin(GL_QUADS);
-        glTexCoord2f(u1, v1); glVertex3f( vx[0]+vy[0], vx[1]+vy[1], vx[2]+vy[2]);
-        glTexCoord2f(u0, v1); glVertex3f(-vx[0]+vy[0],-vx[1]+vy[1],-vx[2]+vy[2]);
-        glTexCoord2f(u0, v0); glVertex3f(-vx[0]-vy[0],-vx[1]-vy[1],-vx[2]-vy[2]);
-        glTexCoord2f(u1, v0); glVertex3f( vx[0]-vy[0], vx[1]-vy[1], vx[2]-vy[2]);
-      glEnd;
-      glPopAttrib;
-      if Assigned(libMat) then libMat.UnApply(rci);
     end;
   end;
-
-  if renderChildren and (Count>0) then
-    Self.RenderChildren(0, Count-1, rci);
 end;
 
 // DoProgress
 //
 procedure TGLAnimatedSprite.DoProgress(const progressTime : TProgressTimes);
+var
+  i : Integer;
 begin
   inherited;
   if (FAnimationMode<>samNone) and (Interval>0) and (AnimationIndex<>-1) then begin
     FCurrentFrameDelta:=FCurrentFrameDelta+(progressTime.deltaTime*1000)/FInterval;
     if FCurrentFrameDelta>=1 then begin
-      NextFrame;
-      FCurrentFrameDelta:=0;
+      for i:=0 to Floor(FCurrentFrameDelta)-1 do begin
+        NextFrame;
+        FCurrentFrameDelta:=FCurrentFrameDelta-1;
+      end;
     end;
   end;
 end;
@@ -660,12 +685,22 @@ end;
 procedure TGLAnimatedSprite.NextFrame;
 var
   currentFrame,
+  startFrame,
   endFrame : Integer;
+  Anim : TSpriteAnimation;
 begin
   if (FAnimationIndex=-1) or (FAnimationIndex>=Animations.Count) then exit;
 
-  currentFrame:=TSpriteAnimation(Animations[FAnimationIndex]).CurrentFrame;
-  endFrame:=TSpriteAnimation(Animations[FAnimationIndex]).Frames.Count-1;
+  Anim:=TSpriteAnimation(Animations[FAnimationIndex]);
+
+  currentFrame:=Anim.CurrentFrame;
+  if Anim.Dimensions = sfdManual then begin
+    startFrame:=0;
+    endFrame:=Anim.Frames.Count-1
+  end else begin
+    startFrame:=Anim.StartFrame;
+    endFrame:=Anim.EndFrame;
+  end;
 
   case AnimationMode of
     samLoop, samBounceForward, samPlayOnce : begin
@@ -673,17 +708,16 @@ begin
       if (currentFrame = endFrame) and Assigned(FOnEndFrameReached) then
         FOnEndFrameReached(Self);
     end;
-
     samBounceBackward, samLoopBackward : begin
       Dec(CurrentFrame);
-      if (currentFrame = 0) and Assigned(FOnStartFrameReached) then
+      if (currentFrame = startFrame) and Assigned(FOnStartFrameReached) then
         FOnStartFrameReached(Self);
     end;
   end;
 
   if (AnimationMode<>samNone) and Assigned(FOnFrameChanged) then
     FOnFrameChanged(Self);
-
+  
   case AnimationMode of
 
     samPlayOnce : begin
@@ -693,7 +727,7 @@ begin
 
     samLoop : begin
       if currentFrame > endFrame then
-        currentFrame:=0;
+        currentFrame:=startFrame;
     end;
 
     samBounceForward : begin
@@ -702,18 +736,18 @@ begin
     end;
 
     samLoopBackward : begin
-      if currentFrame < 0 then
+      if currentFrame < startFrame then
         CurrentFrame:=endFrame;
     end;
 
     samBounceBackward : begin
-      if currentFrame = 0 then
+      if currentFrame = startFrame then
         AnimationMode:=samBounceForward;
     end;
 
   end;
 
-  TSpriteAnimation(Animations[FAnimationIndex]).CurrentFrame:=currentFrame;
+  Anim.CurrentFrame:=currentFrame;
 end;
 
 // SetInterval
@@ -722,7 +756,7 @@ procedure TGLAnimatedSprite.SetInterval(const val : Integer);
 begin
   if val<>FInterval then begin
     FInterval:=val;
-    StructureChanged;
+    NotifyChange(Self);
   end;
 end;
 
@@ -733,7 +767,7 @@ begin
   if val<>FAnimationIndex then begin
     FAnimationIndex:=val;
     if FAnimationIndex<0 then FAnimationIndex:=-1;
-    StructureChanged;
+    NotifyChange(Self);
   end;
 end;
 
@@ -743,17 +777,21 @@ procedure TGLAnimatedSprite.SetAnimationMode(const val : TSpriteAnimationMode);
 begin
   if val<>FAnimationMode then begin
     FAnimationMode:=val;
-    StructureChanged;
+    NotifyChange(Self);
   end;
 end;
 
 // SetMaterialLibrary
 //
 procedure TGLAnimatedSprite.SetMaterialLibrary(const val : TGLMaterialLibrary);
+var
+  i : Integer;
 begin
   if val<>FMaterialLibrary then begin
     FMaterialLibrary:=val;
-    StructureChanged;
+    for i:=0 to Animations.Count-1 do
+      TSpriteAnimation(Animations[i]).FLibMaterialCached:=nil;
+    NotifyChange(Self);
   end;
 end;
 
@@ -763,7 +801,37 @@ procedure TGLAnimatedSprite.SetPixelRatio(const val : Integer);
 begin
   if (FPixelRatio<>val) and (val>0) then begin
     FPixelRatio:=val;
-    StructureChanged;
+    NotifyChange(Self);
+  end;
+end;
+
+// SetRotation
+//
+procedure TGLAnimatedSprite.SetRotation(const val : Integer);
+begin
+  if val<>FRotation then begin
+    FRotation:=val;
+    NotifyChange(Self);
+  end;
+end;
+
+// SetMirrorU
+//
+procedure TGLAnimatedSprite.SetMirrorU(const val : Boolean);
+begin
+  if val<>FMirrorU then begin
+    FMirrorU:=val;
+    NotifyChange(Self);
+  end;
+end;
+
+// SetMirrorV
+//
+procedure TGLAnimatedSprite.SetMirrorV(const val : Boolean);
+begin
+  if val<>FMirrorV then begin
+    FMirrorV:=val;
+    NotifyChange(Self);
   end;
 end;
 
