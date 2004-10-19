450cc1003ba963b1cc10db3c8cb31b47e88f0307
mrqzzz <mrqzzz@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-10-19 21:27:33 +0000

    New PFX gallery with some effects
    git-svn-id: http://svn.code.sf.net/p/glscene/code@2975 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Demos/specialsFX/PFXGallery/PFXGallery.dpr
@@ -0,0 +1,13 @@
+program PFXGallery;
+
+uses
+  Forms,
+  UPFXGallery in 'UPFXGallery.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
