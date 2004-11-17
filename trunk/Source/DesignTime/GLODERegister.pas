c9f834cc1c056e43888da64dd96296c15ab6dca5
sgooding <sgooding@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2004-11-17 03:06:53 +0000

    TGLODEDummy -> TGLODEDynamicDummy, Removed initialization code.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3027 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/DesignTime/GLODERegister.pas
+++ trunk/Source/DesignTime/GLODERegister.pas
@@ -26,25 +26,10 @@ implementation
 //
 procedure Register;
 begin
-  RegisterClasses([TGLODEManager, TGLODEJointList, TGLODEDummy,
+  RegisterClasses([TGLODEManager, TGLODEJointList, TGLODEDynamicDummy,
                    TGLODEPlane, TGLODEStaticDummy, TGLODETerrain,
                    TODEJoints, TODEElements]);
   RegisterComponents('GLScene',[TGLODEManager,TGLODEJointList]);
 end;
-
-// ------------------------------------------------------------------
-// ------------------------------------------------------------------
-// ------------------------------------------------------------------
-initialization
-// ------------------------------------------------------------------
-// ------------------------------------------------------------------
-// ------------------------------------------------------------------
-
-  {with ObjectManager do begin
-    RegisterSceneObject(TGLODEDummy,'GLODEDummy','ODE Objects');
-    RegisterSceneObject(TGLODEStaticDummy,'GLODEStaticDummy','ODE Objects');
-    RegisterSceneObject(TGLODEPlane,'GLODEPlane','ODE Objects');
-    RegisterSceneObject(TGLODETerrain,'GLODETerrain','ODE Objects');
-  end;//}
 
 end.
