bc0f2510ab3432fe4674cd334a32b743e33b69ac
egrange <egrange@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2005-02-14 12:39:08 +0000

    Passes GLSL validation (const array initialization not supported without Cg)
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3217 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/glslshaders/ocean/ocean_vp.glsl
+++ trunk/Demos/glslshaders/ocean/ocean_vp.glsl
@@ -20,60 +20,70 @@ const half cBumpSpeed = 0.4;
 
 #define NWAVES 3
 struct Wave {
-  half freq;  // 2*PI / wavelength
-  half amp;   // amplitude
-  half phase; // speed * 2*PI / wavelength
-  half2 dir;
+   half freq;  // 2*PI / wavelength
+   half amp;   // amplitude
+   half phase; // speed * 2*PI / wavelength
+   half2 dir;
 };
-Wave wave[NWAVES] = {
-	{ 0.2, 0.9, 12.0, half2(1.0, 0.0) },
-	{ 0.3, 0.7, 9.0,  half2(0.98, 0.2) },
-	{ 0.4, 0.5, 8.0,  half2(0.99, -0.15) }
-};
+Wave wave[NWAVES];
 
-const int k = 2;
+const half k = 2.0;
+
+Wave initWave(half freq, half amp, half phase, half2 dir)
+{
+   Wave result;
+   result.freq = freq;
+   result.amp = amp;
+   result.phase = phase;
+   result.dir = dir;
+   return result;
+}
 
 half2 evaluateWave(Wave w, vec2 pos, float t)
 {
-    vec2 result;
+   vec2 result;
 
-    float wavePos = dot(w.dir, pos)*w.freq + t*w.phase;
-    half waveAmp = sin(wavePos)*0.5 + 0.5;
+   float wavePos = dot(w.dir, pos)*w.freq + t*w.phase;
+   half waveAmp = sin(wavePos)*0.5 + 0.5;
 
-    result.x = w.amp * pow(waveAmp, k);
-    result.y = k*w.freq*w.amp * pow(waveAmp, k-1) * cos(wavePos);
+   result.x = w.amp * pow(waveAmp, k);
+   result.y = k*w.freq*w.amp * pow(waveAmp, k-1.0) * cos(wavePos);
 
-    return result;
+   return result;
 }
 
 void main()
 {
-    vec4 P = gl_Vertex;
+   vec4 P = gl_Vertex;
 
-    // sum waves
-	vec2 dd = vec2(0.0, 0.0);
-	for(int i=0; i<NWAVES; i++) {
-    	vec2 waveEval = evaluateWave(wave[i], P.xy, Time);
-    	P.z += waveEval.x;
-        dd += waveEval.y * wave[i].dir;
-    }
+   wave[0] = initWave( 0.2, 0.9, 12.0, half2(1.0, 0.0) );
+   wave[1] = initWave( 0.3, 0.7, 9.0,  half2(0.98, 0.2) );
+   wave[2] = initWave( 0.4, 0.5, 8.0,  half2(0.99, -0.15) );
 
-    gl_Position = gl_ModelViewProjectionMatrix * P;
+   // sum waves
+   vec2 dd = vec2(0.0, 0.0);
+   for(int i=0; i<NWAVES; i++) {
+      vec2 waveEval = evaluateWave(wave[i], P.xy, Time);
+      P.z += waveEval.x;
+      dd += waveEval.y * wave[i].dir;
+   }
 
-	// compute tangent basis
-    half3 B = half3(1.0, 0.0, dd.x);
-    half3 T = half3(0.0, 1.0, dd.y);
-    half3 N = half3(-dd.x, -dd.y, 1);
+   gl_Position = gl_ModelViewProjectionMatrix * P;
 
-	// compute the 3x3 tranform from tangent space to object space
-	// first rows are the tangent and binormal scaled by the bump scale
-	ObjToTangentSpace[0] = cBumpScale * normalize(T);
-	ObjToTangentSpace[1] = cBumpScale * normalize(B);
-	ObjToTangentSpace[2] = normalize(N);
+   // compute tangent basis
+   half3 B = half3(1.0, 0.0, dd.x);
+   half3 T = half3(0.0, 1.0, dd.y);
+   half3 N = half3(-dd.x, -dd.y, 1);
 
-	float texTime = Time*cBumpSpeed;
-    gl_TexCoord[0].xy = gl_Vertex.xy*cTexScale + texTime;
-    gl_TexCoord[1].xy = gl_Vertex.xy*(2*cTexScale) - texTime;
+   // compute the 3x3 tranform from tangent space to object space
+   // first rows are the tangent and binormal scaled by the bump scale
+   ObjToTangentSpace[0] = cBumpScale * normalize(T);
+   ObjToTangentSpace[1] = cBumpScale * normalize(B);
+   ObjToTangentSpace[2] = normalize(N);
 
-    EyeVec = normalize(gl_Vertex.xyz - EyePos.xyz);
+   float texTime = Time*cBumpSpeed;
+   gl_TexCoord[0].xy = gl_Vertex.xy*cTexScale + texTime;
+   gl_TexCoord[1].xy = gl_Vertex.xy*(2.0*cTexScale) - texTime;
+
+   EyeVec = normalize(gl_Vertex.xyz - EyePos.xyz);
 }
