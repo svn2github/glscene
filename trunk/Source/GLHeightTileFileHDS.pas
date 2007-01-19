05f879a4ef1aff3ea881a96f3e1635970b3648ee
rjklindsay <rjklindsay@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2007-01-19 13:58:33 +0000

    Added 'Inverted' property to TGLHeightTileFile. The TGLTerrainRender renders a mirror image of your terrain data. Set the TGLHeightTileFile.Inverted property to false, to flip the terrain back.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3494 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLHeightTileFileHDS.pas
+++ trunk/Source/GLHeightTileFileHDS.pas
@@ -2,6 +2,10 @@
 {: HeightDataSource for the HTF (HeightTileFile) format.<p>
 
 	<b>History : </b><font size=-1><ul>
+      <li>19/01/04 - LIN -Bug fix/workaround: Added 'Inverted' property to GLHeightTileFieHDS
+                          Set Inverted to false, if you DONT want your rendered
+                          terrain to be a mirror image of your height data.
+                          (Defaults to true, so it doesnt affect existing apps);
       <li>29/01/03 - EG - Creation
 	</ul></font>
 }
@@ -20,6 +24,7 @@ type
 	   private
 	      { Private Declarations }
          FInfiniteWrap : Boolean;
+         FInverted     : Boolean;
          FHTFFileName : String;
          FHTF : THeightTileFile;
          FMinElevation : Integer;
@@ -28,6 +33,7 @@ type
 	      { Protected Declarations }
          procedure SetHTFFileName(const val : String);
          procedure SetInfiniteWrap(val : Boolean);
+         procedure SetInverted(val : Boolean);
          procedure SetMinElevation(val : Integer);
 
          procedure StartPreparingData(heightData : THeightData); override;
@@ -46,12 +52,15 @@ type
          property HTFFileName : String read FHTFFileName write SetHTFFileName;
          {: If true the height field is wrapped indefinetely. }
          property InfiniteWrap : Boolean read FInfiniteWrap write SetInfiniteWrap default True;
+         {: If true the height data is inverted.(Top to bottom) }
+         property Inverted : Boolean read FInverted write SetInverted default True;
          {: Minimum elevation of the tiles that are considered to exist.<p>
             This property can typically be used to hide underwater tiles. }
          property MinElevation : Integer read FMinElevation write SetMinElevation default -32768;
 
          property MaxPoolSize;
          property DefaultHeight;
+         //property HeightTileFile :THeightTileFile read FHTF;   //Used for inverting the terrain
 	end;
 
 // ------------------------------------------------------------------
@@ -74,6 +83,7 @@ constructor TGLHeightTileFileHDS.Create(AOwner: TComponent);
 begin
 	inherited Create(AOwner);
    FInfiniteWrap:=True;
+   FInverted:=True;
    FMinElevation:=-32768;
 end;
 
@@ -100,11 +110,20 @@ end;
 //
 procedure TGLHeightTileFileHDS.SetInfiniteWrap(val : Boolean);
 begin
-   if FInfiniteWrap<>val then begin
-      FInfiniteWrap:=val;
-      MarkDirty;
-   end;
+  if FInfiniteWrap=val then exit;
+  FInfiniteWrap:=val;
+  MarkDirty;
 end;
+
+// SetInfiniteWrap
+//
+procedure TGLHeightTileFileHDS.SetInverted(val : Boolean);
+begin
+  if FInverted=Val then exit;
+  FInverted:=val;
+  MarkDirty;
+end;
+
 
 // SetMinElevation
 //
@@ -124,6 +143,16 @@ var
    htfTile : PHeightTile;
    htfTileInfo : PHeightTileInfo;
    x, y : Integer;
+   YPos:integer;
+   //YInx,XInx:integer;
+   //Yinx,Yinv:integer;
+   inY,outY:integer;
+   Yinx:integer;
+
+
+   PLineIn, PLineOut : ^PSmallIntArray;
+
+   LineDataSize:integer;
 begin
    // access htf data
    if not Assigned(FHTF) then begin
@@ -133,19 +162,21 @@ begin
       end;
       FHTF:=THeightTileFile.Create(FHTFFileName);
       Assert(FHTF.TileSize=heightData.Size,
-             'HTF TileSize and HeightData size don''t match');
+             'HTF TileSize and HeightData size don''t match.('+IntToStr(FHTF.TileSize)+' , '+Inttostr(heightData.Size)+')');
    end;
 
    // retrieve data and place it in the heightData
    with heightData do begin
+      if Inverted then YPos:=YTop
+                  else YPos:=-YTop;
       if InfiniteWrap then begin
          x:=XLeft mod FHTF.SizeX;
          if x<0 then x:=x+FHTF.SizeX;
-         y:=YTop mod FHTF.SizeY;
+         y:=YPos mod FHTF.SizeY;
          if y<0 then y:=y+FHTF.SizeY;
          htfTile:=FHTF.GetTile(x, y, @htfTileInfo);
       end else begin
-         htfTile:=FHTF.GetTile(XLeft, YTop, @htfTileInfo);
+         htfTile:=FHTF.GetTile(XLeft, YPos, @htfTileInfo);
       end;
       if (htfTile=nil) or (htfTileInfo.max<=FMinElevation) then begin
          // non-aligned tiles aren't handled (would be slow anyway)
@@ -153,7 +184,20 @@ begin
       end else begin
          oldType:=DataType;
          Allocate(hdtSmallInt);
-         Move(htfTile.data[0], SmallIntData^, DataSize);
+
+         if Inverted then Move(htfTile.data[0], SmallIntData^, DataSize)
+         else begin // invert the terrain (top to bottom) To compensate for the inverted terrain renderer
+           LineDataSize:=DataSize div size;
+           for y:=0 to size-1 do begin
+             inY:=y*HeightData.Size;
+             outY:=((size-1)-y)*HeightData.Size;
+             PLineIn :=@htfTile.data[inY];
+             PLineOut:=@heightData.SmallIntData[outY];
+             Move(PLineIn^,PLineOut^,LineDataSize);
+           end;
+         end;
+         //Move(htfTile.data[0], SmallIntData^, DataSize);
+
          if oldType<>hdtSmallInt then
             DataType:=oldType;
          DataState:=hdsReady;
@@ -161,6 +205,13 @@ begin
          HeightMax:=htfTileInfo.max;
       end;
    end;
+
+
+
+
+
+
+
 end;
 
 // ------------------------------------------------------------------
