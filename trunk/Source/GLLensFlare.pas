aa4f9f0dba3ad6e3e88897ac512fe0263c26b241
da_stranger <da_stranger@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2007-03-22 08:05:18 +0000

    Cleanup after previous fix - now object does not igore its children in picking state Removed "unsafe type/unsafe code" warnings
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3745 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLLensFlare.pas
+++ trunk/Source/GLLensFlare.pas
@@ -5,6 +5,9 @@
 {: Lens flare object.<p>
 
 	<b>History : </b><font size=-1><ul>
+      <li>22/03/07 - DaStr - Cleanup after previous fix - now object does not
+                             igore its children in picking state
+                             Removed "unsafe type/unsafe code" warnings
       <li>15/03/07 - DaStr - Removed flicker that occured when LensFlare was
                              rendered in a picking state (BugTracker ID = 1681031)
       <li>19/04/04 - EG - Fixed occlusion test and pojection matrix stack issues
@@ -218,6 +221,9 @@ implementation
 // ------------------------------------------------------------------
 
 uses GLUtils;
+
+{$WARN UNSAFE_TYPE OFF}
+{$WARN UNSAFE_CODE OFF}
 
 // ------------------
 // ------------------ TGLFlareGradient ------------------
@@ -491,7 +497,13 @@ var
    oldSeed : LongInt;
    projMatrix : TMatrix;
 begin
-   if (rci.drawState = dsPicking) then Exit;
+   if (rci.drawState = dsPicking) then
+   begin
+      if Count <> 0 then
+         Self.RenderChildren(0, Count - 1, rci);
+      Exit;
+   end;
+
    SetVector(v, AbsolutePosition);
    // are we looking towards the flare?
    rv:=VectorSubtract(v, PAffineVector(@rci.cameraPosition)^);
@@ -502,7 +514,7 @@ begin
                        and (screenPos[1]<rci.viewPortSize.cy) and (screenPos[1]>=0);
    end else flareInViewPort:=False;
 
-   dynamicSize:=Dynamic and not (csDesigning in ComponentState);
+   dynamicSize:= FDynamic and not (csDesigning in ComponentState);
    if dynamicSize then begin
       // make the glow appear/disappear progressively
       if flareInViewPort and FlareIsNotOccluded then begin
