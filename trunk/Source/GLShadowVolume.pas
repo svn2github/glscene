fbb4bd7fa890b17aa57ed6c2e54f0825cc6382d5
da_stranger <da_stranger@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2007-03-30 08:29:41 +0000

    Added $I GLScene.inc
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3825 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLShadowVolume.pas
+++ trunk/Source/GLShadowVolume.pas
@@ -10,6 +10,7 @@
    or the casters will be rendered incorrectly.<p>
 
 	<b>History : </b><font size=-1><ul>
+      <li>30/03/07 - DaStr - Added $I GLScene.inc
       <li>28/03/07 - DaStr - Renamed parameters in some methods
                              (thanks Burkhard Carstens) (Bugtracker ID = 1678658)
       <li>08/12/04 - DB - Fixed bug in TGLShadowVolumeCaster.SetCaster
@@ -26,6 +27,8 @@
 unit GLShadowVolume;
 
 interface
+
+{$I GLScene.inc}
 
 uses Classes, GLScene, VectorGeometry, OpenGL1x, GLMisc, GLSilhouette, GLTexture,
   GLCrossPlatform, PersistentClasses, GeometryBB;
