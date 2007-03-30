2b2658f88953122d57387c5a177c3e664b24848a
da_stranger <da_stranger@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2007-03-30 11:27:40 +0000

    Added TVSyncMode type and constants.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3833 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Platform/GLViewer.pas
+++ trunk/Source/Platform/GLViewer.pas
@@ -7,6 +7,7 @@
 
     History:
       <li>30/03/07 - DaStr - Another update after the previous fix (removed class())
+                             Added TVSyncMode type and constants.
       <li>24/03/07 - DaStr - Update for Windows after the previous fix
       <li>21/03/07 - DaStr - Improved Cross-Platform compatibility
                              (thanks Burkhard Carstens) (Bugtracker ID = 1684432)
@@ -31,12 +32,31 @@ uses
 type
 {$IFDEF FPC}  //For FPC, always use LCLViewer
   TGLSceneViewer = GLLCLViewer.TGLSceneViewer;
+  TVSyncMode = GLLCLViewer.TVSyncMode;
 {$ELSE}  // if not FPC then
   {$IFDEF UNIX}  // kylix
-  TGLSceneViewer = GLLinuxViewer.TGLLinuxSceneViewer;
+    TGLSceneViewer = GLLinuxViewer.TGLLinuxSceneViewer;
+    TVSyncMode = GLLinuxViewer.TVSyncMode;
   {$ENDIF UNIX}
   {$IFDEF MSWINDOWS} // windows
-  TGLSceneViewer = GLWin32Viewer.TGLSceneViewer;
+    TGLSceneViewer = GLWin32Viewer.TGLSceneViewer;
+    TVSyncMode = GLWin32Viewer.TVSyncMode;
+  {$ENDIF MSWINDOWS}
+{$ENDIF FPC}
+
+const
+{$IFDEF FPC}  // FPC.
+  // TVSyncMode.
+  vsmSync = GLLCLViewer.vsmSync;
+  vsmNoSync = GLLCLViewer.vsmNoSync;
+{$ELSE}
+  {$IFDEF UNIX}  // Kylix.
+    vsmSync = GLLinuxViewer.vsmSync;
+    vsmNoSync = GLLinuxViewer.vsmNoSync;
+  {$ENDIF UNIX}
+  {$IFDEF MSWINDOWS} // Windows.
+    vsmSync = GLWin32Viewer.vsmSync;
+    vsmNoSync = GLWin32Viewer.vsmNoSync;
   {$ENDIF MSWINDOWS}
 {$ENDIF FPC}
 
