cb1de910766b5c305f5d6ee5f93f444d092b4946
da_stranger <da_stranger@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2007-08-20 16:36:38 +0000

    Initial version
    git-svn-id: http://svn.code.sf.net/p/glscene/code@3936 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Demos/interface/SimpleNavigation/Unit1.pas
@@ -0,0 +1,46 @@
+// a Demo with absolutely no code :)
+unit Unit1;
+
+interface
+
+{$I GLScene.inc}
+
+uses
+  // VCL
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs,
+
+  // GLScene
+  GLScene, GLObjects, GLMisc, GLWin32Viewer, GLCadencer, GLSimpleNavigation;
+
+type
+  TForm1 = class(TForm)
+    GLScene1: TGLScene;
+    GLSceneViewer1: TGLSceneViewer;
+    GLCamera1: TGLCamera;
+    GLCadencer1: TGLCadencer;
+    GLCube1: TGLCube;
+    GLLightSource1: TGLLightSource;
+    GLSimpleNavigation1: TGLSimpleNavigation;
+    procedure GLCadencer1Progress(Sender: TObject; const deltaTime,
+      newTime: Double);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.GLCadencer1Progress(Sender: TObject; const deltaTime,
+  newTime: Double);
+begin
+  GLSceneViewer1.Invalidate;
+end;
+
+end.
