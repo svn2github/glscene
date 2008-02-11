116ba45a158be4a6d308635d769402c9e379c602
da_stranger <da_stranger@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2008-02-11 01:39:02 +0000

    Bugfixed TPersistentObjectList.Move() once again  (BugTracker ID = 1857974)
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4046 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Base/PersistentClasses.pas
+++ trunk/Source/Base/PersistentClasses.pas
@@ -13,6 +13,9 @@
    Internal Note: stripped down versions of XClasses & XLists.<p>
 
 	<b>History : </b><font size=-1><ul>
+      <li>11/02/08 - DaStr - Bugfixed TPersistentObjectList.Move() once again
+                             (BugTracker ID = 1857974)
+                             (thanks Yann PAPOUIN and Burkhard Carstens)
       <li>04/02/08 - DaStr - Bugfixed TPersistentObjectList.Move() (BugTracker ID = 1857974)
       <li>06/03/07 - DaStr - Added TGLOwnedPersistent
       <li>04/01/04 - EG - Fixed ReadString & ReadWideString for empty strings (thx Kenguru)
@@ -1055,10 +1058,10 @@ begin
       item:=List[curIndex];
       if curIndex<newIndex then begin
          // curIndex+1 necessarily exists since curIndex<newIndex and newIndex<Count
-         System.Move(List[curIndex+1], List[curIndex], (Count-curIndex-1)*SizeOf(TObject));
+         System.Move(List[curIndex+1], List[curIndex], (NewIndex-CurIndex)*SizeOf(TObject));
       end else begin
          // newIndex+1 necessarily exists since newIndex<curIndex and curIndex<Count
-         System.Move(List[newIndex], List[newIndex+1], (Count-newIndex-1)*SizeOf(TObject));
+         System.Move(List[newIndex], List[newIndex+1], (CurIndex-NewIndex)*SizeOf(TObject));
       end;
       List[newIndex]:=item;
    end;
