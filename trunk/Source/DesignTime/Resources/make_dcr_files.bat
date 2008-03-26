c383e1ba1d0e3afd304d6f1ad81944fa400e3283
danbartlett <danbartlett@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2008-03-26 00:11:28 +0000

    *** empty log message ***
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4089 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/DesignTime/Resources/make_dcr_files.bat
+++ trunk/Source/DesignTime/Resources/make_dcr_files.bat
@@ -5,6 +5,7 @@ BRCC32 -r -foGLSceneObjects.dcr GLSceneObjects.rc
 BRCC32 -r -fononGLSceneVCL.dcr nonGLSceneVCL.rc
 BRCC32 -r -foGLSceneVCLBass.dcr GLSceneVCLBass.rc
 BRCC32 -r -foGLSceneVCLFMod.dcr GLSceneVCLFMod.rc
+BRCC32 -r -foGLSceneVCLOpenAL.dcr GLSceneVCLOpenAL.rc
 BRCC32 -r -foGLSceneVCLODE.dcr GLSceneVCLODE.rc
 BRCC32 -r -foGLSceneVCLSDL.dcr GLSceneVCLSDL.rc
 BRCC32 -r -foGLSceneVCLCg.dcr GLSceneVCLCg.rc
