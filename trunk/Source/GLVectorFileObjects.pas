c832879e69b7e6e9266d3f2fdd42ab9c3077f5d9
da_stranger <da_stranger@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2008-04-30 22:48:21 +0000

    Added a "history" entry after previous commit:   Implemented TGLBaseMesh.BarycenterAbsolutePosition()   Bugfixed TGLBaseMesh.AxisAlignedDimensionsUnscaled()
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4133 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLVectorFileObjects.pas
+++ trunk/Source/GLVectorFileObjects.pas
@@ -6,9 +6,12 @@
 	Vector File related objects for GLScene<p>
 
 	<b>History :</b><font size=-1><ul>
+      <li>01/05/08 - DaStr - Implemented TGLBaseMesh.BarycenterAbsolutePosition()
+                             Bugfixed TGLBaseMesh.AxisAlignedDimensionsUnscaled()
       <li>06/04/08 - DaStr - TMeshObjectList.MorphTo() and Lerp() are now virtual
       <li>06/06/07 - DaStr - Added GLColor to uses (BugtrackerID = 1732211)
-      <li>16/05/07 - PvD - Applied fixes to skeletonmesh to fix problems with physics engines. (Bugtracker ID = 1719652)
+      <li>16/05/07 - PvD - Applied fixes to skeletonmesh to fix problems with
+                            physics engines. (Bugtracker ID = 1719652)
       <li>15/05/07 - LC - Added workaround for ATI bug in TFGVertexIndexList. (Bugtracker ID = 1719611)
       <li>13/05/07 - LC - Fixed AV bug in TMeshObject.BufferArrays (Bugtracker ID = 1718033)
       <li>03/04/07 - LC - Added VBO support for TextureEx (Bugtracker ID = 1693378) 
