4db09a1926779a865100f01f27acdd6cd7b6fecf
yarunderoaker <yarunderoaker@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2010-01-24 11:34:55 +0000

    Removed initialization form constructor
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4546 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLPBuffer.pas
+++ trunk/Source/GLPBuffer.pas
@@ -4,8 +4,13 @@
 {: GLPBuffer<p>
 
   Simple handling of pixelbuffers.<p>
+  TGLPixelBuffer can be used for offscreen rendering.<p>
+  Goal is that it does not require the creation of a window.<p>
 
   <b>Historique : </b><font size=-1><ul>
+      <li>24/01/10 - Yar - Removed initialization form constructor,
+                           changes of use of desktop windows on foreground
+                           (improved work on Delphi7 and Lazarus)
       <li>21/01/10 - DaStr - Bugfixed range check error
       <li>22/01/10 - Yar - Added to GLScene
   </ul></font>
@@ -31,46 +36,55 @@ unit GLPBuffer;
 interface
 
 uses
+{$IFDEF MSWINDOWS}
   Windows,
   Classes,
   SysUtils,
+{$ENDIF}
   OpenGL1x;
 
 type
 
-  TPixelBuffer = class
+  TGLPixelBuffer = class
   private
     DC: HDC;
     RC: HGLRC;
     ParentDC: HDC;
     ParentRC: HGLRC;
-    fHandle: HPBUFFERARB;
-    fWidth: GLInt;
-    fHeight: GLInt;
-    fTextureID: GLUInt;
+    fHandle: GLuint;
+    fWidth: GLuint;
+    fHeight: GLuint;
+    fTextureID: GLuint;
   public
-    constructor Create(pWidth, pHeight: integer);
+    constructor Create;
     destructor Destroy; override;
 
-    function IsLost: boolean;
+    procedure Initialize(pWidth, pHeight: integer);
+    function  IsLost: boolean;
     procedure Enable;
     procedure Disable;
     procedure Bind;
     procedure Release;
 
-    property Width: GLInt read fWidth;
-    property Height: GLInt read fHeight;
-    property TextureID: GLUInt read fTextureID;
+    property Handle: GLuint read fHandle;
+    property Width: GLuint read fWidth;
+    property Height: GLuint read fHeight;
+    property TextureID: GLuint read fTextureID;
   end;
 
-  EPixelBuffer = class(Exception);
-
-var
-  PixelBuffer: TPixelBuffer;
+  EGLPixelBuffer = class(Exception);
 
 implementation
 
-constructor TPixelBuffer.Create(pWidth, pHeight: integer);
+constructor TGLPixelBuffer.Create;
+begin
+  inherited Create;
+  ParentDC := 0;
+  ParentRC := 0;
+end;
+
+procedure TGLPixelBuffer.Initialize(pWidth, pHeight: integer);
+{$IFDEF MSWINDOWS}
 const
   PixelFormatAttribs: array[0..12] of TGLInt =
    (WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
@@ -84,35 +98,33 @@ const
     (WGL_TEXTURE_FORMAT_ARB, WGL_TEXTURE_RGBA_ARB,
     WGL_TEXTURE_TARGET_ARB,
     WGL_TEXTURE_2D_ARB, 0);
+
   EmptyF: TGLFLoat = 0;
 var
   PFormat: array[0..64] of TGLInt;
   NumPFormat: TGLUInt;
   TempW, TempH: TGLUInt;
+{$ELSE}
+
+{$ENDIF}
 begin
-  inherited Create;
-  ParentDC := wglGetCurrentDC;
-  ParentRC := wglGetCurrentContext;
   fWidth := pWidth;
   fHeight := pHeight;
+{$IFDEF MSWINDOWS}
+  ParentDC := wglGetCurrentDC;
+  ParentRC := wglGetCurrentContext;
   if ParentDC = 0 then
   begin
-    ParentDC := GetDC(GetDesktopWindow);
+    ParentDC := GetDC(GetForegroundWindow);
     if ParentDC = 0 then
-    begin
-      raise EPixelBuffer.Create(
+      raise EGLPixelBuffer.Create(
         'PixelBuffer->wglGetCurrentDC->Couldn''t obtain valid device context');
-      exit;
-    end;
   end;
 
   if not wglChoosePixelFormatARB(ParentDC, @PixelFormatAttribs, @EmptyF,
     Length(PFormat), @PFormat, @NumPFormat) then
-  begin
-    raise EPixelBuffer.Create(
+    raise EGLPixelBuffer.Create(
       'PixelBuffer->wglChoosePixelFormatARB->No suitable pixelformat found');
-    exit;
-  end;
 
   fHandle := wglCreatePBufferARB(ParentDC, PFormat[0], fWidth, fHeight,
     @PixelBufferAttribs);
@@ -122,74 +134,98 @@ begin
     wglQueryPbufferARB(fHandle, WGL_PBUFFER_HEIGHT_ARB, @TempH);
   end
   else
-  begin
-    raise EPixelBuffer.Create(
+    raise EGLPixelBuffer.Create(
       'PixelBuffer->wglCreatePBufferARB->Couldn''t obtain valid handle');
-    exit;
-  end;
 
   DC := wglGetPBufferDCARB(fHandle);
   if DC = 0 then
-  begin
-    raise EPixelBuffer.Create(
+    raise EGLPixelBuffer.Create(
       'PixelBuffer->wglGetPBufferDCARB->Couldn''t obtain valid DC for PBuffer');
-    exit;
-  end;
 
   RC := wglCreateContext(DC);
   if RC = 0 then
-  begin
-    raise EPixelBuffer.Create(
+    raise EGLPixelBuffer.Create(
       'PixelBuffer->wglGetPBufferDCARB->Couldn''t create rendercontext for PBuffer');
-    exit;
-  end;
 
   wglMakeCurrent(DC, RC);
   glGenTextures(1, @fTextureID);
+{$ELSE}
+
+{$ENDIF}
 end;
 
-destructor TPixelBuffer.Destroy;
+destructor TGLPixelBuffer.Destroy;
 begin
+{$IFDEF MSWINDOWS}
   Disable;
-  wglDeleteContext(RC);
-  wglReleasePbufferDCARB(fHandle, DC);
-  wglDestroyPbufferARB(fHandle);
+  if fHandle<>0 then
+  begin
+    wglDeleteContext(RC);
+    wglReleasePbufferDCARB(fHandle, DC);
+    wglDestroyPbufferARB(fHandle);
+  end;
+{$ELSE}
+
+{$ENDIF}
   inherited;
 end;
 
-function TPixelBuffer.IsLost: boolean;
+function TGLPixelBuffer.IsLost: boolean;
 var
   Flag: TGLUInt;
 begin
+{$IFDEF MSWINDOWS}
+  Assert(fHandle<>0);
   Result := False;
   wglQueryPbufferARB(fHandle, WGL_PBUFFER_LOST_ARB, @Flag);
   if Flag <> 0 then
     Result := True;
+{$ELSE}
+
+{$ENDIF}
 end;
 
-procedure TPixelBuffer.Enable;
+procedure TGLPixelBuffer.Enable;
 begin
+{$IFDEF MSWINDOWS}
   ParentDC := wglGetCurrentDC;
   ParentRC := wglGetCurrentContext;
   wglMakeCurrent(DC, RC);
+{$ELSE}
+
+{$ENDIF}
 end;
 
-procedure TPixelBuffer.Disable;
+procedure TGLPixelBuffer.Disable;
 begin
+{$IFDEF MSWINDOWS}
   if (ParentDC = 0) or (ParentRC = 0) then
     wglMakeCurrent(0, 0)
   else
     wglMakeCurrent(ParentDC, ParentRC);
+{$ELSE}
+
+{$ENDIF}
 end;
 
-procedure TPixelBuffer.Bind;
+procedure TGLPixelBuffer.Bind;
 begin
+{$IFDEF MSWINDOWS}
+  Assert(fHandle<>0);
   wglBindTexImageARB(fHandle, WGL_FRONT_LEFT_ARB);
+{$ELSE}
+
+{$ENDIF}
 end;
 
-procedure TPixelBuffer.Release;
+procedure TGLPixelBuffer.Release;
 begin
+{$IFDEF MSWINDOWS}
+  Assert(fHandle<>0);
   wglReleaseTexImageARB(fHandle, WGL_FRONT_LEFT_ARB);
+{$ELSE}
+
+{$ENDIF}
 end;
 
 end.
