925078e0139b64621fd652d3edd5d35ee015dbe1
yarunderoaker <yarunderoaker@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2010-03-24 15:07:40 +0000

    Fix for Unix-based systems
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4710 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/PropertyEditors/FLibMaterialPickerLCL.pas
+++ trunk/Source/PropertyEditors/FLibMaterialPickerLCL.pas
@@ -4,7 +4,8 @@
 	Allows choosing a material in a material library<p>
 
     <b>Historique : </b><font size=-1><ul>
-
+      <li>24/03/10 - Yar - Fix for Unix-based systems
+                           (thanks to Rustam Asmandiarov aka Predator)
     </ul></font>
 }
 
@@ -15,9 +16,11 @@ interface
 {$i GLScene.inc}
 
 uses
-  lresources,
-  Forms, StdCtrls, Buttons, FRMaterialPreviewLCL,dialogs,
-  Controls, Classes, GLViewer, GLMaterial,ToolWin;
+  lresources, LCLIntf,
+  Controls, Classes, Forms, StdCtrls, Buttons, dialogs, ToolWin,Graphics,
+  GLViewer, GLMaterial, GLScene, GLObjects, GLTexture,
+  GLHUDObjects, GLTeapot,  GLGeomObjects, GLColor,
+  GLCoordinates;
 
 type
 
@@ -29,17 +32,43 @@ type
     Label2: TLabel;
     BBOk: TBitBtn;
     BBCancel: TBitBtn;
-    MPPreview: TRMaterialPreview;
+    GLScene1: TGLScene;
+    SceneViewer: TGLSceneViewer;
+    CBObject: TComboBox;
+    Camera: TGLCamera;
+    Cube: TGLCube;
+    Sphere: TGLSphere;
+    LightSource: TGLLightSource;
+    CBBackground: TComboBox;
+    BackGroundSprite: TGLHUDSprite;
+    Cone: TGLCone;
+    Teapot: TGLTeapot;
+    World: TGLDummyCube;
+    Light: TGLDummyCube;
+    FireSphere: TGLSphere;
+    GLMaterialLibrary: TGLMaterialLibrary;
     procedure LBMaterialsClick(Sender: TObject);
     procedure LBMaterialsKeyPress(Sender: TObject; var Key: Char);
     procedure LBMaterialsDblClick(Sender: TObject);
-    procedure MPPreviewClick(Sender: TObject);
+    procedure CBObjectChange(Sender: TObject);
+    procedure CBBackgroundChange(Sender: TObject);
+    procedure SceneViewerMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure SceneViewerMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure SceneViewerMouseWheel(Sender: TObject; Shift: TShiftState;
+      WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean);
   private
+    function GetMaterial: TGLMaterial;
+    procedure SetMaterial(const Value: TGLMaterial);
+    property Material : TGLMaterial read GetMaterial write SetMaterial;
     { Diclarations privies }
   public
     { Diclarations publiques }
+    constructor Create(AOwner : TComponent); override;
 	  function Execute(var materialName : TGLLibMaterialName;
                       materialLibrary : TGLMaterialLibrary) : Boolean;
+
   end;
 
 function LibMaterialPicker : TLibMaterialPicker;
@@ -86,10 +115,29 @@ begin
 	end;
 end;
 
+var
+  MX, MY: Integer;
+
+constructor TLibMaterialPicker.Create(AOwner : TComponent);
+begin
+   inherited;
+   {$IFDEF UNIX}
+   SceneViewer.Height:= 160;
+   SceneViewer.Top:= 64;
+   {$ENDIF}
+   BackGroundSprite.Position.X := SceneViewer.Width div 2;
+   BackGroundSprite.Position.Y := SceneViewer.Height div 2;
+   BackGroundSprite.Width := SceneViewer.Width;
+   BackGroundSprite.Height := SceneViewer.Height;
+
+   CBObject.ItemIndex:=0;     CBObjectChange(Self);
+   CBBackground.ItemIndex:=0; CBBackgroundChange(Self);
+end;
+
 procedure TLibMaterialPicker.LBMaterialsClick(Sender: TObject);
 begin
    with LBMaterials do if ItemIndex>=0 then
-      MPPreview.Material:=TGLLibMaterial(Items.Objects[ItemIndex]).Material;
+      Material:=TGLLibMaterial(Items.Objects[ItemIndex]).Material;
 end;
 
 procedure TLibMaterialPicker.LBMaterialsKeyPress(Sender: TObject;
@@ -103,9 +151,71 @@ begin
   BBOk.Click;
 end;
 
-procedure TLibMaterialPicker.MPPreviewClick(Sender: TObject);
+procedure TLibMaterialPicker.CBObjectChange(Sender: TObject);
+var
+   i : Integer;
 begin
+   i:=CBObject.ItemIndex;
+   Cube.Visible   := I = 0;
+   Sphere.Visible := I = 1;
+   Cone.Visible   := I = 2;
+   Teapot.Visible := I = 3;
+end;
 
+procedure TLibMaterialPicker.CBBackgroundChange(Sender: TObject);
+var
+   bgColor : TColor;
+begin
+   case CBBackground.ItemIndex of
+      1 : bgColor:=clWhite;
+      2 : bgColor:=clBlack;
+      3 : bgColor:=clBlue;
+      4 : bgColor:=clRed;
+      5 : bgColor:=clGreen;
+   else
+      bgColor:=clNone;
+   end;
+   with BackGroundSprite.Material do begin
+      Texture.Disabled:=(bgColor<>clNone);
+      FrontProperties.Diffuse.Color:=ConvertWinColor(bgColor);
+   end;
+end;
+
+procedure TLibMaterialPicker.SceneViewerMouseMove(Sender: TObject;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if (ssRight in Shift) and (ssLeft in Shift) then
+    Camera.AdjustDistanceToTarget(1 - 0.01 * (MY - Y))
+  else
+  if (ssRight in Shift) or (ssLeft in Shift) then
+    Camera.MoveAroundTarget(Y - MY, X - MX);
+
+  MX := X;
+  MY := Y;
+end;
+
+procedure TLibMaterialPicker.SceneViewerMouseDown(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  MX := X;
+  MY := Y;
+end;
+
+procedure TLibMaterialPicker.SceneViewerMouseWheel(Sender: TObject;
+  Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+  var Handled: Boolean);
+begin
+  Camera.AdjustDistanceToTarget(1 - 0.1 * (Abs(WheelDelta) / WheelDelta));
+end;
+
+function TLibMaterialPicker.GetMaterial: TGLMaterial;
+begin
+  Result := GLMaterialLibrary.Materials[0].Material;
+end;
+
+procedure TLibMaterialPicker.SetMaterial(const Value: TGLMaterial);
+begin
+  GLMaterialLibrary.Materials[0].Material.Assign(Value);
 end;
 
 initialization
--- trunk/Source/PropertyEditors/FMaterialEditorFormLCL.pas
+++ trunk/Source/PropertyEditors/FMaterialEditorFormLCL.pas
@@ -3,43 +3,72 @@
    Editor window for a material (with preview).<p>
 
    <b>Historique : </b><font size=-1><ul>
-
+      <li>24/03/10 - Yar - Fix for Unix-based systems
+                           (thanks to Rustam Asmandiarov aka Predator)
    </ul></font>
 }
 unit FMaterialEditorFormLCL;
 
 interface
-
+{$MODE DELPHI}
 {$i GLScene.inc}
 
 uses
 
   {$IFDEF MSWINDOWS} Windows,{$ENDIF} FRTrackBarEditLCL, Forms,
-  FRMaterialPreviewLCL, FRColorEditorLCL, ComCtrls, FRFaceEditorLCL, StdCtrls, Controls,
-  Classes, GLTexture, Buttons, TypInfo, FRTextureEditLCL, GLViewer,
-  GLMaterial,lresources;
+  FRColorEditorLCL, ComCtrls, FRFaceEditorLCL, StdCtrls, Controls,
+  Graphics,
+  Classes, Buttons, TypInfo, FRTextureEditLCL, 
+  GLScene, GLObjects, GLTexture, GLHUDObjects, GLTeapot,
+  GLGeomObjects, GLColor, GLLCLViewer, GLCoordinates,
+  GLCrossPlatform, BaseClasses, GLMaterial
+  ,lresources;
 
 type
   TMaterialEditorForm = class(TForm)
-	 PageControl1: TPageControl;
+    PageControl1: TPageControl;
     TSFront: TTabSheet;
     TSBack: TTabSheet;
     TSTexture: TTabSheet;
     GroupBox1: TGroupBox;
-    MPPreview: TRMaterialPreview;
     BBOk: TBitBtn;
     BBCancel: TBitBtn;
     RTextureEdit: TRTextureEdit;
     CBBlending: TComboBox;
     Label1: TLabel;
+    GLScene1: TGLScene;
+    SceneViewer: TGLSceneViewer;
+    CBObject: TComboBox;
+    Camera: TGLCamera;
+    Cube: TGLCube;
+    Sphere: TGLSphere;
+    LightSource: TGLLightSource;
+    CBBackground: TComboBox;
+    BackGroundSprite: TGLHUDSprite;
+    Cone: TGLCone;
+    Teapot: TGLTeapot;
+    World: TGLDummyCube;
+    Light: TGLDummyCube;
+    FireSphere: TGLSphere;
+    GLMaterialLibrary: TGLMaterialLibrary;
+    procedure CBObjectChange(Sender: TObject);
+    procedure CBBackgroundChange(Sender: TObject);
+    procedure SceneViewerMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure SceneViewerMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure SceneViewerMouseWheel(Sender: TObject; Shift: TShiftState;
+      WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean);
     procedure OnMaterialChanged(Sender : TObject);
   private
     { Diclarations privies }
+    function GetMaterial: TGLMaterial;
+    procedure SetMaterial(const Value: TGLMaterial);
   public
     { Diclarations publiques }
     constructor Create(AOwner : TComponent); override;
-
-	 function Execute(var material : TGLMaterial) : Boolean;
+    function Execute(var amaterial : TGLMaterial) : Boolean;
+    property Material : TGLMaterial read GetMaterial write SetMaterial;
   end;
 
 function MaterialEditorForm : TMaterialEditorForm;
@@ -51,6 +80,7 @@ var
 	vMaterialEditorForm : TMaterialEditorForm;
         FEFront: TRFaceEditor;
         FEBack: TRFaceEditor;
+        MX, MY: Integer;
 
 function MaterialEditorForm : TMaterialEditorForm;
 begin
@@ -88,19 +118,27 @@ begin
     FEFront.OnChange:=OnMaterialChanged;
     FEBack.OnChange:=OnMaterialChanged;
     RTextureEdit.OnChange:=OnMaterialChanged;
+
+    BackGroundSprite.Position.X := SceneViewer.Width div 2;
+    BackGroundSprite.Position.Y := SceneViewer.Height div 2;
+    BackGroundSprite.Width := SceneViewer.Width;
+    BackGroundSprite.Height := SceneViewer.Height;
+
+    CBObject.ItemIndex:=0;     CBObjectChange(Self);
+    CBBackground.ItemIndex:=0; CBBackgroundChange(Self);
 end;
 
 // Execute
 //
-function TMaterialEditorForm.Execute(var material : TGLMaterial) : Boolean;
+function TMaterialEditorForm.Execute(var amaterial : TGLMaterial) : Boolean;
 begin
-   with material do begin
+   with amaterial do begin
       FEFront.FaceProperties:=FrontProperties;
 		FEBack.FaceProperties:=BackProperties;
 		RTextureEdit.Texture:=Texture;
       CBBlending.ItemIndex:=Integer(BlendingMode);
 	end;
-	MPPreview.Material:=material;
+	Self.Material:=material;
 	Result:=(ShowModal=mrOk);
 	if Result then with material do begin
 		FrontProperties:=FEFront.FaceProperties;
@@ -114,7 +152,7 @@ end;
 //
 procedure TMaterialEditorForm.OnMaterialChanged(Sender : TObject);
 begin
-   with MPPreview.Material do begin
+   with Self.Material do begin
       FrontProperties:=FEFront.FaceProperties;
 		BackProperties:=FEBack.FaceProperties;
 		Texture:=RTextureEdit.Texture;
@@ -122,6 +160,74 @@ begin
 	end;
  // MPPreview.Render;
 end;
+
+procedure TMaterialEditorForm.CBObjectChange(Sender: TObject);
+var
+   i : Integer;
+begin
+   i:=CBObject.ItemIndex;
+   Cube.Visible   := I = 0;
+   Sphere.Visible := I = 1;
+   Cone.Visible   := I = 2;
+   Teapot.Visible := I = 3;
+end;
+
+procedure TMaterialEditorForm.CBBackgroundChange(Sender: TObject);
+var
+   bgColor : TColor;
+begin
+   case CBBackground.ItemIndex of
+      1 : bgColor:=clWhite;
+      2 : bgColor:=clBlack;
+      3 : bgColor:=clBlue;
+      4 : bgColor:=clRed;
+      5 : bgColor:=clGreen;
+   else
+      bgColor:=clNone;
+   end;
+   with BackGroundSprite.Material do begin
+      Texture.Disabled:=(bgColor<>clNone);
+      FrontProperties.Diffuse.Color:=ConvertWinColor(bgColor);
+   end;
+end;
+
+procedure TMaterialEditorForm.SceneViewerMouseMove(Sender: TObject;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if (ssRight in Shift) and (ssLeft in Shift) then
+    Camera.AdjustDistanceToTarget(1 - 0.01 * (MY - Y))
+  else
+  if (ssRight in Shift) or (ssLeft in Shift) then
+    Camera.MoveAroundTarget(Y - MY, X - MX);
+
+  MX := X;
+  MY := Y;
+end;
+
+procedure TMaterialEditorForm.SceneViewerMouseDown(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  MX := X;
+  MY := Y;
+end;
+
+procedure TMaterialEditorForm.SceneViewerMouseWheel(Sender: TObject;
+  Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+  var Handled: Boolean);
+begin
+  Camera.AdjustDistanceToTarget(1 - 0.1 * (Abs(WheelDelta) / WheelDelta));
+end;
+
+function TMaterialEditorForm.GetMaterial: TGLMaterial;
+begin
+  Result := GLMaterialLibrary.Materials[0].Material;
+end;
+
+procedure TMaterialEditorForm.SetMaterial(const Value: TGLMaterial);
+begin
+  GLMaterialLibrary.Materials[0].Material.Assign(Value);
+end;
+
 
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
