7522a26e32b408790b3ec5db4ccd415ed126048e
yarunderoaker <yarunderoaker@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2010-04-20 15:19:14 +0000

    Returned
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4783 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/meshes/actor/Unit1.pas
+++ trunk/Demos/meshes/actor/Unit1.pas
@@ -26,15 +26,13 @@
 
 unit Unit1;
 
-{$MODE Delphi}
-
 interface
 
 uses
-  GLCadencer, GLVectorFileObjects, GLScene, GLObjects,
+  Windows, GLCadencer, GLVectorFileObjects, GLScene, GLObjects,
   StdCtrls, Buttons, Controls, ExtCtrls, ComCtrls, Classes, Forms,
-  GLViewer, GLFileMD2, GLGeomObjects, GLCrossPlatform, GLCoordinates,
-  LResources, Graphics;
+  GLWin32Viewer, GLFileMD2, GLGeomObjects, GLCrossPlatform, GLCoordinates,
+  BaseClasses;
 
 type
   TForm1 = class(TForm)
@@ -75,7 +73,10 @@ type
     procedure GLCadencer1Progress(Sender: TObject; const deltaTime,
       newTime: Double);
   private
+    { Déclarations privées }
     mdx, mdy : Integer;
+  public
+	 { Déclarations publiques }
   end;
 
 var
@@ -84,29 +85,21 @@ var
 
 implementation
 
-{$R *.lfm}
+{$R *.DFM}
 
-uses VectorGeometry, SysUtils, FileUtil;
+uses VectorGeometry, SysUtils, Jpeg;
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  path: UTF8String;
-  p: Integer;
 begin
-   path := ExtractFilePath(ParamStrUTF8(0));
-   p := Pos('DemosLCL', path);
-   Delete(path, p+5, Length(path));
-   path := IncludeTrailingPathDelimiter(path) + 'media';
-   SetCurrentDirUTF8(path);
    // Load Actor into GLScene
-   Actor1.LoadFromFile('waste.md2');
-   Actor1.Material.Texture.Image.LoadFromFile('waste.jpg');
+   Actor1.LoadFromFile('..\..\media\waste.md2');
+   Actor1.Material.Texture.Image.LoadFromFile('..\..\media\waste.jpg');
 
    // Load Quake2 animations defaults, for "waste.md2", this is not required
    // since the author did not renamed the frames, and thus, GLScene can
    // recover them from the .MD2, but other authors just made a mess...
    // Loading the default animations takes care of that
-   Actor1.Animations.LoadFromFile('Quake2Animations.aaf');
+   Actor1.Animations.LoadFromFile('..\..\media\Quake2Animations.aaf');
 
    // Scale Actor for put in the Scene
    Actor1.Scale.SetVector(0.04, 0.04, 0.04, 0);
@@ -118,7 +111,7 @@ begin
    CBAnimationsChange(Self);
 
    // Load Texture for ground disk
-   Disk1.Material.Texture.Image.LoadFromFile('clover.jpg');
+   Disk1.Material.Texture.Image.LoadFromFile('..\..\media\clover.jpg');
 end;
 
 procedure TForm1.SBPlayClick(Sender: TObject);
@@ -148,8 +141,8 @@ end;
 procedure TForm1.BBLoadWeaponClick(Sender: TObject);
 begin
    // Load weapon model and texture
-   Actor2.LoadFromFile('WeaponWaste.md2');
-   Actor2.Material.Texture.Image.LoadFromFile('WeaponWaste.jpg');
+   Actor2.LoadFromFile('..\..\media\WeaponWaste.md2');
+   Actor2.Material.Texture.Image.LoadFromFile('..\..\media\WeaponWaste.jpg');
 
    // Get animations frames from the main actor
    Actor2.Animations.Assign(Actor1.Animations);
