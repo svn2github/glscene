90d782fed4547679444e0aade9de6f4cb87b2946
yarunderoaker <yarunderoaker@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2010-06-14 10:33:11 +0000

    Added Filtering Quality ComboBox. Bugfixed texture image class filling.
    git-svn-id: http://svn.code.sf.net/p/glscene/code@4927 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/PropertyEditors/FRTextureEditLCL.pas
+++ trunk/Source/PropertyEditors/FRTextureEditLCL.pas
@@ -1,10 +1,10 @@
 // FRTextureEdit
 {: Egg<p>
 
-	Basic editing frame for TGLTexture<p>
+  Basic editing frame for TGLTexture<p>
 
     <b>Historique : </b><font size=-1><ul>
-
+        <li>14/06/10 - Yar - Added Filtering Quality ComboBox. Bugfixed texture image class filling.
     </ul></font>
 }
 { TODO : Replace STImageClass with a dropdown (polymorphism) }
@@ -17,24 +17,30 @@ interface
 
 uses
   Forms, StdCtrls, Buttons, Controls, Classes, TypInfo,
-  GLTexture, GLGraphics,lresources;
+  GLTexture, GLGraphics, lresources;
 
 type
+
+  { TRTextureEdit }
+
   TRTextureEdit = class(TFrame)
+    CBFilteringQuality: TComboBox;
     Label2: TLabel;
+    Label7: TLabel;
     SBEditImage: TSpeedButton;
     CBMagFilter: TComboBox;
-	 Label3: TLabel;
-	 Label4: TLabel;
-	 CBMinFilter: TComboBox;
-	 CBTextureMode: TComboBox;
-	 Label1: TLabel;
-	 Label5: TLabel;
-	 CBTextureWrap: TComboBox;
-	 CBDisabled: TCheckBox;
-	 CBImageClass: TComboBox;
+    Label3: TLabel;
+    Label4: TLabel;
+    CBMinFilter: TComboBox;
+    CBTextureMode: TComboBox;
+    Label1: TLabel;
+    Label5: TLabel;
+    CBTextureWrap: TComboBox;
+    CBDisabled: TCheckBox;
+    CBImageClass: TComboBox;
     CBImageAlpha: TComboBox;
     Label6: TLabel;
+    procedure CBFilteringQualityChange(Sender: TObject);
     procedure CBMagFilterChange(Sender: TObject);
     procedure CBMinFilterChange(Sender: TObject);
     procedure CBTextureModeChange(Sender: TObject);
@@ -43,25 +49,25 @@ type
     procedure SBEditImageClick(Sender: TObject);
     procedure CBImageClassChange(Sender: TObject);
     procedure CBImageAlphaChange(Sender: TObject);
-	 
+
   private
-	 { Diclarations privies }
-	 FTexture : TGLTexture;
-	 FOnChange : TNotifyEvent;
-	 changeing : Boolean;
+    { Diclarations privies }
+    FTexture: TGLTexture;
+    FOnChange: TNotifyEvent;
+    changeing: boolean;
 
   protected
-	 { Diclarations protigies }
-	 procedure SetTexture(const val : TGLTexture);
-	 procedure DoOnChange; dynamic;
+    { Diclarations protigies }
+    procedure SetTexture(const val: TGLTexture);
+    procedure DoOnChange; dynamic;
 
   public
-	 { Diclarations publiques }
-	 constructor Create(AOwner: TComponent); override;
-	 destructor Destroy; override;
+    { Diclarations publiques }
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
 
-	 property Texture : TGLTexture read FTexture write SetTexture;
-	 property OnChange : TNotifyEvent read FOnChange write FOnChange;
+    property Texture: TGLTexture read FTexture write SetTexture;
+    property OnChange: TNotifyEvent read FOnChange write FOnChange;
 
   end;
 
@@ -72,132 +78,145 @@ uses
   SysUtils, GLState, GLTextureImageEditors;
 
 // Create
-//
+
 constructor TRTextureEdit.Create(AOwner: TComponent);
 var
-  I: Integer;
+  I: integer;
 begin
-	inherited;
-	FTexture:=TGLTexture.Create(Self);
-	SetTexture(FTexture);
-	SetGLTextureImageClassesToStrings(CBImageClass.Items);
+  inherited;
+  FTexture := TGLTexture.Create(Self);
+  SetTexture(FTexture);
+  SetGLTextureImageClassesToStrings(CBImageClass.Items);
 
-	for i := 0 to Integer(High(TGLTextureImageAlpha)) do
-	  CBImageAlpha.Items.Add(GetEnumName(TypeInfo(TGLTextureImageAlpha), i));
-	for i := 0 to Integer(High(TGLMagFilter)) do
-	  CBMagFilter.Items.Add(GetEnumName(TypeInfo(TGLMagFilter), i));
-	for i := 0 to Integer(High(TGLMinFilter)) do
-	  CBMinFilter.Items.Add(GetEnumName(TypeInfo(TGLMinFilter), i));
-	for i := 0 to Integer(High(TGLTextureMode)) do
-	  CBTextureMode.Items.Add(GetEnumName(TypeInfo(TGLTextureMode), i));
-	for i := 0 to Integer(High(TGLTextureWrap)) do
-	  CBTextureWrap.Items.Add(GetEnumName(TypeInfo(TGLTextureWrap), i));
+  for i := 0 to integer(High(TGLTextureImageAlpha)) do
+    CBImageAlpha.Items.Add(GetEnumName(TypeInfo(TGLTextureImageAlpha), i));
+  for i := 0 to integer(High(TGLMagFilter)) do
+    CBMagFilter.Items.Add(GetEnumName(TypeInfo(TGLMagFilter), i));
+  for i := 0 to integer(High(TGLMinFilter)) do
+    CBMinFilter.Items.Add(GetEnumName(TypeInfo(TGLMinFilter), i));
+  for i := 0 to Integer(High(TGLTextureFilteringQuality)) do
+    CBFilteringQuality.Items.Add(GetEnumName(TypeInfo(TGLTextureFilteringQuality), i));
+  for i := 0 to integer(High(TGLTextureMode)) do
+    CBTextureMode.Items.Add(GetEnumName(TypeInfo(TGLTextureMode), i));
+  for i := 0 to integer(High(TGLTextureWrap)) do
+    CBTextureWrap.Items.Add(GetEnumName(TypeInfo(TGLTextureWrap), i));
 end;
 
 // Destroy
-//
+
 destructor TRTextureEdit.Destroy;
 begin
-	FTexture.Free;
-	inherited;
+  FTexture.Free;
+  inherited;
 end;
 
 // SetTexture
-//
-procedure TRTextureEdit.SetTexture(const val : TGLTexture);
+
+procedure TRTextureEdit.SetTexture(const val: TGLTexture);
 begin
-	FTexture.Assign(val);
-	changeing:=True;
-	try
-		with CBImageClass do ItemIndex:=Items.IndexOfObject(Pointer(FTexture.ClassType));
-		CBImageAlpha.ItemIndex:=Integer(FTexture.ImageAlpha);
-		CBMagFilter.ItemIndex:=Integer(FTexture.MagFilter);
-		CBMinFilter.ItemIndex:=Integer(FTexture.MinFilter);
-		CBTextureMode.ItemIndex:=Integer(FTexture.TextureMode);
-		CBTextureWrap.ItemIndex:=Integer(FTexture.TextureWrap);
-		CBDisabled.Checked:=FTexture.Disabled;
-	finally
-		changeing:=False;
-		DoOnChange;
-	end;
+  FTexture.Assign(val);
+  changeing := True;
+  try
+    with CBImageClass do
+      ItemIndex := Items.IndexOfObject(Pointer(FTexture.Image.ClassType));
+    CBImageAlpha.ItemIndex := integer(FTexture.ImageAlpha);
+    CBMagFilter.ItemIndex := integer(FTexture.MagFilter);
+    CBMinFilter.ItemIndex := integer(FTexture.MinFilter);
+    CBFilteringQuality.ItemIndex:=Integer(FTexture.FilteringQuality);
+    CBTextureMode.ItemIndex := integer(FTexture.TextureMode);
+    CBTextureWrap.ItemIndex := integer(FTexture.TextureWrap);
+    CBDisabled.Checked := FTexture.Disabled;
+  finally
+    changeing := False;
+    DoOnChange;
+  end;
 end;
 
 // DoOnChange
-//
+
 procedure TRTextureEdit.DoOnChange;
 begin
-	if (not changeing) and Assigned(FOnChange) then
-		OnChange(Self);
+  if (not changeing) and Assigned(FOnChange) then
+    OnChange(Self);
 end;
 
 // CBImageClassChange
-//
+
 procedure TRTextureEdit.CBImageClassChange(Sender: TObject);
 var
-	tic : TGLTextureImageClass;
-	ti : TGLTextureImage;
+  tic: TGLTextureImageClass;
+  ti: TGLTextureImage;
 begin
-	if not changeing then begin
-		with CBImageClass do	tic:=TGLTextureImageClass(Items.Objects[ItemIndex]);
-		if FTexture.Image.ClassType<>tic then begin
-			ti:=TGLTextureImageClass(tic).Create(FTexture);
-			FTexture.Image:=ti;
-			ti.Free;
-		end;
-		DoOnChange;
-	end;
+  if not changeing then
+  begin
+    with CBImageClass do
+      tic := TGLTextureImageClass(Items.Objects[ItemIndex]);
+    if FTexture.Image.ClassType <> tic then
+    begin
+      ti := TGLTextureImageClass(tic).Create(FTexture);
+      FTexture.Image := ti;
+      ti.Free;
+    end;
+    DoOnChange;
+  end;
 end;
 
 // CBImageAlphaChange
-//
+
 procedure TRTextureEdit.CBImageAlphaChange(Sender: TObject);
 begin
-	FTexture.ImageAlpha:=TGLTextureImageAlpha(CBImageAlpha.ItemIndex);
-	DoOnChange;
+  FTexture.ImageAlpha := TGLTextureImageAlpha(CBImageAlpha.ItemIndex);
+  DoOnChange;
 end;
 
 // CBMagFilterChange
-//
+
 procedure TRTextureEdit.CBMagFilterChange(Sender: TObject);
 begin
-	FTexture.MagFilter:=TGLMagFilter(CBMagFilter.ItemIndex);
-	DoOnChange;
+  FTexture.MagFilter := TGLMagFilter(CBMagFilter.ItemIndex);
+  DoOnChange;
+end;
+
+procedure TRTextureEdit.CBFilteringQualityChange(Sender: TObject);
+begin
+  FTexture.FilteringQuality:=TGLTextureFilteringQuality(CBFilteringQuality.ItemIndex);
+  DoOnChange;
 end;
 
 // CBMinFilterChange
-//
+
 procedure TRTextureEdit.CBMinFilterChange(Sender: TObject);
 begin
-	FTexture.MinFilter:=TGLMinFilter(CBMinFilter.ItemIndex);
-	DoOnChange;
+  FTexture.MinFilter := TGLMinFilter(CBMinFilter.ItemIndex);
+  DoOnChange;
 end;
 
 // CBTextureModeChange
-//
+
 procedure TRTextureEdit.CBTextureModeChange(Sender: TObject);
 begin
-	FTexture.TextureMode:=TGLTextureMode(CBTextureMode.ItemIndex);
-	DoOnChange;
+  FTexture.TextureMode := TGLTextureMode(CBTextureMode.ItemIndex);
+  DoOnChange;
 end;
 
 // CBTextureWrapChange
-//
+
 procedure TRTextureEdit.CBTextureWrapChange(Sender: TObject);
 begin
-	FTexture.TextureWrap:=TGLTextureWrap(CBTextureWrap.ItemIndex);
-	DoOnChange;
+  FTexture.TextureWrap := TGLTextureWrap(CBTextureWrap.ItemIndex);
+  DoOnChange;
 end;
 
 // CBDisabledClick
-//
+
 procedure TRTextureEdit.CBDisabledClick(Sender: TObject);
 begin
-	FTexture.Disabled:=CBDisabled.Checked;
-	DoOnChange;
+  FTexture.Disabled := CBDisabled.Checked;
+  DoOnChange;
 end;
 
 // SBEditImageClick
-//
+
 procedure TRTextureEdit.SBEditImageClick(Sender: TObject);
 begin
   EditGLTextureImage(FTexture.Image);
@@ -210,6 +229,4 @@ initialization
   {$I FRTextureEditLCL.lrs}
 
 end.
-
-
 
