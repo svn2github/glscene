608aceec93a75ef33bed903f563d2f7d1bc9defa
lemerou <lemerou@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2010-09-19 16:41:26 +0000

    Fixed invalid µs value for new cubes
    git-svn-id: http://svn.code.sf.net/p/glscene/code@5170 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Demos/physics/OdeConveyor/Unit1.pas
+++ trunk/Demos/physics/OdeConveyor/Unit1.pas
@@ -1,3 +1,44 @@
+{ : ODE Conveyor belt demo.<p>
+
+  This demo demontrates how to use the Motion1 parameter of ODE.
+
+  Surface settings applied when a collision occured. In this situation, the ODE
+  Manager uses surface settings of both objects:
+  eg: muFinal = (muObject1 + muObject2)*0.5
+
+  To choose the direction of the motion, we are changing the FDir-1 parameter of
+  the collision contact.
+
+  For a full explanation take a look at:
+  http://opende.sourceforge.net/wiki/index.php/Manual_(All)#Contact
+
+  Approximate coefficients of friction (from http://en.wikipedia.org/wiki/Friction)
+  ------------------------------------
+  Materials 	    Static friction (µs)
+  ---------------------------------------------------
+  Dry & clean 	Lubricated
+  Aluminum 	      Steel 	  0.61
+  Copper 	        Steel 	  0.53
+  Brass 	        Steel 	  0.51
+  Cast iron 	    Copper 	  1.05
+  Cast iron 	    Zinc 	    0.85
+  Concrete (wet) 	Rubber 	  0.30
+  Concrete (dry) 	Rubber 	  1.0
+  Concrete 	      Wood 	    0.62[7]
+  Copper 	        Glass 	  0.68
+  Glass 	        Glass 	  0.94
+  Metal 	        Wood 	    0.2-0.6[7] 	  0.2 (wet)[7]
+  Polythene 	    Steel 	  0.2[8] 	      0.2[8]
+  Steel 	        Steel 	  0.80[8] 	    0.16[8]
+  Steel 	        Teflon  	0.04[8] 	    0.04[8]
+  Teflon 	        Teflon 	  0.04[8] 	    0.04[8]
+  Wood 	          Wood 	    0.25-0.5[7] 	0.2 (wet)[7]
+
+  <b>History : </b><font size=-1><ul>
+  <li>19/09/10 - YP - Created by Yann Papouin
+  </ul>
+}
+
 unit Unit1;
 
 interface
@@ -58,9 +99,11 @@ type
     NormX: TLabel;
     AddODECube: TButton;
     SpawnPoint: TGLDummyCube;
-    procedure GLCadencer1Progress(Sender: TObject; const deltaTime, newTime: Double);
+    procedure GLCadencer1Progress(Sender: TObject;
+      const deltaTime, newTime: Double);
     procedure FormShow(Sender: TObject);
-    procedure GLODEManager1Collision(Sender, Object1, Object2: TObject; var Contact: TdContact; var HandleCollision: Boolean);
+    procedure GLODEManager1Collision(Sender, Object1, Object2: TObject;
+      var Contact: TdContact; var HandleCollision: Boolean);
     procedure TrackBarMotionSpeedChange(Sender: TObject);
     procedure FrictionChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -104,14 +147,14 @@ begin
   GLCadencer1.Enabled := true;
 end;
 
-procedure TForm1.GLCadencer1Progress(Sender: TObject; const deltaTime, newTime: Double);
+procedure TForm1.GLCadencer1Progress(Sender: TObject;
+  const deltaTime, newTime: Double);
 begin
   GLODEManager1.Step(deltaTime);
 end;
 
-procedure TForm1.GLODEManager1Collision(Sender, Object1, Object2: TObject; var Contact: TdContact; var HandleCollision: Boolean);
-var
-  AObject: TGLBaseSceneObject;
+procedure TForm1.GLODEManager1Collision(Sender, Object1, Object2: TObject;
+  var Contact: TdContact; var HandleCollision: Boolean);
 begin
   if Object2 = GetOrCreateOdeStatic(ConveyorBelt1) then
   begin
@@ -158,6 +201,12 @@ begin
   // Add ODE Dynamic behaviour on it
   AODEDynamic := GetOrCreateOdeDynamic(ACube);
   AODEDynamic.Manager := GLODEManager1;
+
+  // Set µs value to 1 (default=1000), just uses the one from the conveyor
+  with AODEDynamic.Surface do
+  begin
+    Mu := 1;
+  end;
 
   // Finally create physical data in this behaviour
   AODEElementBox := TODEElementBox(AODEDynamic.AddNewElement(TODEElementBox));
