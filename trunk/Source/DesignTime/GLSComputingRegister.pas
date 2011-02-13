c0e590373debb1c8270908d714e1b5ba80dfd984
yarunderoaker <yarunderoaker@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2011-02-13 23:03:23 +0000

    Added CUDA units
    git-svn-id: http://svn.code.sf.net/p/glscene/code@5507 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Source/DesignTime/GLSComputingRegister.pas
@@ -0,0 +1,416 @@
+//
+// This unit is part of the GLScene Project, http://glscene.org
+//
+{: GLSComputingRegister<p>
+
+   Registration unit for GLScene Computing package.<p>
+
+	<b>History : </b><font size=-1><ul>
+      <li>22/08/10 - Yar - Some improvements for FPC (thanks Predator)
+      <li>09/06/10 - Yar - Added dropdown list ProjectModule for TGLSCUDACompiler
+      <li>19/03/10 - Yar - Creation
+	</ul></font>
+}
+unit GLSComputingRegister;
+
+interface
+
+uses
+  Classes, SysUtils, GLSceneRegister,
+  {$IFNDEF FPC}
+  DesignIntf, DesignEditors, STREDIT, ToolsAPI
+  {$ELSE}
+  propedits, componenteditors
+  {$ENDIF};
+
+procedure Register;
+
+type
+
+  TGLSCUDAEditor = class(TComponentEditor)
+  public
+    { Public Declarations }
+    procedure Edit; override;
+    procedure ExecuteVerb(Index: Integer); override;
+    function GetVerb(Index: Integer): string; override;
+    function GetVerbCount: Integer; override;
+  end;
+
+  TGLSCUDACompilerEditor = class(TComponentEditor)
+  public
+    { Public Declarations }
+    procedure Edit; override;
+    procedure ExecuteVerb(Index: Integer); override;
+    function GetVerb(Index: Integer): string; override;
+    function GetVerbCount: Integer; override;
+  end;
+
+  TGLSCUDACompilerSourceProperty = class(TStringProperty)
+  private
+    FModuleList: TStringList;
+    procedure RefreshModuleList;
+  public
+    { Public Declarations }
+    {$IFNDEF FPC}
+    constructor Create(const ADesigner: IDesigner; APropCount: Integer); override;
+    {$ELSE}
+    constructor Create(Hook: TPropertyEditorHook; APropCount: Integer); override;
+    {$ENDIF}
+    destructor Destroy; override;
+    function GetAttributes: TPropertyAttributes; override;
+    procedure GetValues(Proc: TGetStrProc); override;
+    procedure SetValue(const Value: String); override;
+  end;
+
+  TCUDAModuleCodeEditor = class(TStringListProperty)
+  public
+    { Public Declarations }
+    procedure Edit; override;
+  end;
+
+  TGLSCUDADeviceProperty = class(TStringProperty)
+  private
+    FDeviceList: TStringList;
+  public
+    { Public Declarations }
+    {$IFNDEF FPC}
+    constructor Create(const ADesigner: IDesigner; APropCount: Integer); override;
+    {$ELSE}
+    constructor Create(Hook: TPropertyEditorHook; APropCount: Integer); override;
+    {$ENDIF}
+    destructor Destroy; override;
+    function GetAttributes: TPropertyAttributes; override;
+    procedure GetValues(Proc: TGetStrProc); override;
+    procedure SetValue(const Value: String); override;
+  end;
+
+implementation
+
+uses
+  GLSCUDAEditor,
+  GLSCUDAContext, GLSCUDA, GLSCUDACompiler, GLSCUDAFFTPlan, GLSCUDAGraphics;
+
+procedure Register;
+begin
+  RegisterComponents('GLScene Computing', [TGLSCUDA, TGLSCUDADevice,
+    TGLSCUDACompiler]);
+  RegisterComponentEditor(TGLSCUDA, TGLSCUDAEditor);
+  RegisterComponentEditor(TGLSCUDACompiler, TGLSCUDACompilerEditor);
+	RegisterPropertyEditor(TypeInfo(TStringList), TCUDAModule, 'Code',
+    TCUDAModuleCodeEditor);
+  RegisterPropertyEditor(TypeInfo(string), TGLSCUDACompiler, 'ProjectModule',
+    TGLSCUDACompilerSourceProperty);
+  RegisterPropertyEditor(TypeInfo(string), TGLSCUDADevice, 'SelectDevice',
+    TGLSCUDADeviceProperty);
+  RegisterNoIcon([TCUDAModule, TCUDAMemData, TCUDAFunction, TCUDATexture,
+    TCUDAFFTPlan, TCUDAGLImageResource, TCUDAGLGeometryResource]);
+
+  ObjectManager.RegisterSceneObject(TGLFeedBackMesh, 'GPU generated mesh', 'Computing', HInstance);
+end;
+
+function FindCuFile(var AModuleName: string): Boolean;
+var
+  proj: IOTAProject;
+  I: Integer;
+  LModule: IOTAModuleInfo;
+  LName: string;
+begin
+  proj := GetActiveProject;
+  if proj <> nil then
+  begin
+    for I := 0 to proj.GetModuleCount - 1 do
+    begin
+      LModule := proj.GetModule(I);
+      LName := ExtractFileName(LModule.FileName);
+      if LName = AModuleName then
+      begin
+        AModuleName := LModule.FileName;
+        exit(True);
+      end;
+    end;
+  end;
+  Result := False;
+end;
+
+// ------------------
+// ------------------ TGLSCUDAEditor ------------------
+// ------------------
+
+procedure TGLSCUDAEditor.Edit;
+begin
+  with GLSCUDAEditorForm do
+  begin
+    SetCUDAEditorClient(TGLSCUDA(Self.Component), Self.Designer);
+    Show;
+  end;
+end;
+
+procedure TGLSCUDAEditor.ExecuteVerb(Index: Integer);
+begin
+  case Index of
+    0: Edit;
+  end;
+end;
+
+function TGLSCUDAEditor.GetVerb(Index: Integer): string;
+begin
+  case Index of
+    0: Result := 'Show CUDA Items Editor';
+  end;
+end;
+
+function TGLSCUDAEditor.GetVerbCount: Integer;
+begin
+  Result := 1;
+end;
+
+// ------------------
+// ------------------ TGLSCUDACompilerEditor ------------------
+// ------------------
+
+procedure TGLSCUDACompilerEditor.Edit;
+var
+  CUDACompiler: TGLSCUDACompiler;
+  i: Integer;
+  Task: PDesignerTask;
+  obj: TCUDAComponent;
+begin
+  CUDACompiler := TGLSCUDACompiler(Self.Component);
+  CUDACompiler.Compile;
+  // Ugly hack for design time dynamicaly creating components
+  if CUDACompiler.DesignerTaskList <> nil then
+  begin
+    for i := 0 to CUDACompiler.DesignerTaskList.Count - 1 do
+    begin
+      Task := CUDACompiler.DesignerTaskList.Items[i];
+      if Task.Creating then
+      begin
+        obj := TCUDAComponent(Designer.CreateComponent(Task.ItemClass,
+          Task.Owner, 0,
+          0, 0, 0));
+        obj.Master := Task.Owner;
+        if Task.ItemClass = TCUDAFunction then
+          TCUDAFunction(obj).KernelName := Task.KernelName
+        else if Task.ItemClass = TCUDATexture then
+          TCUDATexture(obj).KernelName := Task.KernelName;
+      end
+      else
+      begin
+        Designer.SelectComponent(Task.Owner);
+        Designer.DeleteSelection(true);
+      end;
+      Dispose(Task);
+    end;
+    CUDACompiler.DesignerTaskList.Free;
+    CUDACompiler.DesignerTaskList := nil;
+    Designer.Modified;
+  end;
+end;
+
+procedure TGLSCUDACompilerEditor.ExecuteVerb(Index: Integer);
+begin
+  case Index of
+    0: Edit;
+  end;
+end;
+
+function TGLSCUDACompilerEditor.GetVerb(Index: Integer): string;
+begin
+  case Index of
+    0: Result := 'Compile Module';
+  end;
+end;
+
+function TGLSCUDACompilerEditor.GetVerbCount: Integer;
+begin
+  Result := 1;
+end;
+
+// ------------------
+// ------------------ TGLSCUDACompilerSourceProperty ------------------
+// ------------------
+
+{$IFNDEF FPC}
+constructor TGLSCUDACompilerSourceProperty.Create(
+    const ADesigner: IDesigner; APropCount: Integer);
+{$ELSE}
+constructor TGLSCUDACompilerSourceProperty.Create(
+       Hook:TPropertyEditorHook; APropCount: Integer);
+{$ENDIF}
+begin
+  inherited;
+  FModuleList := TStringList.Create;
+end;
+
+// Destroy
+//
+
+destructor TGLSCUDACompilerSourceProperty.Destroy;
+begin
+  FModuleList.Destroy;
+  inherited;
+end;
+
+// RefreshModuleList
+//
+
+procedure TGLSCUDACompilerSourceProperty.RefreshModuleList;
+var
+  proj: IOTAProject;
+  I: Integer;
+  LModule: IOTAModuleInfo;
+  LName: string;
+begin
+  FModuleList.Clear;
+  FModuleList.Add('none');
+  proj := GetActiveProject;
+  if proj <> nil then
+  begin
+    for I := 0 to proj.GetModuleCount - 1 do
+    begin
+      LModule := proj.GetModule(I);
+      LName := UpperCase(ExtractFileExt(LModule.FileName));
+      if LName = '.CU' then
+        FModuleList.Add(LModule.FileName);
+    end;
+  end;
+end;
+
+// GetAttributes
+//
+
+function TGLSCUDACompilerSourceProperty.GetAttributes;
+begin
+  Result := [paValueList];
+end;
+
+// GetValues
+//
+procedure TGLSCUDACompilerSourceProperty.GetValues(Proc: TGetStrProc);
+var
+   I : Integer;
+begin
+  RefreshModuleList;
+  for I := 0 to FModuleList.Count - 1 do
+      Proc(ExtractFileName(FModuleList[I]));
+end;
+
+// SetValue
+//
+procedure TGLSCUDACompilerSourceProperty.SetValue(const Value: String);
+var
+  I, J: Integer;
+begin
+  RefreshModuleList;
+  J := -1;
+  for I := 1 to FModuleList.Count - 1 do
+    if Value = ExtractFileName(FModuleList[I]) then
+    begin
+      J := I;
+      Break;
+    end;
+
+  if J > 0 then
+  begin
+    TGLSCUDACompiler(GetComponent(0)).SetSourceCodeFile(FModuleList[J]);
+    SetStrValue(ExtractFileName(Value));
+  end
+  else
+  begin
+    SetStrValue('none');
+  end;
+	Modified;
+end;
+
+// ------------------
+// ------------------ TCUDAModuleCodeEditor ------------------
+// ------------------
+
+procedure TCUDAModuleCodeEditor.Edit;
+var
+  i: Integer;
+  module: TCUDAModule;
+  TaskList: TList;
+  Task: PDesignerTask;
+  obj: TCUDAComponent;
+begin
+  inherited Edit;
+  module := TCUDAModule(GetComponent(0));
+  TaskList := module.GetDesignerTaskList;
+  if Assigned(TaskList) then
+  begin
+    for i := 0 to TaskList.Count - 1 do
+    begin
+      Task := TaskList.Items[i];
+      if Task.Creating then
+      begin
+        obj := TCUDAComponent(Designer.CreateComponent(Task.ItemClass,
+          Task.Owner, 0,
+          0, 0, 0));
+        obj.Master := Task.Owner;
+        if Task.ItemClass = TCUDAFunction then
+          TCUDAFunction(obj).KernelName := Task.KernelName
+        else if Task.ItemClass = TCUDATexture then
+          TCUDATexture(obj).KernelName := Task.KernelName;
+      end
+      else
+      begin
+        Designer.SelectComponent(Task.Owner);
+        Designer.DeleteSelection(true);
+      end;
+      Dispose(Task);
+    end;
+    TaskList.Free;
+    Designer.Modified;
+  end;
+end;
+
+// ------------------
+// ------------------ TGLSCUDADeviceProperty ------------------
+// ------------------
+
+{$IFNDEF FPC}
+constructor TGLSCUDADeviceProperty.Create(const ADesigner: IDesigner; APropCount: Integer);
+{$ELSE}
+constructor TGLSCUDADeviceProperty.Create(Hook: TPropertyEditorHook; APropCount: Integer);
+{$ENDIF}
+begin
+  inherited;
+  FDeviceList := TStringList.Create;
+end;
+
+destructor TGLSCUDADeviceProperty.Destroy;
+begin
+  FDeviceList.Destroy;
+  inherited;
+end;
+
+function TGLSCUDADeviceProperty.GetAttributes: TPropertyAttributes;
+begin
+  Result := [paValueList];
+end;
+
+procedure TGLSCUDADeviceProperty.GetValues(Proc: TGetStrProc);
+begin
+  CUDAContextManager.FillUnusedDeviceList(FDeviceList);
+end;
+
+procedure TGLSCUDADeviceProperty.SetValue(const Value: String);
+var
+  I: Integer;
+begin
+  for I := 0 to FDeviceList.Count - 1 do
+    if Value = FDeviceList[I] then
+    begin
+      SetStrValue(Value);
+      Break;
+    end;
+  Modified;
+end;
+
+initialization
+
+  vFindCuFileFunc := FindCuFile;
+
+end.
+
