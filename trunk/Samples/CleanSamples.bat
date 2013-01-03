0e64c1208bd4cb56d60a0c8244531248275b1634
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2013-01-03 17:38:40 +0000

    Added delete of Release_Build folders for CPPB projects
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6161 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Samples/CleanSamples.bat
+++ trunk/Samples/CleanSamples.bat
@@ -59,9 +59,4 @@ echo delete debug and Platform directories with all subdirectories and files
 for /r %1 %%R in (Win32) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (Win64) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (Debug_Build) do if exist "%%R" (rd /s /q "%%R")
-
-
-
-
-
-
+for /r %1 %%R in (Release_Build) do if exist "%%R" (rd /s /q "%%R")
