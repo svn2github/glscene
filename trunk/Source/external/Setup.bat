2daa4e35add2701ff5d04bfcd7b6fa0c9f6e2da6
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2013-01-04 09:58:21 +0000

    Added more dlls
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6164 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/external/Setup.bat
+++ trunk/Source/external/Setup.bat
@@ -1,40 +1,31 @@
 @echo off
-echo Copying cg.dll to the windows system32 directory
-
-@echo Deploying for NT
+echo Copying DLLs to the windows system32 directory
 copy cg.dll %SystemRoot%\System32\
-
-@echo off
-echo Copying cgGL.dll to the windows system32 directory
-
-@echo Deploying for NT
 copy cgGL.dll %SystemRoot%\System32\
-
-@echo off
-echo Copying SDL.dll to the windows system32 directory
-
-@echo Deploying for NT
+copy sdl.dll %SystemRoot%\System32\
 copy SDL.dll %SystemRoot%\System32\
 
-@echo off
-echo Copying bass.dll to the windows system32 directory
-
-@echo Deploying for NT
+echo Copying Sound's dll to the windows system32 directory
 copy bass.dll %SystemRoot%\System32\
-
-@echo off
-echo Copying fmod.dll to the windows system32 directory
-
-@echo Deploying for NT
 copy fmod.dll %SystemRoot%\System32\
+copy OpenAL64.dll %SystemRoot%\System32\
 
-@echo off
+
 echo Copying ODE's dll to the windows system32 directory
-
-@echo Deploying for NT
 copy ode_single.dll %SystemRoot%\System32\
 copy ode_singled.dll %SystemRoot%\System32\
 copy ode_double.dll %SystemRoot%\System32\
 copy ode_doubled.dll %SystemRoot%\System32\
+
+echo Copying Newton's dll to the windows system32 directory
+copy ode_single.dll %SystemRoot%\System32\
+copy ode_singled.dll %SystemRoot%\System32\
+copy newton.dll %SystemRoot%\System32\
+copy newton_d.dll %SystemRoot%\System32\
+copy dJointLibrary.dll %SystemRoot%\System32\
+copy dJointLibrary_d.dll %SystemRoot%\System32\
+
+
+copy zlib1.dll %SystemRoot%\System32\
 
 pause
