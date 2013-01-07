6c1c02d9815effbc23528ab560e310ee3aeeff04
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2013-01-07 14:10:29 +0000

    Added del exe demos
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6205 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Samples/CleanSamples.bat
+++ trunk/Samples/CleanSamples.bat
@@ -1,4 +1,5 @@
 echo off
+del *.exe /s  
 del *.dcu /s 
 del *.ddp /s
 del *.ppu /s
@@ -49,7 +50,6 @@ echo ************************************************
 echo _
 
 
-rem del *.exe /s  applications
 rem del *.cfg /s  animations
 
 
