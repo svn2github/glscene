e44d0dd83a889e64e14ede168065683fc53bcb00
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2013-01-17 19:51:18 +0000

    Added more rendering demos for cppb
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6227 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Samples/CPP/Demos/rendering/invariance/Unit1.h
@@ -0,0 +1,49 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include "BaseClasses.hpp"
+#include "GLCoordinates.hpp"
+#include "GLCrossPlatform.hpp"
+#include "GLGeomObjects.hpp"
+#include "GLMaterial.hpp"
+#include "GLObjects.hpp"
+#include "GLScene.hpp"
+#include "GLWin32Viewer.hpp"
+#include "JPEG.hpp"
+#include "GLUtils.hpp"
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TGLSceneViewer *GLSceneViewer1;
+	TGLScene *GLScene1;
+	TGLDummyCube *DCCamera;
+	TGLCamera *GLCamera;
+	TGLPlane *PLGround;
+	TGLCube *GLCube1;
+	TGLLightSource *GLLightSource1;
+	TGLDummyCube *DCPositionInvariant;
+	TGLCylinder *GLCylinder1;
+	TGLDummyCube *DCOrientationInvariant;
+	TGLArrowLine *GLArrowLine1;
+	TGLMaterialLibrary *GLMaterialLibrary;
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall GLSceneViewer1MouseMove(TObject *Sender, TShiftState Shift, int X,
+		  int Y);
+	void __fastcall GLSceneViewer1MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+		  int X, int Y);
+private:	// User declarations
+    int mx, my;
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
