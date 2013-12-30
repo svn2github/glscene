121bad40e8680f6f99cc46e530c411117917178a
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2013-12-30 17:34:34 +0000

    Updated Christmas.scr demo with Happy New Year 2014 to All!
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6409 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Samples/Delphi/AdvDemos/Christmas/FMain.pas
+++ trunk/Samples/Delphi/AdvDemos/Christmas/FMain.pas
@@ -3,94 +3,92 @@ unit FMain;
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
-  Dialogs, GLScene, GLVectorFileObjects, GLObjects, GLWin32Viewer,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms,  Vcl.Dialogs,
+
+  //GLScene
+  GLScene, GLVectorFileObjects, GLObjects, GLWin32Viewer,
   GLTexture, ExtCtrls, GLCadencer, GLSkydome, GLParticleFX, VectorGeometry,
-  GLLensFlare, GLSound, GLSMBASS, GLBitmapFont, GLWindowsFont, GLHUDObjects,
+  GLLensFlare, GLBitmapFont, GLWindowsFont, GLHUDObjects,
   GLScreenSaver, GLShadowPlane, GLFile3DS, GLGeomObjects, GLMaterial,
-  GLCoordinates, BaseClasses, GLCrossPlatform, GLColor, GLFileWAV;
+  GLCoordinates, BaseClasses, GLCrossPlatform, GLColor,  Jpeg,
+  GLSound, GLSMBASS, GLFileWAV, Bass;
 
 type
   TMain = class(TForm)
     Scene: TGLScene;
     Viewer: TGLSceneViewer;
-    GLCamera: TGLCamera;
+    Camera: TGLCamera;
     DCFirTree: TGLDummyCube;
     FFFirTree: TGLFreeForm;
     LSRoom: TGLLightSource;
     POFirTree2: TGLProxyObject;
     POFirTree3: TGLProxyObject;
-    Timer: TTimer;
-    GLCadencer: TGLCadencer;
+    Cadencer: TGLCadencer;
     DCCameraTarget: TGLDummyCube;
     FFFirePlace: TGLFreeForm;
-    GLMaterialLibrary: TGLMaterialLibrary;
+    MaterialLibrary: TGLMaterialLibrary;
     LSFire: TGLLightSource;
     PFXFire: TGLPolygonPFXManager;
     DCFireSource: TGLDummyCube;
     ParticleFXRenderer: TGLParticleFXRenderer;
     CYLog: TGLCylinder;
     DCLensFlares: TGLDummyCube;
-    GLLensFlare1: TGLLensFlare;
-    GLLensFlare2: TGLLensFlare;
-    GLLensFlare3: TGLLensFlare;
-    GLLensFlare4: TGLLensFlare;
-    GLSMBASS: TGLSMBASS;
-    GLSoundLibrary: TGLSoundLibrary;
+    LensFlare1: TGLLensFlare;
+    LensFlare2: TGLLensFlare;
+    LensFlare3: TGLLensFlare;
+    LensFlare4: TGLLensFlare;
+    SMBASS: TGLSMBASS;
+    SoundLibrary: TGLSoundLibrary;
     DCDecoWhite: TGLDummyCube;
-    DCTemplates: TGLDummyCube;
+    DCBalls: TGLDummyCube;
     SPWhiteBall: TGLSphere;
-    GLProxyObject1: TGLProxyObject;
+    POWhiteBall1: TGLProxyObject;
     SPGoldBall: TGLSphere;
-    GLProxyObject2: TGLProxyObject;
+    POGoldBall1: TGLProxyObject;
     DCDecoGold: TGLDummyCube;
-    GLProxyObject3: TGLProxyObject;
-    LSFireLens: TGLLensFlare;
-    GLLensFlare5: TGLLensFlare;
-    GLProxyObject4: TGLProxyObject;
-    GLProxyObject5: TGLProxyObject;
-    GLProxyObject6: TGLProxyObject;
-    GLLensFlare6: TGLLensFlare;
+    POGoldBall2: TGLProxyObject;
+    LFFireLens: TGLLensFlare;
+    LensFlare5: TGLLensFlare;
+    POWhiteBall2: TGLProxyObject;
+    POGoldBall3: TGLProxyObject;
+    POWhiteBall3: TGLProxyObject;
+    LensFlare6: TGLLensFlare;
     PFXTree: TGLPolygonPFXManager;
     FTCountDown: TGLFlatText;
-    GLWindowsBitmapFont: TGLWindowsBitmapFont;
-    GLCube1: TGLCube;
+    WindowsBitmapFont: TGLWindowsBitmapFont;
+    Cube1: TGLCube;
     DCGifts: TGLDummyCube;
-    GLCube2: TGLCube;
-    HSGLScene: TGLHUDSprite;
-//    ScreenSaver: TScreenSaver;
-    GLShadowPlane: TGLShadowPlane;
+    Cube2: TGLCube;
+    ShadowPlane: TGLShadowPlane;
     DCTree: TGLDummyCube;
-    GLCube3: TGLCube;
-    GLCube4: TGLCube;
+    Cube3: TGLCube;
+    Cube4: TGLCube;
     DCFire: TGLDummyCube;
-    GLScreenSaver1: TGLScreenSaver;
+    ScreenSaver: TGLScreenSaver;
+    Timer: TTimer;
+    HUDSprite: TGLHUDSprite;
     procedure FormCreate(Sender: TObject);
-    procedure ViewerMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
-    procedure ViewerMouseMove(Sender: TObject; Shift: TShiftState; X,
-      Y: Integer);
-    procedure TimerTimer(Sender: TObject);
-    procedure GLCadencerProgress(Sender: TObject; const deltaTime,
+    procedure CadencerProgress(Sender: TObject; const deltaTime,
       newTime: Double);
     procedure FormResize(Sender: TObject);
     procedure FormKeyPress(Sender: TObject; var Key: Char);
+    procedure TimerTimer(Sender: TObject);
+    procedure ScreenSaverCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure ScreenSaverExecute(Sender: TObject);
+    procedure ScreenSaverPreview(Sender: TObject; previewHwnd: HWND);
+    procedure ViewerMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure ViewerMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
     procedure ViewerDblClick(Sender: TObject);
-    procedure GLScreenSaver1CloseQuery(Sender: TObject; var CanClose: Boolean);
-    procedure GLScreenSaver1Execute(Sender: TObject);
-    procedure GLScreenSaver1Preview(Sender: TObject; previewHwnd: HWND);
-    procedure GLScreenSaver1PropertiesRequested(Sender: TObject);
-//    procedure ScreenSaverCloseQuery(Sender: TObject;
-//      var CanClose: Boolean);
-//    procedure ScreenSaverPreview(Sender: TObject; previewHwnd: HWND);
-//    procedure ScreenSaverExecute(Sender: TObject);
-//    procedure ScreenSaverPropertiesRequested(Sender: TObject);
+    procedure ScreenSaverPropertiesRequested(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
     mx, my : Integer;
-    fireLight : Single;
+    FireLight : Single;
     inPreview, inSaver : Boolean;
     bStream : Cardinal;
   end;
@@ -102,10 +100,6 @@ implementation
 
 {$R *.dfm}
 
-uses
-  Jpeg,
-  Bass;
-
 procedure TMain.FormCreate(Sender: TObject);
 var
   DataPath : String;
@@ -115,21 +109,68 @@ begin
    SetCurrentDir(DataPath);
    FFFirTree.LoadFromFile('firtree.3ds');
    FFFirePlace.LoadFromFile('fireplace.3ds');
-   fireLight:=0.5;
+   FireLight:=0.5;
+end;
+
+procedure TMain.FormResize(Sender: TObject);
+begin
+   Camera.SceneScale:=Width/640;
+   if Visible then
+      HUDSprite.Position.X:=Self.Width-200;
+   if (Width>=Screen.Width) then
+      ViewerDblClick(Self);
+end;
+
+procedure TMain.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+   Key:=#0;
+   Application.Terminate;
+end;
+
+procedure TMain.ViewerDblClick(Sender: TObject);
+begin
+   if (not inPreview) and (not inSaver) and (not Application.Terminated) and (BorderStyle<>bsNone) then
+   begin
+      BorderStyle:=bsNone;
+      FormStyle:=fsStayOnTop;
+      Align:=alClient;
+   end;
 end;
 
 procedure TMain.ViewerMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-   mx:=x; my:=y;
+  mx:=x; my:=y;
 end;
 
 procedure TMain.ViewerMouseMove(Sender: TObject; Shift: TShiftState; X,
   Y: Integer);
 begin
    if Shift=[ssLeft] then begin
-      GLCamera.MoveAroundTarget(my-y, mx-x);
+      Camera.MoveAroundTarget(my-y, mx-x);
       mx:=x; my:=y;
+   end;
+end;
+
+
+procedure TMain.CadencerProgress(Sender: TObject; const deltaTime,
+  newTime: Double);
+begin
+   Viewer.Invalidate();
+   FireLight:=ClampValue(fireLight+Random*0.4-0.2, 0, 1);
+   LSFire.Diffuse.Color:=VectorLerp(clrYellow, VectorMake(0.5, 0, 0, 1),
+                                    fireLight);
+   LSFire.Position.Y:=FireLight*0.1;
+
+   if inPreview then
+      HUDSprite.Visible:=False;
+
+   if HUDSprite.Visible then
+   begin
+     HUDSprite.Material.FrontProperties.Diffuse.Alpha:=
+         HUDSprite.Material.FrontProperties.Diffuse.Alpha-deltaTime*0.1;
+     if HUDSprite.Material.FrontProperties.Diffuse.Alpha<0.01 then
+         HUDSprite.Visible:=False;
    end;
 end;
 
@@ -142,17 +183,14 @@ var
 begin
    Caption:=Format('%.1f FPS', [Viewer.FramesPerSecond]);
    Viewer.ResetPerformanceMonitor;
-
-   if GLSMBASS.Active and (bStream=0) then
+   if SMBASS.Active and (bStream=0) then
    begin
       bStream:=BASS_StreamCreateFile(false, PAnsiChar('Jingle_Bells_64.mp3'), 0, 0, BASS_STREAM_AUTOFREE);
       BASS_ChannelPlay(bStream, False);
    end;
-
-   DecodeDate(Now, y, m, d);
-   t:=EncodeDate(y, 12, 25)-Now;
-   if t<0 then
-      FTCountDown.Text:='Merry Christmas!';
+//   t:=EncodeDate(y, 12, 25)-Now;     //Merry Cristmas!
+//   t:=EncodeDate(2014, 01, 07)-Now;  //Merry Cristmas for Orthodox!
+   t:=EncodeDate(2014, 01, 01)-Now;    //Happy New Year!
    if (t<1) and (t>-1) then
       DCGifts.Visible:=True;
    if t>=2 then
@@ -186,76 +224,28 @@ begin
    end;
 end;
 
-procedure TMain.GLCadencerProgress(Sender: TObject; const deltaTime,
-  newTime: Double);
-begin
-   Viewer.Invalidate;
-   fireLight:=ClampValue(fireLight+Random*0.4-0.2, 0, 1);
-   LSFire.Diffuse.Color:=VectorLerp(clrYellow, VectorMake(0.5, 0, 0, 1),
-                                    fireLight);
-   LSFire.Position.Y:=fireLight*0.1;
 
-   if inPreview then
-      HSGLScene.Visible:=False;
-   if Visible then
-   begin
-      with HSGLScene.Material.FrontProperties.Diffuse do
-      begin
-         Alpha:=Alpha-deltaTime*0.15;
-         if Alpha<0.01 then
-            Visible:=False;
-      end;
-   end;
-end;
-
-procedure TMain.FormResize(Sender: TObject);
-begin
-   GLCamera.SceneScale:=Width/640;
-   if Visible then
-      HSGLScene.Position.X:=Self.Width-200;
-   if (Width>=Screen.Width) then
-      ViewerDblClick(Self);
-end;
-
-procedure TMain.FormKeyPress(Sender: TObject; var Key: Char);
-begin
-   Key:=#0;
-   Application.Terminate;
-end;
-
-procedure TMain.ViewerDblClick(Sender: TObject);
-begin
-   if (not inPreview) and (not inSaver) and (not Application.Terminated) and (BorderStyle<>bsNone) then
-   begin
-      BorderStyle:=bsNone;
-      FormStyle:=fsStayOnTop;
-      Align:=alClient;
-   end;
-end;
-
-procedure TMain.GLScreenSaver1CloseQuery(Sender: TObject;
+procedure TMain.ScreenSaverCloseQuery(Sender: TObject;
   var CanClose: Boolean);
 begin
    Application.Terminate;
    CanClose:=False;
 end;
 
-procedure TMain.GLScreenSaver1Execute(Sender: TObject);
+procedure TMain.ScreenSaverExecute(Sender: TObject);
 begin
    inSaver:=True;
 end;
 
-procedure TMain.GLScreenSaver1Preview(Sender: TObject; previewHwnd: HWND);
+procedure TMain.ScreenSaverPreview(Sender: TObject; previewHwnd: HWND);
 begin
-   inPreview:=True;
+  inPreview:=True;
 end;
 
-procedure TMain.GLScreenSaver1PropertiesRequested(Sender: TObject);
+procedure TMain.ScreenSaverPropertiesRequested(Sender: TObject);
 begin
-   ShowMessage( 'GLScene Christmas 2014'#13#10#13#10
-               +'http://glscene.org');
+   ShowMessage( 'GLScene Merry Christmas and Happy New Year 2014'#13#10#13#10
+               +'http://www.sourceforge.net/projects/glscene');
 end;
-
-
 
 end.
