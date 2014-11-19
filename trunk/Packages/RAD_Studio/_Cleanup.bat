c4817932bec951bce1405b6797cd8309387051da
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2014-11-19 10:21:31 +0000

    GLScene packages for RAD Studio
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6489 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

---
+++ trunk/Packages/RAD_Studio/_Cleanup.bat
@@ -0,0 +1,19 @@
+rem del *.dproj /s - it will delete compiler options for c++ outputs
+del *.local /s
+del *.identcache /s
+del *.obj /s
+del *.~* /s
+del *.dsk /s
+del *.res /s
+del *.log /s
+del *.tvsconfig /s
+
+
+echo --------------------------------------------------------
+echo delete directories with subdirectories and files 
+echo---------------------------------------------------------
+for /r %1 %%R in (Win32) do if exist "%%R" (rd /s /q "%%R")
+for /r %1 %%R in (Win64) do if exist "%%R" (rd /s /q "%%R")
+for /r %1 %%R in (Debug_Build) do if exist "%%R" (rd /s /q "%%R")
+for /r %1 %%R in (Release_Build) do if exist "%%R" (rd /s /q "%%R")
+for /r %1 %%R in (__history) do if exist "%%R" (rd /s /q "%%R")
