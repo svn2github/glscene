6564fc3eb86ed15fb08bdb0191e9f0179981c707
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2015-05-04 19:01:58 +0000

    Fixed import from surfer and arcinfo grd formats to GLHeightField
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6659 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLFileGRD.pas
+++ trunk/Source/GLFileGRD.pas
@@ -6,8 +6,10 @@
   GRD (Grid Text Format) vector file format implementation.<p>
 
   <b>History :</b><font size=-1><ul>
-  <li>01/05/15 - PW - Fixed LoadFromStrem procedure
+  <li>01/05/15 - PW - Fixed LoadFromStream procedure
   <li>10/04/09 - PW - Separated from GLVectorFileObjects.pas
+  <li>10/04/09 - Aaron Hochwimmer - Added support for Surfer ASCII grd format
+  <li>26/10/00 - Phil Scadden - created import of Arcinfo ASCII grd format
   </ul></font>
 }
 unit GLFileGRD;
@@ -21,35 +23,45 @@ uses
 
   // GLS
   GLVectorGeometry, GLVectorTypes, GLVectorFileObjects,
-  GLApplicationFileIO, GLGraph, GLTypes;
+  GLApplicationFileIO, GLGraph;
 
 type
   // TGLGRDVectorFile
   //
   { : The GRD file represents ascii grid formats in 2D/3D.<p>
-    This is a format for storing regular grids as a
-    matrices of cell centers. The format is extensible, supports variations and
-    subformats. This importer only works for the simplest variant (triangles
-    without specified normals, and will ignore most header specifications. }
+    This is a format for storing regular grid values as a
+    matrices of cell centers. The format supports variations and
+    subformats. This importer works for Sutfer, ArcInfo and GMS formats}
+
   TGLGRDVectorFile = class(TVectorFile)
-  public
+   public
     { Public Declarations }
     HeightField: TGLHeightField;
-    GridArray: TGLPoint3DArray;
+    Nodes: array of TSingleArray;
     class function Capabilities: TDataFileCapabilities; override;
     procedure LoadFromStream(aStream: TStream); override;
-    procedure HeightFieldGetHeight(const X, Y: Single; var Z: Single;
-      var Color: TVector4f; var TexPoint: TTexPoint);
+   private
+    StrVal: String;
+    StrLine: String;
+    MaxZ: Single;
+    function ExtractWord(N: Integer; const S: string;
+      const WordDelims: TSysCharSet): string;
+    function WordPosition(const N: Integer; const S: string;
+      const WordDelims: TSysCharSet): Integer;
+
   end;
 
   // ------------------------------------------------------------------
   // ------------------------------------------------------------------
   // ------------------------------------------------------------------
 implementation
-
 // ------------------
 // ------------------ TGLGRDVectorFile ------------------
 // ------------------
+
+const
+  dSURFBLANKVAL = 1.70141E38; // default value in Surfer for blanking
+  NODATA_value =  -9999; //default value in GIS ArcInfo for blanking
 
 // Capabilities
 //
@@ -58,57 +70,81 @@ begin
   Result := [dfcRead];
 end;
 
-procedure TGLGRDVectorFile.HeightFieldGetHeight(const X, Y: Single;
-  var Z: Single; var Color: TVector4f; var TexPoint: TTexPoint);
-
-// This assigns the heights from the grid in memory
+function TGLGRDVectorFile.WordPosition(const N: Integer; const S: string;
+  const WordDelims: TSysCharSet): Integer;
 var
-  Heit: Single;
+  Count, I: Integer;
+begin
+  Count := 0;
+  I := 1;
+  Result := 0;
+  while ((I <= Length(S)) and (Count <> N)) do
+  begin
+    // skip over delimiters
+    while (I <= Length(S)) and (S[I] in WordDelims) do
+      Inc(I);
+    // if we're not beyond end of S, we're at the start of a word
+    if I <= Length(S) then
+      Inc(Count);
+    // if not finished, find the end of the current word
+    if Count <> N then
+      while (I <= Length(S)) and not(S[I] in WordDelims) do
+        Inc(I)
+    else
+      Result := I;
+  end;
+end;
+
+// ExtractWord
+//
+function TGLGRDVectorFile.ExtractWord(N: Integer; const S: string;
+  const WordDelims: TSysCharSet): string;
+
+var
+  I, Len: Integer;
 
 begin
-  (*
-    Heit := GridArray[Trunc(Y) + (GRD.Height div 2),Trunc(X) + (GRD.Width div 2)];
-    if Heit = GRD.null then
-    Z := 0
-    else
-    Z := (Heit / GRD.CellSize) * Vertical_exaggeration;
-
-    {The equation above is based on fact that each x,y is a step on the grid and
-    in real terms, each step is cellsize metres wide}
-    with HeightField do
+  Len := 0;
+  I := WordPosition(N, S, WordDelims);
+  if (I <> 0) then
+    // find the end of the current word
+    while (I <= Length(S)) and not(S[I] in WordDelims) do
     begin
-    TexPoint.S := (x - XSamplingScale.min)/GRD.Width;
-    TexPoint.T := (y - YSamplingScale.min)/GRD.Height;
+      // add the I'th character to result
+      Inc(Len);
+      SetLength(Result, Len);
+      Result[Len] := S[I];
+      Inc(I);
     end;
-  *)
+  SetLength(Result, Len);
 end;
 
 // LoadFromStream
 //
 procedure TGLGRDVectorFile.LoadFromStream(aStream: TStream);
 var
-  i, j: Integer;
-  sl, tl: TStringList;
-
-  // mesh : TMeshObject;
-  // fg : TFGVertexIndexList;
-  // n1, n2, n3 : Integer;
-  // v1, v2, v3, n : TAffineVector;
-  // p : PChar;
+  I, J, K: Integer;
+  N : Integer; // N => counter to increment through file
+  Sl, Tl: TStringList;
 
   Nx, Ny: Integer;
   Dx, Dy: Single;
   Xo, Xe, Yo, Ye, Zo, Ze: Single;
+  NBlanks : Integer; // Number of blank nodes
+  BlankVal, NoData : Double;
 
-  S: String;
+  { sub } function ReadLine: string;
+  begin
+    Result := Sl[N];
+    Inc(N);
+  end;
 
 begin
   sl := TStringList.Create;
   tl := TStringList.Create;
   try
     sl.LoadFromStream(aStream);
-    S := Copy(sl[0], 1, 5);
-    if (sl[0] <> 'DSAA') and (S <> 'ncols') then
+    if (Copy(sl[0], 1, 4) <> 'DSAA') and (Copy(sl[0], 1, 5) <> 'ncols') then
     begin
       raise Exception.Create('Not a valid grd file !');
       Exit;
@@ -116,93 +152,102 @@ begin
 
     HeightField := TGLHeightField.Create(Owner);
 
-    SetLength(GridArray, sl.Count);
     Zo := 3 * 10E38; // Low
     Ze := -3 * 10E38; // High
+    Tl.DelimitedText := Copy(ReadLine, 1, 4);
 
-    if (sl[0] = 'DSAA') then // Surfer grid
+    if (Tl[0] = 'DSAA') then // Surfer ASCII grid
     begin
-      tl.DelimitedText := sl[1];
-      Nx := StrToInt(tl[0]);
-      Ny := StrToInt(tl[1]); // Nx Ny
+      Tl.DelimitedText := ReadLine;
+      Nx := StrToInt(Tl[0]);
+      Ny := StrToInt(Tl[1]);
 
-      tl.DelimitedText := sl[2];
-      Xo := StrToInt(tl[0]);
-      Xe := StrToInt(tl[1]); // Xo Xe
+      Tl.DelimitedText := ReadLine;
+      Xo := StrToFloat(Tl[0]);
+      Xe := StrToFloat(Tl[1]);
 
-      tl.DelimitedText := sl[3];
-      Yo := StrToInt(tl[0]);
-      Ye := StrToInt(tl[1]); // Yo Ye
+      Tl.DelimitedText := ReadLine;
+      Yo := StrToFloat(Tl[0]);
+      Ye := StrToFloat(Tl[1]);
 
-      tl.DelimitedText := sl[4];
-      Zo := StrToInt(tl[0]);
-      Ze := StrToInt(tl[1]); // Zo Ze
+      Tl.DelimitedText := ReadLine;
+      Zo := StrToFloat(Tl[0]);
+      Ze := StrToFloat(Tl[1]);
 
-      for i := 0 to Ny - 1 do
+      Dx := (Xe - Xo) / Nx;
+      Dy := (Ye - Yo) / Ny;
+
+      SetLength(Nodes, Ny, Nx);
+
+      Nblanks := 0;
+      BlankVal := dSURFBLANKVAL;
+      NoData := BlankVal; // NoData value
+
+      // loop over the Ny-1 Rows
+      for I := 0 to Ny - 1 do
       begin
-        tl.DelimitedText := sl[i];
-        GridArray[i].X := Xo + (i mod Nx) * Dx;
-        GridArray[i].Y := Yo + Dy * (i div Nx);
-        GridArray[i].Z := Zo + StrToFloat(tl[i]);
+        J := 0;
+        // reading lines until Nx-1 Cols entries have been obtained
+        while J <= Nx - 1 do
+        begin
+          StrLine := ReadLine;
+          K := 1;
+          StrVal := ExtractWord(K, StrLine, [' ']);
+          while (StrVal <> '') do
+          begin
+            if (J <= Nx - 1) then
+              Nodes[I, J] := StrToFloat(StrVal);
+            if Nodes[I, J] > MaxZ then
+              MaxZ := Nodes[I, J];
+            if (Nodes[I, J] >= BlankVal) then
+              Nblanks := Nblanks + 1;
 
-        {
-          while i < sl.Count do begin
-          //
+            Inc(J);
+            Inc(K);
+            StrVal := ExtractWord(K, StrLine, [' ']);
           end;
-        }
-      end;
-
+          if (J > Nx - 1) then
+            Break;
+        end;
+      end
     end
-    else // ArcInfo grid
+    else // ArcInfo ASCII grid
     begin
-      tl.DelimitedText := sl[0];
-      Nx := StrToInt(tl[1]); // ncols
-      tl.DelimitedText := sl[1];
-      Ny := StrToInt(tl[1]); // nrows
-      tl.DelimitedText := sl[2];
-      Xo := StrToFloat(tl[1]); // xllcorner
-      tl.DelimitedText := sl[3];
-      Yo := StrToFloat(tl[1]); // yllcorner
-      tl.DelimitedText := sl[4];
-      Dx := StrToFloat(tl[1]); // cellsize
-      Dy := Dx;
-      // NODATA_value :=  -9999
+      Tl.DelimitedText := Sl[0];
+      Ny := StrToInt(Tl[1]); // ncols
+      Tl.DelimitedText := Sl[1];
+      Nx := StrToInt(Tl[1]); // nrows
+      Tl.DelimitedText := Sl[2];
+      Xo := StrToFloat(Tl[1]); // xllcorner
+      Tl.DelimitedText := Sl[3];
+      Yo := StrToFloat(Tl[1]); // yllcorner
+      Tl.DelimitedText := Sl[4];
+      Dx := StrToFloat(Tl[1]);
+      Dy := Dx;     // cellsize
+      Tl.DelimitedText := Sl[5];
+      NoData := StrToFloat(Tl[1]); // NoData value
 
+      MaxZ := -3 * 10E38;
+      SetLength(Nodes, Nx, Ny);
+
+      for I := 0 to Nx - 1 do
+      begin
+        Tl.DelimitedText := Sl[I + 6];
+        for J := 0 to Ny - 1 do
+        begin
+          StrVal := Tl[J];
+          Nodes[I, J] := StrToFloat(StrVal);
+          if Nodes[I, J] > MaxZ then
+            MaxZ := Nodes[I, J];
+        end;
+      end;
     end;
 
-    (*
-      grid := TGLHeightField.Create();
-      grid.Mode := momFaceGroups;
-      while i<sl.Count do begin
-      if sl[i]='end_header' then Break;
-      if Copy(sl[i], 1, 14)='element vertex' then
-      nbVertices:=StrToIntDef(Copy(sl[i], 16, MaxInt), 0);
-      if Copy(sl[i], 1, 12)='element face' then
-      nbFaces:=StrToIntDef(Copy(sl[i], 14, MaxInt), 0);
-      Inc(i);
-      end;
-      Inc(i);
-      // vertices
-      grid.Vertices.Capacity:=nbVertices;
-      while (i<sl.Count) and (nbVertices>0) do begin
-      p:=PChar(sl[i]);
-      grid.Vertices.Add(ParseFloat(p), ParseFloat(p), ParseFloat(p));//AffineVectorMake(StrToFloatDef(tl[0]), StrToFloatDef(tl[1]), StrToFloatDef(tl[2])));}
-      Dec(nbVertices);
-      Inc(i);
-      end;
-      // faces
-      fg:=TFGVertexIndexList.CreateOwned(mesh.FaceGroups);
-      fg.Mode:=fgmmTriangles;
-      fg.VertexIndices.Capacity:=nbFaces*3;
-      while (i<sl.Count) and (nbFaces>0) do begin
-      p:=PChar(sl[i]);
-      ParseInteger(p); // skip index
-      fg.VertexIndices.Add(ParseInteger(p), ParseInteger(p), ParseInteger(p));
-      Dec(nbFaces);
-      Inc(i);
-      end;
-      grid.BuildNormals(fg.VertexIndices, momTriangles);
-    *)
+    HeightField.XSamplingScale.Min := -(Nx div 2);
+    HeightField.XSamplingScale.Max := (Nx div 2);
+    HeightField.YSamplingScale.Min := -(Ny div 2);
+    HeightField.YSamplingScale.Max := (Ny div 2);
+
   finally
     tl.Free;
     sl.Free;
