c2573e4df81c76900c284ae08836123186335cac
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2016-10-09 19:41:33 +0000

    Added VKScene groupprojects for Win32/Win64
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6912 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- branches/VKScene/CleanForRelease.bat
+++ branches/VKScene/CleanForRelease.bat
@@ -1,5 +1,6 @@
 @echo off
 del *.dcu /s
+del *.dcp /s
 del *.ddp /s
 del *.ppu /s
 del *.o /s
@@ -17,14 +18,33 @@ del *.rsm /s
 del *.drc /s
 del *.2007 /s
 del *.local /s
+
+rem delete more files
+
+del *.bak /s
+del *.xml /s
 del *.cvsignore /s
 del *.identcache /s
 del *.otares /s
 del *.tvsconfig /s
 del *.stat /s
+del *.db /s
+
+rem delete cpp builder files
+
 del *.hpp /s
 del *.bpi /s
 del *.lib /s
+del *.#00 /s
+del *.pch /s
+del *.tds /s
+del *.ilc /s
+del *.ild /s
+del *.ilf /s
+del *.ils /s
+del *.pdi /s
+del *.vlb /s
+
 
 echo _
 echo ************************************************
@@ -33,22 +53,22 @@ echo ************************************************
 echo _
 
 attrib +R "AdvDemos/Q3Demo/Model/animation.cfg"
-attrib +R "Source/DesignTime/Resources/lazres.exe"
+rem del *.cfg /s  - there are quake's animations
+attrib -R "AdvDemos/Q3Demo/Model/animation.cfg"
 
 rem del *.res /s
-rem del *.cur /s
-rem del *.exe /s
-rem del *.cfg /s
+rem del *.ico /s  - some projects have own icos
+rem del *.cur /s  - cursors
+rem del *.obj /s  - obj models and resources for lazarus
 
-attrib -R "AdvDemos/Q3Demo/Model/animation.cfg"
-attrib -R "Source/DesignTime/Resources/lazres.exe"
 
-for /r %1 %%R in (Win32) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (Win64) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (iOS) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (Android) do if exist "%%R" (rd /s /q "%%R")
+
+
+echo delete all .svn directories with subdirectories and files 
+for /r %1 %%R in (.svn) do if exist "%%R" (rd /s /q "%%R")
+echo---------------------------------------------------------
+echo delete debug and Platform directories with all subdirectories and files 
 for /r %1 %%R in (Debug_Build) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (Release_Build) do if exist "%%R" (rd /s /q "%%R")
-
 for /r %1 %%R in (__history) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (__recovery) do if exist "%%R" (rd /s /q "%%R")
