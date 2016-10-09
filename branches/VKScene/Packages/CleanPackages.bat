2bde36ed6a12129576d0f419e0bb4dc31e0cca8d
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2016-10-09 19:37:30 +0000

    Added VKScene groupprojects for Win32/Win64
    git-svn-id: http://svn.code.sf.net/p/glscene/code@6910 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- branches/VKScene/Packages/CleanPackages.bat
+++ branches/VKScene/Packages/CleanPackages.bat
@@ -11,14 +11,9 @@ del *.log /s
 del *.tvsconfig /s
 del *.stat /s
 
-
 echo --------------------------------------------------------
 echo delete directories with subdirectories and files 
 echo---------------------------------------------------------
-for /r %1 %%R in (Win32) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (Win64) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (iOS) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (Android) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (Debug_Build) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (Release_Build) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (__history) do if exist "%%R" (rd /s /q "%%R")
