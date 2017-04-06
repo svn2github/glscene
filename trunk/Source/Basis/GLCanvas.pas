c3729ab60a457060420072a3d633a0046e5b4729
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2017-04-06 07:42:35 +0000

    Updated to use with RAD Studio 10.2 Tokyo
    git-svn-id: http://svn.code.sf.net/p/glscene/code@7020 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/Basis/GLCanvas.pas
+++ trunk/Source/Basis/GLCanvas.pas
@@ -33,23 +33,19 @@ type
 
   TArcDirection = (adCounterClockWise, adClockWise);
 
-  // TGLCanvas
-  //
     {A simple Canvas-like interface for OpenGL.
        This class implements a small "shell" for 2D operations in OpenGL,
        it operates over the current OpenGL context and provides methods
        for drawing lines, ellipses and points.
        This class is typically used by creating an instance, using it for drawing,
        and freeing the instance. When drawing (0, 0) is the top left corner.
-       All coordinates are internally maintained with floating point precision. 
+       All coordinates are internally maintained with floating point precision.
        Several states are cached and it is of primary importance not to invoke
        OpenGL directly throughout the life of an instance (at the cost of
        unespected behaviour). }
   TGLCanvas = class
   private
-     
     FBufferSizeX, FBufferSizeY: Integer;
-
     FLastPrimitive: Integer;
     FCurrentPos: TAffineVector;
     FPenColor: TColor;
@@ -57,122 +53,98 @@ type
     FCurrentPenColorVector: TVector;
     FArcDirection: TArcDirection;
   protected
-    
     procedure BackupOpenGLStates;
-
     procedure StartPrimitive(const primitiveType: Integer);
-
     procedure EllipseVertices(x, y, xRadius, yRadius: Single);
-
     procedure SetPenColor(const val: TColor);
-
     function GetPenAlpha: Single;
     procedure SetPenAlpha(const val: Single);
     procedure SetPenWidth(const val: Integer);
-
     procedure SwapSingle(pX, pY: PSingle);
     procedure NormalizePoint(const x1, y1, x2, y2: Single;
       const x, y: Single; pX, pY: PSingle);
-
     procedure DrawArc(x1, y1, x2, y2, x3, y3, x4, y4: Single;
       UpdateCurrentPos: Boolean); overload;
     procedure DrawArc(x1, y1, x2, y2: Single;
       AngleBegin, AngleEnd: Single;
       UpdateCurrentPos: Boolean); overload;
   public
-    
     constructor Create(bufferSizeX, bufferSizeY: Integer;
       const baseTransform: TMatrix); overload;
     constructor Create(bufferSizeX, bufferSizeY: Integer); overload;
     destructor Destroy; override;
-
-    {Stops the current internal primitive. 
+    {Stops the current internal primitive.
        This function is invoked automatically by TGLCanvas when changeing
        primitives, you should directly call if you want to render your
        own stuff intertwined with TGLCanvas drawings. In that case, call
        it before your own OpenGL calls. }
     procedure StopPrimitive;
-
-    {Inverts the orientation of the Y Axis. 
+    {Inverts the orientation of the Y Axis.
        If (0, 0) was in the top left corner, it will move to the bottom
        left corner or vice-versa. }
     procedure InvertYAxis;
-
     property CanvasSizeX: Integer read FBufferSizeX;
     property CanvasSizeY: Integer read FBufferSizeY;
-
     {Current Pen Color. }
     property PenColor: TColor read FPenColor write SetPenColor;
     {Current Pen Alpha channel (from 0.0 to 1.0) }
     property PenAlpha : Single read GetPenAlpha write SetPenAlpha;
     {Current Pen Width. }
     property PenWidth: Integer read FPenWidth write SetPenWidth;
-
     {Updates the current position (absolute coords). }
     procedure MoveTo(const x, y: Integer); overload;
     procedure MoveTo(const x, y: Single); overload;
     {Updates the current position (relative coords). }
     procedure MoveToRel(const x, y: Integer); overload;
     procedure MoveToRel(const x, y: Single); overload;
-
-    {Draws a line from current position to given coordinate. 
-       Current position is updated. }
+    {Draws a line from current position to given coordinate.
+     Current position is updated. }
     procedure LineTo(const x, y: Integer); overload;
     procedure LineTo(const x, y: Single); overload;
     procedure LineToRel(const x, y: Integer); overload;
     procedure LineToRel(const x, y: Single); overload;
-    {Draws a line from (x1, y1) to (x2, y2). 
-       The current position is NOT updated. }
+    {Draws a line from (x1, y1) to (x2, y2).
+     The current position is NOT updated. }
     procedure Line(const x1, y1, x2, y2: Integer); overload;
     procedure Line(const x1, y1, x2, y2: Single); overload;
-
-    {Draws the set of lines defined by connecting the points. 
+    {Draws the set of lines defined by connecting the points.
        Similar to invoking MoveTo on the first point, then LineTo
        on all the following points. }
     procedure Polyline(const points: array of TGLPoint);
     {Similar to Polyline but also connects the last point to the first. }
     procedure Polygon(const points: array of TGLPoint);
-
     {Plots a pixel at given coordinate.
-       PenWidth affects pixel size.
-       The current position is NOT updated. }
+     PenWidth affects pixel size.
+     The current position is NOT updated. }
     procedure PlotPixel(const x, y: Integer); overload;
     procedure PlotPixel(const x, y: Single); overload;
-
     {Draw the (x1,y1)-(x2, y2) rectangle's frame (border). }
     procedure FrameRect(const x1, y1, x2, y2: Integer); overload;
     procedure FrameRect(const x1, y1, x2, y2: Single); overload;
-
     {Draw the (x1,y1)-(x2, y2) rectangle (filled with PenColor). }
     procedure FillRect(const x1, y1, x2, y2: Integer); overload;
     procedure FillRect(const x1, y1, x2, y2: Single); overload;
-
     {Draw the (x1,y1)-(x2, y2) rectangle (filled with given gradient's color). }
     procedure FillRectGradient(const x1, y1, x2, y2: Single;
       const x1y1Color, x2y1Color, x2y2Color, x1y2Color: TColorVector); overload;
     procedure FillRectGradient(const x1, y1, x2, y2: Integer;
       const x1y1Color, x2y1Color, x2y2Color, x1y2Color: TColorVector); overload;
-
     {Draws an ellipse with (x1,y1)-(x2, y2) bounding rectangle. }
     procedure EllipseBB(const x1, y1, x2, y2: Integer); overload;
     procedure EllipseBB(const x1, y1, x2, y2: Single); overload;
-
     {Draws and ellipse centered at (x, y) with given radiuses. }
     procedure Ellipse(const x, y: Integer; const xRadius, yRadius: Single);
       overload;
     procedure Ellipse(const x, y: Single; const xRadius, yRadius: Single);
       overload;
     procedure Ellipse(const x, y: Single; const Radius: Single); overload;
-
     {Draw a filled ellipse. }
     procedure FillEllipse(const x, y: Integer; const xRadius, yRadius: Single);
       overload;
     procedure FillEllipse(const x, y: Single; const xRadius, yRadius: Single);
       overload;
-
     procedure FillEllipse(const x, y: Single; const Radius: Single); overload;
-
-    {Draw a filled gradient ellipse. 
+    {Draw a filled gradient ellipse.
     OpenGL will use the last PenColor and PenAlpha as the center color and do gradient to edge of ellipse using the edgeColor parameter. }
     procedure FillEllipseGradient(const x, y, xRadius, yRadius: Single;
       const edgeColor: TColorVector); overload;
@@ -180,36 +152,30 @@ type
       const xRadius, yRadius: Integer; const edgeColor: TColorVector); overload;
     procedure FillEllipseGradient(const x, y, Radius: Single;
       const edgeColor: TColorVector); overload;
-    {Draw an elliptical arc. 
-       The points (x1, y1) and (x2, y2) specify the bounding rectangle. 
-       An ellipse formed by the specified bounding rectangle defines the curve of the arc. 
+    {Draw an elliptical arc.
+       The points (x1, y1) and (x2, y2) specify the bounding rectangle.
+       An ellipse formed by the specified bounding rectangle defines the curve of the arc.
        The arc extends in the current drawing direction from the point where it intersects the radial from the center of the bounding rectangle to the (x3, y3) point.
-       The arc ends where it intersects the radial from the center of the bounding rectangle to the (x4, y4) point. 
-       If the starting point and ending point are the same, a complete ellipse is drawn. 
-       Use the ArcDirection property to get and set the current drawing direction for a device context. 
+       The arc ends where it intersects the radial from the center of the bounding rectangle to the (x4, y4) point.
+       If the starting point and ending point are the same, a complete ellipse is drawn.
+       Use the ArcDirection property to get and set the current drawing direction for a device context.
        The default drawing direction is counterclockwise. }
     procedure Arc(const x1, y1, x2, y2, x3, y3, x4, y4: Integer); overload;
     procedure Arc(const x1, y1, x2, y2, x3, y3, x4, y4: Single); overload;
-    procedure Arc(const x1, y1, x2, y2: Single; AngleBegin,
-      AngleEnd: Single); overload;
-
+    procedure Arc(const x1, y1, x2, y2: Single; AngleBegin, AngleEnd: Single); overload;
     {Same as Arc but update the current position. }
     procedure ArcTo(const x1, y1, x2, y2, x3, y3, x4, y4: Integer); overload;
     procedure ArcTo(const x1, y1, x2, y2, x3, y3, x4, y4: Single); overload;
-    procedure ArcTo(const x1, y1, x2, y2: Single; AngleBegin,
-      AngleEnd: Single); overload;
-
+    procedure ArcTo(const x1, y1, x2, y2: Single; AngleBegin, AngleEnd: Single); overload;
     procedure RoundRect(const x1, y1, x2, y2, xr, yr: Integer); overload;
     procedure RoundRect(const x1, y1, x2, y2, xr, yr: Single); overload;
-
-
     property ArcDirection: TArcDirection read FArcDirection
       write FArcDirection;
   end;
 
-  //-------------------------------------------------------------
-  //-------------------------------------------------------------
-  //-------------------------------------------------------------
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+//-------------------------------------------------------------
 implementation
 //-------------------------------------------------------------
 //-------------------------------------------------------------
@@ -220,13 +186,9 @@ const
   pion2 = pi/2;
   pi3on2 = 3*pion2;
 
-  // ------------------
-  // ------------------ TGLCanvas ------------------
-  // ------------------
-
-   
-  //
-
+// ------------------
+// ------------------ TGLCanvas ------------------
+// ------------------
 constructor TGLCanvas.Create(bufferSizeX, bufferSizeY: Integer;
   const baseTransform: TMatrix);
 var
@@ -239,41 +201,27 @@ begin
   GL.PushMatrix;
   PM := CreateOrthoMatrix(0, bufferSizeX, bufferSizeY, 0, -1, 1);
   GL.LoadMatrixf(@PM);
-
   GL.MatrixMode(GL_MODELVIEW);
   GL.PushMatrix;
   GL.LoadMatrixf(@baseTransform);
-
   BackupOpenGLStates;
-
   FLastPrimitive := cNoPrimitive;
   FArcDirection := adCounterClockWise;
 end;
-
- 
-//
 
 constructor TGLCanvas.Create(bufferSizeX, bufferSizeY: Integer);
 begin
   Create(bufferSizeX, bufferSizeY, IdentityHmgMatrix);
 end;
 
- 
-//
-
 destructor TGLCanvas.Destroy;
 begin
   StopPrimitive;
-
   GL.MatrixMode(GL_PROJECTION);
   GL.PopMatrix;
-
   GL.MatrixMode(GL_MODELVIEW);
   GL.PopMatrix;
 end;
-
-// BackupOpenGLStates
-//
 
 procedure TGLCanvas.BackupOpenGLStates;
 begin
@@ -300,9 +248,6 @@ begin
   end;
 end;
 
-// StartPrimitive
-//
-
 procedure TGLCanvas.StartPrimitive(const primitiveType: Integer);
 begin
   if primitiveType <> FLastPrimitive then
@@ -315,16 +260,10 @@ begin
   end;
 end;
 
-// StopPrimitive
-//
-
 procedure TGLCanvas.StopPrimitive;
 begin
   StartPrimitive(cNoPrimitive);
 end;
-
-// InvertYAxis
-//
 
 procedure TGLCanvas.InvertYAxis;
 var
@@ -336,9 +275,6 @@ begin
   GL.MultMatrixf(@mat);
 end;
 
-// SetPenColor
-//
-
 procedure TGLCanvas.SetPenColor(const val: TColor);
 begin
   SetVector(FCurrentPenColorVector, ConvertWinColor(val,
@@ -347,17 +283,11 @@ begin
   GL.Color4fv(@FCurrentPenColorVector);
 end;
 
-// SetPenAlpha
-//
-
 procedure TGLCanvas.SetPenAlpha(const val: Single);
 begin
   FCurrentPenColorVector.W := val;
   GL.Color4fv(@FCurrentPenColorVector);
 end;
-
-// SetPenWidth
-//
 
 procedure TGLCanvas.SetPenWidth(const val: Integer);
 begin
@@ -373,17 +303,11 @@ begin
     end;
 end;
 
-// MoveTo
-//
-
 procedure TGLCanvas.MoveTo(const x, y: Integer);
 begin
   FCurrentPos.X := x;
   FCurrentPos.Y := y;
 end;
-
-// MoveTo
-//
 
 procedure TGLCanvas.MoveTo(const x, y: Single);
 begin
@@ -391,26 +315,17 @@ begin
   FCurrentPos.Y := y;
 end;
 
-// MoveToRel
-//
-
 procedure TGLCanvas.MoveToRel(const x, y: Integer);
 begin
   FCurrentPos.X := FCurrentPos.X + x;
   FCurrentPos.Y := FCurrentPos.Y + y;
 end;
 
-// MoveToRel
-//
-
 procedure TGLCanvas.MoveToRel(const x, y: Single);
 begin
   FCurrentPos.X := FCurrentPos.X + x;
   FCurrentPos.Y := FCurrentPos.Y + y;
 end;
-
-// LineTo
-//
 
 procedure TGLCanvas.LineTo(const x, y: Integer);
 begin
@@ -420,9 +335,6 @@ begin
   GL.Vertex2fv(@FCurrentPos);
 end;
 
-// LineTo
-//
-
 procedure TGLCanvas.LineTo(const x, y: Single);
 begin
   StartPrimitive(GL_LINES);
@@ -431,24 +343,15 @@ begin
   GL.Vertex2fv(@FCurrentPos);
 end;
 
-// LineToRel
-//
-
 procedure TGLCanvas.LineToRel(const x, y: Integer);
 begin
   LineTo(FCurrentPos.X + x, FCurrentPos.Y + y);
 end;
 
-// LineToRel
-//
-
 procedure TGLCanvas.LineToRel(const x, y: Single);
 begin
   LineTo(FCurrentPos.X + x, FCurrentPos.Y + y);
 end;
-
-// Line
-//
 
 procedure TGLCanvas.Line(const x1, y1, x2, y2: Integer);
 begin
@@ -457,18 +360,12 @@ begin
   GL.Vertex2i(x2, y2);
 end;
 
-// Line
-//
-
 procedure TGLCanvas.Line(const x1, y1, x2, y2: Single);
 begin
   StartPrimitive(GL_LINES);
   GL.Vertex2f(x1, y1);
   GL.Vertex2f(x2, y2);
 end;
-
-// Polyline
-//
 
 procedure TGLCanvas.Polyline(const points: array of TGLPoint);
 var
@@ -485,9 +382,6 @@ begin
   end;
 end;
 
-// Polygon
-//
-
 procedure TGLCanvas.Polygon(const points: array of TGLPoint);
 var
   i, n: Integer;
@@ -503,26 +397,17 @@ begin
   end;
 end;
 
-// PlotPixel
-//
-
 procedure TGLCanvas.PlotPixel(const x, y: Integer);
 begin
   StartPrimitive(GL_POINTS);
   GL.Vertex2i(x, y);
 end;
 
-// PlotPixel
-//
-
 procedure TGLCanvas.PlotPixel(const x, y: Single);
 begin
   StartPrimitive(GL_POINTS);
   GL.Vertex2f(x, y);
 end;
-
-// FrameRect (integer)
-//
 
 procedure TGLCanvas.FrameRect(const x1, y1, x2, y2: Integer);
 begin
@@ -533,9 +418,6 @@ begin
   GL.Vertex2i(x1, y2);
   StopPrimitive;
 end;
-
-// FrameRect (single)
-//
 
 procedure TGLCanvas.FrameRect(const x1, y1, x2, y2: Single);
 begin
@@ -552,9 +434,6 @@ begin
   Result := FCurrentPenColorVector.W;
 end;
 
-// FillRect (integer)
-//
-
 procedure TGLCanvas.FillRect(const x1, y1, x2, y2: Integer);
 begin
   StartPrimitive(GL_QUADS);
@@ -565,9 +444,6 @@ begin
   StopPrimitive;
 end;
 
-// FillRect (single)
-//
-
 procedure TGLCanvas.FillRect(const x1, y1, x2, y2: Single);
 begin
   StartPrimitive(GL_QUADS);
@@ -577,9 +453,6 @@ begin
   GL.Vertex2f(x1, y2);
   StopPrimitive;
 end;
-
-// EllipseVertices
-//
 
 procedure TGLCanvas.EllipseVertices(x, y, xRadius, yRadius: Single);
 var
@@ -607,17 +480,11 @@ begin
     GL.Vertex2f(x + c[i], y + s[i]);
 end;
 
-// EllipseBB
-//
-
 procedure TGLCanvas.EllipseBB(const x1, y1, x2, y2: Integer);
 begin
   Ellipse((x1 + x2) * 0.5, (y1 + y2) * 0.5, Abs(x2 - x1) * 0.5, Abs(y2 - y1) *
     0.5);
 end;
-
-// EllipseBB
-//
 
 procedure TGLCanvas.EllipseBB(const x1, y1, x2, y2: Single);
 begin
@@ -625,16 +492,10 @@ begin
     0.5);
 end;
 
-// Ellipse
-//
-
 procedure TGLCanvas.Ellipse(const x, y: Single; const Radius: Single);
 begin
   Ellipse(x, y, Radius, Radius);
 end;
-
-// Ellipse
-//
 
 procedure TGLCanvas.Ellipse(const x, y: Integer; const xRadius, yRadius:
   Single);
@@ -646,18 +507,12 @@ begin
   Ellipse(sx, sy, xRadius, yRadius);
 end;
 
-// Ellipse
-//
-
 procedure TGLCanvas.Ellipse(const x, y: Single; const xRadius, yRadius: Single);
 begin
   StartPrimitive(GL_LINE_STRIP);
   EllipseVertices(x, y, xRadius, yRadius);
   StopPrimitive;
 end;
-
-// FillEllipse
-//
 
 procedure TGLCanvas.FillEllipse(const x, y: Integer; const xRadius, yRadius:
   Single);
@@ -668,9 +523,6 @@ begin
   StopPrimitive;
 end;
 
-// FillEllipse
-//
-
 procedure TGLCanvas.FillEllipse(const x, y, xRadius, yRadius: Single);
 begin
   StartPrimitive(GL_TRIANGLE_FAN);
@@ -679,16 +531,10 @@ begin
   StopPrimitive;
 end;
 
-// FillEllipse
-//
-
 procedure TGLCanvas.FillEllipse(const x, y, Radius: Single);
 begin
   FillEllipse(x, y, Radius, Radius);
 end;
-
-// FillRectGradient
-//
 
 procedure TGLCanvas.FillRectGradient(const x1, y1, x2, y2: Single;
   const x1y1Color, x2y1Color, x2y2Color, x1y2Color: TColorVector);
@@ -708,9 +554,6 @@ begin
   GL.Color4fv(@FCurrentPenColorVector);
 end;
 
-// FillRectGradient
-//
-
 procedure TGLCanvas.FillRectGradient(const x1, y1, x2, y2: Integer;
   const x1y1Color, x2y1Color, x2y2Color, x1y2Color: TColorVector);
 begin
@@ -729,9 +572,6 @@ begin
   GL.Color4fv(@FCurrentPenColorVector);
 end;
 
-// FillEllipseGradient (integer)
-//
-
 procedure TGLCanvas.FillEllipseGradient(const x, y: Integer; const xRadius, yRadius: Integer; const edgeColor: TColorVector);
 begin
   StartPrimitive(GL_TRIANGLE_FAN);
@@ -748,9 +588,6 @@ begin
   GL.Color4fv(@FCurrentPenColorVector);
 end;
 
-// FillEllipseGradient (single)
-//
-
 procedure TGLCanvas.FillEllipseGradient(const x, y, xRadius, yRadius: Single; const edgeColor: TColorVector);
 begin
   StartPrimitive(GL_TRIANGLE_FAN);
@@ -763,16 +600,10 @@ begin
   GL.Color4fv(@FCurrentPenColorVector);
 end;
 
-// FillEllipseGradient (single)
-//
-
 procedure TGLCanvas.FillEllipseGradient(const x, y, Radius: Single; const edgeColor: TColorVector);
 begin
   FillEllipseGradient(x, y, Radius, Radius, edgeColor);
 end;
-
-// Arc
-//
 
 procedure TGLCanvas.Arc(const x1, y1, x2, y2, x3, y3, x4, y4: Integer);
 begin
@@ -788,9 +619,6 @@ procedure TGLCanvas.Arc(const x1, y1, x2, y2: Single; AngleBegin, AngleEnd: Sing
 begin
   DrawArc(x1, y1, x2, y2, AngleBegin, AngleEnd, False);
 end;
-
-// ArcTo
-//
 
 procedure TGLCanvas.ArcTo(const x1, y1, x2, y2, x3, y3, x4, y4: Integer);
 begin
@@ -843,11 +671,7 @@ begin
   Line(x + xr, y1, x2 - xr, y1);
 end;
 
-
-// Arc Draw
-//
-
-// wrapper from "ByPoints" methode
+// wrapper from "ByPoints" method
 
 procedure TGLCanvas.DrawArc(x1, y1, x2, y2, x3, y3, x4, y4: Single; UpdateCurrentPos: Boolean);
 var
