f4b0ef5b9df5bc46d378ad4d826cc9c5a3336145
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2017-10-15 17:11:35 +0000

    Added bass64 dll and sdl2_32 and sdl2_64 dlls
    git-svn-id: http://svn.code.sf.net/p/glscene/code@7061 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/external/SetupDLLs.bat
+++ trunk/external/SetupDLLs.bat
@@ -20,7 +20,7 @@ copy bass.dll %SystemRoot%\SysWOW64\
 copy fmod.dll %SystemRoot%\SysWOW64\
 copy OpenAL32.dll %SystemRoot%\SysWOW64\
 rem win64
-copy bass.dll %SystemRoot%\System32\
+copy bass64.dll %SystemRoot%\System32\
 copy fmod.dll %SystemRoot%\System32\
 copy OpenAL64.dll %SystemRoot%\System32\
 
