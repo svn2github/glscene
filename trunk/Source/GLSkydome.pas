adffa07da36d7bda03f480ef84535417bb9add6b
vassiliev <vassiliev@c452ef9c-9f09-11df-acb8-e9cdb1ccbceb>
2017-12-04 17:08:09 +0000

    Updated GLSkydome unit
    git-svn-id: http://svn.code.sf.net/p/glscene/code@7098 c452ef9c-9f09-11df-acb8-e9cdb1ccbceb

--- trunk/Source/GLSkydome.pas
+++ trunk/Source/GLSkydome.pas
@@ -7,7 +7,7 @@
   History :
     14/01/01 - EG - Creation
     The whole history is logged in previous version of the unit
-  
+
 }
 unit GLSkydome;
 
@@ -16,14 +16,20 @@ interface
 {$I GLScene.inc}
 
 uses
+{$IFDEF USE_FASTMATH}
+  Neslib.FastMath,
+{$ENDIF}  
   System.Classes,
   System.SysUtils,
   System.UITypes,
   System.Math,
   Vcl.Graphics,
 
+  OpenGLAdapter,
   OpenGLTokens,
   GLScene,
+  GLContext,
+  GLState,
   GLVectorGeometry,
   GLGraphics,
   GLCrossPlatform,
@@ -32,6 +38,13 @@ uses
   GLRenderContextInfo;
 
 type
+   TGLStarRecord = packed record
+      RA : Word;              // x100 builtin factor, degrees
+      DEC : SmallInt;         // x100 builtin factor, degrees
+      BVColorIndex : Byte;    // x100 builtin factor
+      VMagnitude : Byte;      // x10 builtin factor
+   end;
+   PGLStarRecord = ^TGLStarRecord;
 
   TGLSkyDomeBand = class(TCollectionItem)
   private
@@ -224,14 +237,60 @@ type
     property Stacks: Integer read FStacks write SetStacks default 48;
   end;
 
+{ Computes position on the unit sphere of a star record (Z=up). }
+function StarRecordPositionZUp(const starRecord : TGLStarRecord) : TAffineVector;
+{ Computes position on the unit sphere of a star record (Y=up). }
+function StarRecordPositionYUp(const starRecord : TGLStarRecord) : TAffineVector;
+{ Computes star color from BV index (RGB) and magnitude (alpha). }
+function StarRecordColor(const starRecord : TGLStarRecord; bias : Single) : TVector;
+
+
 // ------------------------------------------------------------------
 implementation
 // ------------------------------------------------------------------
 
-uses
-  GLContext,
-  GLStarRecord,
-  GLState;
+function StarRecordPositionYUp(const starRecord : TGLStarRecord) : TAffineVector;
+var
+   f : Single;
+begin
+   SinCosine(starRecord.DEC*(0.01*PI/180), Result.Y, f);
+   SinCosine(starRecord.RA*(0.01*PI/180), f, Result.X, Result.Z);
+end;
+
+function StarRecordPositionZUp(const starRecord : TGLStarRecord) : TAffineVector;
+var
+   f : Single;
+begin
+   SinCosine(starRecord.DEC*(0.01*PI/180), Result.Z, f);
+   SinCosine(starRecord.RA*(0.01*PI/180), f, Result.X, Result.Y);
+end;
+
+function StarRecordColor(const starRecord : TGLStarRecord; bias : Single) : TVector;
+const
+   // very *rough* approximation
+   cBVm035 : TVector = (X:0.7; Y:0.8; Z:1.0; W:1);
+   cBV015  : TVector = (X:1.0; Y:1.0; Z:1.0; W:1);
+   cBV060  : TVector = (X:1.0; Y:1.0; Z:0.7; W:1);
+   cBV135  : TVector = (X:1.0; Y:0.8; Z:0.7; W:1);
+var
+   bvIndex100 : Integer;
+begin
+   bvIndex100:=starRecord.BVColorIndex-50;
+   // compute RGB color for B&V index
+   if bvIndex100<-035 then
+      Result:=cBVm035
+   else if bvIndex100<015 then
+      VectorLerp(cBVm035, cBV015, (bvIndex100+035)*(1/(015+035)), Result)
+   else if bvIndex100<060 then
+      VectorLerp(cBV015, cBV060, (bvIndex100-015)*(1/(060-015)), Result)
+   else if bvIndex100<135 then
+      VectorLerp(cBV060, cBV135, (bvIndex100-060)*(1/(135-060)), Result)
+   else Result:=cBV135;
+   // compute transparency for VMag
+   // the actual factor is 2.512, and not used here
+   Result.W:=PowerSingle(1.2, -(starRecord.VMagnitude*0.1-bias));
+end;
+
 
 // ------------------
 // ------------------ TGLSkyDomeBand ------------------
@@ -446,9 +505,6 @@ begin
   if Assigned(owner) and (owner is TGLBaseSceneObject) then TGLBaseSceneObject(owner).StructureChanged;
 end;
 
-// BuildList
-//
-
 procedure TGLSkyDomeBands.BuildList(var rci: TGLRenderContextInfo);
 var
   i: Integer;
@@ -616,7 +672,7 @@ begin
   end;
   GL.End_;
 
-  // restore default GLScene AlphaFunc
+  // restore default AlphaFunc
   rci.GLStates.SetGLAlphaFunction(cfGreater, 0);
 end;
 
@@ -1236,11 +1292,7 @@ begin
 end;
 
 //-------------------------------------------------------------
-//-------------------------------------------------------------
-//-------------------------------------------------------------
 initialization
-//-------------------------------------------------------------
-//-------------------------------------------------------------
 //-------------------------------------------------------------
 
   RegisterClasses([TGLSkyDome, TGLEarthSkyDome]);
